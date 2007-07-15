5f33aa893fb0dd2405be1bc1ebcc9a274df8a954
Timo Felbinger <gittf@qipc.org>
2007-07-15 07:26:44 +0000

    Konfiguration ueber leitvariablen-tabelle (fuer persistente variable wie "readonly")

--- www/code/connect_MySQL.php
+++ www/code/connect_MySQL.php
@@ -1,12 +1,6 @@
 <?PHP
 
    // DATEI: Hierwird die Verbindeung zur MySQL-Datenbank aufgebaut und geprüft
-
-
-   if (!($db        = mysql_connect($db_server,$db_user,$db_pwd)) || !@MYSQL_SELECT_DB($db_name))
-   {
-	    echo "<html><body><h1>Datenbankfehler!</h1>Konnte keine Verbindung zur Datenbank herstellen... Bitte später nochmal versuchen.</body></html>";
-			exit();
-   }
+   // ...jetzt bereits in config.php (um config in datenbank speichern zu koennen!)
 
 ?>
