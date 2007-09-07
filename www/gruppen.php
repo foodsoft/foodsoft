dd3680512e87f1940fbbbc39efb26fea8b3046de
Timo Felbinger <gittf@qipc.org>
2007-09-07 12:33:51 +0000

    sql ausgelagert, formulare systematisch per self_url(), self_post()

--- www/gruppen.php
+++ www/gruppen.php
@@ -2,59 +2,56 @@
 
 <?PHP
   
-  require_once("code/login.php");
-  require_once("code/zuordnen.php");
+assert( $angemeldet ) or exit();
 
-  // ggf. Aktionen durchführen (z.B. Gruppe löschen...)
-  get_http_var('action');
-  if( $action == 'delete' ) {
-    fail_if_readonly();
-    nur_fuer_dienst(4,5);
-    need_http_var('gruppen_id');
+// ggf. Aktionen durchführen (z.B. Gruppe löschen...)
+get_http_var('action','w');
+if( $action == 'delete' ) {
+  fail_if_readonly();
+  nur_fuer_dienst(5);
+  need_http_var('gruppen_id','u');
 
-    ( $result = mysql_query( "SELECT * FROM bestellgruppen WHERE id='$gruppen_id'" ) )
-      or error(__LINE__,__FILE__,"Bestellgruppe nicht gefunden.",mysql_error());
-    ( $row = mysql_fetch_array( $result ) )
-      or error(__LINE__,__FILE__,"Bestellgruppe nicht gefunden.",mysql_error());
+  $row = sql_gruppendaten( $gruppen_id );
 
-    $kontostand = kontostand( $row['id'] );
-    if( abs($kontostand) > 0.005 ) {
-      echo "
-        <div class='warn'>Kontostand: $kontostand ist nicht null: L&ouml;schen nicht m&ouml;glich!</div>
-      ";
-    } elseif( $row['mitgliederzahl'] != 0 ) {
-      echo "
-        <div class='warn'>Mitgliederzahl ist nicht null: L&ouml;schen nicht m&ouml;glich (Sockelbetrag!)</div>
-        <div class='warn'>(bitte erst auf null setzen, um Sockelbetrag zu verbuchen!)</div>
-      ";
-    } else {
-      if( ! mysql_query(
-        "UPDATE bestellgruppen SET aktiv=0 WHERE id=".mysql_escape_string($HTTP_GET_VARS['gruppen_id'])
-      ) ) echo "<div class='warn'>Konnte Bestellgruppe nicht l&ouml;schen: " . mysql_error() . "</div>";
-    }
+  $kontostand = kontostand( $row['id'] );
+  if( abs($kontostand) > 0.005 ) {
+    ?>
+      <div class='warn'>Kontostand (<? echo $kontostand; ?> EUR) ist nicht null: L&ouml;schen nicht m&ouml;glich!</div>
+    <?
+  } elseif( $row['mitgliederzahl'] != 0 ) {
+    ?>
+      <div class='warn'>Mitgliederzahl ist nicht null: L&ouml;schen nicht m&ouml;glich (Sockelbetrag!)</div>
+      <div class='warn'>(bitte erst auf null setzen, um Sockelbetrag zu verbuchen!)</div>
+    <?
+  } else {
+    if( ! mysql_query(
+      "UPDATE bestellgruppen SET aktiv=0 WHERE id='$gruppen_id'"
+    ) ) echo "<div class='warn'>Konnte Bestellgruppe nicht l&ouml;schen: " . mysql_error() . "</div>";
   }
+}
 
-  echo "
-    <!-- Hier eine reload-Form die dazu dient, dieses Fenster von einem anderen aus reloaden zu können -->
-    <form action='index.php' name='reload_form' method='post'>
-      <input type='hidden' name='area' value='gruppen'>
-      <input type='hidden' name='action' value='normal'>
+  // Hier eine reload-Form die dazu dient, dieses Fenster von einem anderen aus reloaden zu können
+  ?>
+    <form action='<? echo self_url(); ?>' name='reload_form' method='post'>
+      <? echo self_post(); ?>
       <input type='hidden' name='gruppen_id' value=''>
+      <input type='hidden' name='action' value=''>
     </form>
     <table class='menu'>
-  "; 
+  <?
 
-  if( $hat_dienst_IV || $hat_dienst_V and ! $readonly ) {
-    echo "
+  if( $hat_dienst_V and ! $readonly ) {
+    ?>
       <tr>
         <td>
-          <input type='button' value='Neue Gruppe' class='bigbutton' onClick=\"window.open('windows/insertGroup.php','insertGroup','width=390,height=360,left=200,top=100').focus()\"></td>
-        <td valign='middle' class='smallfont'>Eine neue Bestellgruppe hinzufügen...</td>
+          <input type='button' value='Neue Gruppe' class='bigbutton'
+          onClick="window.open('index.php?window=insertGroup','insertGroup','width=390,height=360,left=200,top=100').focus();"></td>
+        <td valign='middle' class='smallfont'>Eine neue Bestellgruppe hinzufÃ¼gen...</td>
       </tr>
-    ";
+    <?
   }
 
-  echo "
+  ?>
     </table>
 
     <br><br>
@@ -71,10 +68,9 @@
          <th>Diensteinteilung</th>
          <th>Optionen</th>
       </tr>
-  ";
+  <?
 
-  $result = mysql_query("SELECT * FROM bestellgruppen WHERE aktiv=1 ORDER BY (id%1000)")
-    or error(__LINE__,__FILE__,"Konnte Bestellgruppen nicht lesen.",mysql_error());
+  $result = sql_gruppen();
   while ($row = mysql_fetch_array($result)) {
     $kontostand = sprintf( '%10.2lf', kontostand($row['id']) );
     $nr = $row['id'] % 1000;
@@ -99,31 +95,35 @@
           </a>
         ";
       } elseif( $login_gruppen_id == $row['id'] ) {
-        echo "
+        ?>
           <a class='png' style='padding:0pt 1ex 0pt 1ex;'  href='index.php?area=meinkonto'>
            <img src='img/b_browse.png' border='0' titel='Mein Konto' alt='Mein Konto'/>
           </a>
-        ";
+        <?
       }
       if( ( $dienst == 4 ) || ( $dienst == 5 ) || ( $login_gruppen_id == $row['id'] ) ) {
-        echo "<a class='png' style='padding:0pt 1ex 0pt 1ex;'  href=\"javascript:window.open('windows/editGroup.php?gruppen_id={$row['id']}','insertGroup','width=390,height=420,left=200,top=100').focus()\">
-          <img src='img/b_edit.png' border='0' alt='Gruppendaten ändern' titel='Gruppendaten ändern'/></a>
-        ";
+        ?>
+          <a class='png' style='padding:0pt 1ex 0pt 1ex;'
+          href="javascript:window.open('index.php?window=editGroup&gruppen_id=<? echo $row['id']; ?>','insertGroup','width=390,height=420,left=200,top=100').focus();">
+          <img src='img/b_edit.png' border='0' alt='Gruppendaten Ã¤ndern' titel='Gruppendaten Ã¤ndern'/></a>
+        <?
       }
       // loeschen nur wenn
       // - kontostand 0
       // - mitgliederzahl 0 (wegen rueckbuchung sockelbetrag!)
       if( ( $dienst == 5 ) && ( abs($kontostand) < 0.005 ) && ( $row['mitgliederzahl'] == 0 ) ) {
-        echo "<a class='png' href=\"javascript:deleteGroup({$row['id']});\">
-          <img src='img/b_drop.png' border='0' alt='Gruppe löschen' titel='Gruppe löschen'/></a>
-        ";
+        ?>
+          <a class='png' href="javascript:if(confirm('Soll die Gruppe wirklich GELÃ–SCHT werden?')){
+            document.forms['reload_form'].action.value='delete';
+            document.forms['reload_form'].gruppen_id.value='<? echo $row['id']; ?>';
+            document.forms['reload_form'].submit();">
+          <img src='img/b_drop.png' border='0' alt='Gruppe lÃ¶schen' title='Gruppe lÃ¶schen'/></a>
+        <?
       }
     }
-    echo " </td> </tr>";
+    ?> </td> </tr> <?
   }
 ?>
 
 </table>
-</body>
-</html>
 
