dc5202437187cc29e94418ba6f1776331266647c
Timo Felbinger <gittf@qipc.org>
2007-12-03 09:19:05 +0000

    Schwund-Abschreiben bearbeiten

--- www/basar.php
+++ www/basar.php
@@ -8,41 +8,56 @@ assert( $angemeldet ) or exit();  // aufruf nur per index.php?area=basar...
 get_http_var( 'orderby', 'w' , 'artikelname', true );
 get_http_var( 'bestell_id', 'u' , 0, true );
 
-// aktionen:
-//
-get_http_var('gruppe','u', false, false );
-if( $gruppe > 0 ) {
-  // glassrueckgabe bearbeiten:
-  //
-  get_http_var('menge_glas','u', false, false);
-  if( $menge_glas > 0 ) {
-    fail_if_readonly();
-    nur_fuer_dienst(4);
-    sql_groupGlass( $gruppe, $menge_glas );
-    echo "glasrueckgabe: $gruppe, $menge_glas";
-  }
+$editable = ( ! $readonly and ( $dienst == 4 ) );
 
-  // basarzuteilungen bearbeiten:
+if( $editable ) {
+
+  // aktionen:
   //
-  for( $i = 0; get_http_var( "produkt$i", 'u' ) ; $i++ ) {
-    need_http_var( "bestellung$i", 'u' );
-    if( get_http_var( "menge$i", "f" ) ) {
-//      if( ${"menge$i"} > 0 ) {
-        fail_if_readonly();
-        nur_fuer_dienst(4);
-        $pr = sql_bestellvorschlag_daten( ${"bestellung$i"}, ${"produkt$i"} );
-        kanonische_einheit( $pr['verteileinheit'], &$kan_verteileinheit, &$kan_verteilmult );
-        $gruppen_menge = ${"menge$i"} / $kan_verteilmult;
-        sql_basar2group($gruppe, ${"produkt$i"}, ${"bestellung$i"}, $gruppen_menge);
-//      }
+  get_http_var('gruppe','u', false, false );
+  if( $gruppe > 0 ) {
+    // glassrueckgabe bearbeiten:
+    //
+    get_http_var('menge_glas','u', false, false);
+    if( $menge_glas > 0 ) {
+      fail_if_readonly();
+      nur_fuer_dienst(4);
+      sql_groupGlass( $gruppe, $menge_glas );
+      // echo "glasrueckgabe: $gruppe, $menge_glas";
     }
+
+    // basarzuteilungen bearbeiten:
+    //
+    for( $i = 0; get_http_var( "produkt$i", 'u' ) ; $i++ ) {
+      need_http_var( "bestellung$i", 'u' );
+      if( get_http_var( "menge$i", "f" ) ) {
+  //      if( ${"menge$i"} > 0 ) {
+          fail_if_readonly();
+          nur_fuer_dienst(4);
+          $pr = sql_bestellvorschlag_daten( ${"bestellung$i"}, ${"produkt$i"} );
+          kanonische_einheit( $pr['verteileinheit'], &$kan_verteileinheit, &$kan_verteilmult );
+          $gruppen_menge = ${"menge$i"} / $kan_verteilmult;
+          sql_basar2group($gruppe, ${"produkt$i"}, ${"bestellung$i"}, $gruppen_menge);
+  //      }
+      }
+    }
+  }
+  get_http_var( 'action','w','' );
+  if( $action == 'schwund' ) {
+    need_http_var( 'produkt_id', 'u' );
+    need_http_var( 'bestellung', 'u' );
+    need_http_var( 'menge', 'f' );
+    need( $muell_id );
+    echo "Schwundbuchung: $produkt_id, $bestellung, $menge<br>";
+    sql_basar2group( $muell_id, $produkt_id, $bestellung, $menge );
   }
 }
 
+
 ?> <h1>Basar</h1> <?
 
-basar_overview( $bestell_id, $orderby, $dienst > 0 );
-  
+basar_overview( $bestell_id, $orderby, $editable );
+
 ?>
    <form action='index.php' method='post'>
      <input type='submit' value='Zur&uuml;ck '>
