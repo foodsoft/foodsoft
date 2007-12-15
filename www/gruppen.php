4c15ca400d9f0347d6b0d252e58a1f524b0ec9dd
Timo Felbinger <gittf@qipc.org>
2007-12-15 16:28:27 +0000

    inaktive Gruppen anzeigbar machen (fuer Inventur: Konto immer null?)

--- www/gruppen.php
+++ www/gruppen.php
@@ -3,32 +3,18 @@
 <?PHP
   
 assert( $angemeldet ) or exit();
+$problems="";
+$msg="";
+
+
+define( 'OPTION_INAKTIV', 1 );
+get_http_var( 'optionen', 'u', 0, true );
 
 // ggf. Aktionen durchführen (z.B. Gruppe löschen...)
-if(get_http_var('action','w')){
-   if( $action == 'delete' ) {
-     fail_if_readonly();
-     nur_fuer_dienst(5);
-     need_http_var('gruppen_id','u');
-   
-     $row = sql_gruppendaten( $gruppen_id );
-   
-     $kontostand = kontostand( $row['id'] );
-     if( abs($kontostand) > 0.005 ) {
-       ?>
-         <div class='warn'>Kontostand (<? echo $kontostand; ?> EUR) ist nicht null: L&ouml;schen nicht m&ouml;glich!</div>
-       <?
-     } elseif( $row['mitgliederzahl'] != 0 ) {
-       ?>
-         <div class='warn'>Mitgliederzahl ist nicht null: L&ouml;schen nicht m&ouml;glich (Sockelbetrag!)</div>
-         <div class='warn'>(bitte erst auf null setzen, um Sockelbetrag zu verbuchen!)</div>
-       <?
-     } else {
-       $sql = "UPDATE bestellgruppen SET aktiv=0 WHERE id='$gruppen_id'";
-   	doSql($sql, LEVEL_IMPORTANT, "Konnte Bestellgruppe nicht l&ouml;schen");
-     }
-   }
-}
+get_http_var('action','w','');
+$readonly and $action = '';
+
+
   // Hier eine reload-Form die dazu dient, dieses Fenster von einem anderen aus reloaden zu können
   ?>
     <form action='<? echo self_url(); ?>' name='reload_form' method='post'>
@@ -36,6 +22,25 @@ if(get_http_var('action','w')){
       <input type='hidden' name='gruppen_id' value=''>
       <input type='hidden' name='action' value=''>
     </form>
+    <div style='padding-bottom:2em;'>
+    <table class='menu' style='padding-bottom:2em;'>
+      <tr>
+        <th>Optionen</th>
+      </tr>
+      <tr>
+        <td>
+          <input type='checkbox'
+            <? if( $optionen & OPTION_INAKTIV ) echo " checked"; ?>
+            onclick="window.location.href='<?
+              echo self_url('optionen'), "&optionen=", ($optionen ^ OPTION_INAKTIV);
+            ?>';"
+            title='Auch inaktive/gelÃ¶schte Gruppen anzeigen?'
+          > inaktive Gruppen zeigen
+        </td>
+      </tr>
+    </table>
+    </div>
+
   <?
 
   if( $hat_dienst_V and ! $readonly ) {
@@ -50,6 +55,7 @@ if(get_http_var('action','w')){
     <div id='transaction_form' style='display:none;'>
       <form method='post' class='small_form' action='<? echo self_url(); ?>'>
       <? echo self_post(); ?>
+      <input type='hidden' name='action' value='insert'>
       <fieldset class='small_form'>
       <legend>
         <img src='img/close_black_trans.gif' class='button'
@@ -65,10 +71,30 @@ if(get_http_var('action','w')){
     </div>
 
     <?
-	  if(get_http_var('newNumber', 'u')){
-		  $problems="";
-		  $msg="";
-		  get_http_var('newName');
+	
+    if( $action == 'delete' ) {
+      nur_fuer_dienst(5);
+      need_http_var('gruppen_id','u');
+    
+      $row = sql_gruppendaten( $gruppen_id );
+    
+      $kontostand = kontostand( $row['id'] );
+      if( abs($kontostand) > 0.005 ) {
+        ?>
+          <div class='warn'>Kontostand (<? echo $kontostand; ?> EUR) ist nicht null: L&ouml;schen nicht m&ouml;glich!</div>
+        <?
+      } elseif( $row['mitgliederzahl'] != 0 ) {
+        ?>
+          <div class='warn'>Mitgliederzahl ist nicht null: L&ouml;schen nicht m&ouml;glich (Sockelbetrag!)</div>
+          <div class='warn'>(bitte erst auf null setzen, um Sockelbetrag zu verbuchen!)</div>
+        <?
+      } else {
+        sql_update( 'bestellgruppen', $gruppen_id, array( 'aktiv' => 0 ) );
+      }
+   }
+    if( $action == 'insert' ) {
+	    need_http_var('newNumber', 'u');
+		  need_http_var('newName','H');
 		      // vorläufiges Passwort für die Bestellgruppe erzeugen...
 		      $pwd = strval(rand(1010,9999));
 
@@ -80,7 +106,6 @@ if(get_http_var('action','w')){
 			  <div class='ok'>Vorl&auml;ufiges Passwort: <b>$pwd</b> (bitte notieren!)</div>
 			";
 		      }
-              echo $problems; echo $msg; 
 	  }
 
   }
@@ -88,11 +113,12 @@ if(get_http_var('action','w')){
 // Hier ändern. Code in views verschieben, details in editGroup verschieben
    $show_member_details=FALSE;
 
+  echo $problems; echo $msg; 
+
   ?>
 
  
  
-
     <br><br>
 
     <table class='liste'>
@@ -113,13 +139,13 @@ if(get_http_var('action','w')){
       </tr>
   <?
 
-  $result = sql_gruppen();
+  $result = sql_bestellgruppen(0, ! ( $optionen & OPTION_INAKTIV ) );
   while ($row = mysql_fetch_array($result)) {
     $id = $row['id'];
     if( ( $dienst == 4 ) || ( $dienst == 5 ) || ( $login_gruppen_id == $id ) ) {
       $kontostand = sprintf( '%10.2lf', kontostand($row['id']) );
     }
-    $nr = $id % 1000;
+    $nr = $row['gruppennummer'];
     echo "
       <tr>
         <td>$nr</td>
@@ -132,53 +158,59 @@ if(get_http_var('action','w')){
         echo "<td></td>";
 	}
     echo"
-        <td>{$row['mitgliederzahl']}</td>
-        <td>
-          <a class='png' style='padding:0pt 1ex 0pt 1ex;'
-            href=\"javascript:neuesfenster('index.php?window=gruppen_mitglieder&gruppen_id=$id','gruppenmitglieder');\">
-           <img src='img/b_browse.png' border='0' title='Personen' alt='Personen'/>
-          </a>
+      <td>{$row['mitgliederzahl']}</td>
+      <td>
     ";
-    if( ! $readonly ) {
-      if( ( $dienst == 4 ) || ( $dienst == 5 ) ) {
-        echo "
-          <a class='png' style='padding:0pt 1ex 0pt 1ex;'
-            href=\"javascript:neuesfenster('index.php?window=showGroupTransaktions&gruppen_id={$row['id']}','kontoblatt');\">
-           <img src='img/chart.png' border='0' title='Kontotransaktionen' alt='Kontotransaktionen'/>
-          </a>
-        ";
-      } elseif( $login_gruppen_id == $id ) {
-        ?>
-          <a class='png' style='padding:0pt 1ex 0pt 1ex;'  href='index.php?area=meinkonto'>
-           <img src='img/chart.png' border='0' title='Mein Konto' alt='Mein Konto'/>
-          </a>
-        <?
+    if( $row['aktiv'] > 0 ) {
+      echo"
+            <a class='png' style='padding:0pt 1ex 0pt 1ex;'
+              href=\"javascript:neuesfenster('index.php?window=gruppen_mitglieder&gruppen_id=$id','gruppenmitglieder');\">
+             <img src='img/b_browse.png' border='0' title='Personen' alt='Personen'/>
+            </a>
+      ";
+      if( ! $readonly ) {
+        if( ( $dienst == 4 ) || ( $dienst == 5 ) ) {
+          echo "
+            <a class='png' style='padding:0pt 1ex 0pt 1ex;'
+              href=\"javascript:neuesfenster('index.php?window=showGroupTransaktions&gruppen_id={$row['id']}','kontoblatt');\">
+             <img src='img/chart.png' border='0' title='Kontotransaktionen' alt='Kontotransaktionen'/>
+            </a>
+          ";
+        } elseif( $login_gruppen_id == $id ) {
+          ?>
+            <a class='png' style='padding:0pt 1ex 0pt 1ex;'  href='index.php?area=meinkonto'>
+             <img src='img/chart.png' border='0' title='Mein Konto' alt='Mein Konto'/>
+            </a>
+          <?
+        }
+        if( ( $dienst == 4 ) || ( $dienst == 5 ) || ( $login_gruppen_id == $id ) ) {
+          ?>
+       <!-- nicht mehr da, da Formular veraltet ToDo: Gruppennamen hier editierbar machen
+            <a class='png' style='padding:0pt 1ex 0pt 1ex;'
+            href="javascript:window.open('index.php?window=editGroup&gruppen_id=<?// echo $row['id']; ?>','insertGroup','width=390,height=420,left=200,top=100').focus();">
+            <img src='img/b_edit.png' border='0' alt='Gruppendaten Ã¤ndern' title='Gruppendaten Ã¤ndern'/></a>
+        -->
+          <?
+        }
+        // loeschen nur wenn
+        // - kontostand 0
+        // - mitgliederzahl 0 (wegen rueckbuchung sockelbetrag!)
+        if(    ( $dienst == 5 )
+            && ( abs($kontostand) < 0.005 )
+            && ( $row['mitgliederzahl'] == 0 )
+            && ( ! in_array( $id, $specialgroups ) )
+        ) {
+          ?>
+            <a class='png' href="javascript:if(confirm('Soll die Gruppe wirklich GELÃ–SCHT werden?')){
+              document.forms['reload_form'].action.value='delete';
+              document.forms['reload_form'].gruppen_id.value='<? echo $row['id']; ?>';
+              document.forms['reload_form'].submit();}">
+            <img src='img/b_drop.png' border='0' alt='Gruppe lÃ¶schen' title='Gruppe lÃ¶schen'/></a>
+          <?
+        }
       }
-      if( ( $dienst == 4 ) || ( $dienst == 5 ) || ( $login_gruppen_id == $id ) ) {
-        ?>
-     <!-- nicht mehr da, da Formular veraltet ToDo: Gruppennamen hier editierbar machen
-          <a class='png' style='padding:0pt 1ex 0pt 1ex;'
-          href="javascript:window.open('index.php?window=editGroup&gruppen_id=<?// echo $row['id']; ?>','insertGroup','width=390,height=420,left=200,top=100').focus();">
-          <img src='img/b_edit.png' border='0' alt='Gruppendaten Ã¤ndern' title='Gruppendaten Ã¤ndern'/></a>
-      -->
-        <?
-      }
-      // loeschen nur wenn
-      // - kontostand 0
-      // - mitgliederzahl 0 (wegen rueckbuchung sockelbetrag!)
-      if(    ( $dienst == 5 )
-          && ( abs($kontostand) < 0.005 )
-          && ( $row['mitgliederzahl'] == 0 )
-          && ( ! in_array( $id, $specialgroups ) )
-      ) {
-        ?>
-          <a class='png' href="javascript:if(confirm('Soll die Gruppe wirklich GELÃ–SCHT werden?')){
-            document.forms['reload_form'].action.value='delete';
-            document.forms['reload_form'].gruppen_id.value='<? echo $row['id']; ?>';
-            document.forms['reload_form'].submit();}">
-          <img src='img/b_drop.png' border='0' alt='Gruppe lÃ¶schen' title='Gruppe lÃ¶schen'/></a>
-        <?
-      }
+    } else {
+      ?>(inaktiv)<?
     }
     ?> </td> </tr> <?
 
