cd606aa77476dd9d2a23dd5a22da9ad3cc91e78f
Timo Felbinger <gittf@qipc.org>
2008-06-26 15:14:27 +0000

    Formular: editBuchung (bisher readonly...)

--- www/windows/editBuchung.php
+++ www/windows/editBuchung.php
@@ -1,90 +1,104 @@
-<?PHP
+<?
 
-  assert( $angemeldet ) or exit();
+assert( $angemeldet ) or exit();
 
-  setWindowSubtitle( 'Buchung edieren' );
-  setWikiHelpTopic( 'foodsoft:buchung_edieren' );
+setWindowSubtitle( 'Buchung edieren' );
+setWikiHelpTopic( 'foodsoft:buchung_edieren' );
 
-  nur_fuer_dienst_IV();
+nur_fuer_dienst_IV();
   // fail_if_readonly();
-  $editable = ( $dienst == 4 and ! $readonly );
+$editable = ( $dienst == 4 and ! $readonly );
 
+$msg = '';
+$problems = '';
+
+if( get_http_var( 'transaktion_id', 'u', NULL, true ) )
+  $buchung_id = -$transaktion_id;
+else
   need_http_var( 'buchung_id','u', true );
 
-  $buchung = sql_get_transaction( $buchung_id ); 
-  $k_id = $buchung['konterbuchung_id'];
-  if( $k_id )
-    $k_buchung = sql_get_transaction( $k_id );
+$buchung = sql_get_transaction( $buchung_id ); 
+$k_id = $buchung['konterbuchung_id'];
 
 
 
-  get_http_var('action','w','');
-  $editable or $action = '';
-
-  if( $action == 'update' ) {
-    need_http_var("id",'u');
-    
-      need_http_var("startzeit_monat",'u');
-    need_http_var("startzeit_jahr",'u');
-    need_http_var("startzeit_stunde",'u');
-    need_http_var("startzeit_minute",'u');
-    need_http_var("endzeit_tag",'u');
-    need_http_var("endzeit_monat",'u');
-    need_http_var("endzeit_jahr",'u');
-    need_http_var("endzeit_stunde",'u');
-    need_http_var("endzeit_minute",'u');
-    need_http_var("lieferung_tag",'u');
-    need_http_var("lieferung_monat",'u');
-    need_http_var("lieferung_jahr",'u');
-    need_http_var("bestellname",'H');
-
-    $startzeit = "$startzeit_jahr-$startzeit_monat-$startzeit_tag $startzeit_stunde:$startzeit_minute:00";
-    $endzeit = "$endzeit_jahr-$endzeit_monat-$endzeit_tag $endzeit_stunde:$endzeit_minute:00";
-    $lieferung = "$lieferung_jahr-$lieferung_monat-$lieferung_tag";
-
-    if( $bestellname == "" )
-      $problems  .= "<div class='warn'>Die Bestellung mu� einen Namen bekommen!</div>";
-
-    if( $problems == '' ) {
-      if( sql_update_bestellung( $bestellname, $startzeit, $endzeit, $lieferung, $bestell_id ) ) {
-        $done = true;
-        $msg .= "<div class='ok'>Änderungen gespeichert!</div>";
-      } else {
-        $problems .= "<div class='warn'>Änderung fehlgeschlagen!</div>";
-      }
+function show_transaction( $id ) {
+  $t = sql_get_transaction( $id );
+  if( $id > 0 ) {
+    ?>
+      <tr>
+        <th colspan='2'>Bank-Transaktion</th>
+      </tr>
+      <tr>
+        <td><label>Konto:</label></td><td><? echo $t['kontoname']; ?></td>
+      </tr>
+      <tr>
+        <td><label>Auszug:</label></td><td><? echo "{$t['kontoauszug_nr']} / {$t['kontoauszug_jahr']}"; ?></td>
+      </tr>
+      <tr>
+        <td><label>Buchung:</label></td><td><? echo $t['buchungsdatum']; ?></td>
+      </tr>
+      <tr>
+        <td><label>Valuta:</label></td><td><? echo $t['valuta']; ?></td>
+      </tr>
+      <tr>
+        <td><label>Notiz:</label></td><td><? echo $t['kommentar']; ?></td>
+      </tr>
+      <tr>
+        <td><label>Betrag:</label></td><td><? printf( "%.2lf", $t['haben'] ); ?></td>
+      </tr>
+    <?
+  } else {
+    $gruppen_id = $t['gruppen_id'];
+    $lieferanten_id = $t['lieferanten_id'];
+    if( $lieferanten_id > 0 ) {
+      ?>
+        <tr>
+          <th colspan='2'>Lieferanten-Transaktion</th>
+        </tr>
+        <tr>
+          <td><label>Lieferant:</label></td><td><? printf( "%s", lieferant_name( $lieferanten_id ) ); ?></td>
+        </tr>
+      <?
+    } else {
+      ?>
+        <tr>
+          <th colspan='2'>Gruppen-Transaktion</th>
+        </tr>
+        <tr>
+          <td><label>Gruppe:</label></td><td><? printf( "%s (%s)", sql_gruppenname( $gruppen_id ), $gruppen_id ); ?></td>
+        </tr>
+      <?
     }
+      ?>
+      <tr>
+        <td><label>Buchung:</label></td><td><? echo $t['buchungsdatum']; ?></td>
+      </tr>
+      <tr>
+        <td><label>Valuta:</label></td><td><? echo $t['valuta']; ?></td>
+      </tr>
+      <tr>
+        <td><label>Notiz:</label></td><td><? echo $t['kommentar']; ?></td>
+      </tr>
+      <tr>
+        <td><label>Betrag:</label></td><td><? printf( "%.2lf", $t['haben'] ); ?></td>
+      </tr>
+    <?
   }
+}
+
 
 ?>
 <form action='<? echo self_url(); ?>' method='post' class='small_form'>
   <? echo self_post(); ?>
   <input type='hidden' name='action' value='update'>
-  <fieldset style='width:360px;' class='small_form'>
-    <legend>Bestellvorlage edieren</legend>
+  <fieldset style='width:450px;' class='small_form'>
+    <legend>Buchung:</legend>
     <? echo $msg; echo $problems; ?>
     <table style="width:350px;">
-      <tr>
-        <td><label>Name:</label></td>
-        <td><input type="text" name="bestellname" size="35" value="<? echo "$bestellname"; ?>"></td>
-      </tr>
-      <tr>
-        <td valign="top"><label>Startzeit:</label></td>
-        <td>
-          <?date_time_selector($startzeit,"startzeit");?>
-        </td>
-      </tr>
-      <tr>
-        <td valign="top"><label>Ende:</label></td>
-        <td>
-          <?date_time_selector($endzeit,"endzeit");?>
-        </td>
-      </tr>
-      <tr>
-        <td valign="top"><label>Lieferung:</label></td>
-        <td>
-          <?date_time_selector($lieferung,"lieferung",false);?>
-        </td>
-      </tr>
+      <? show_transaction( $buchung_id ); ?>
+      <tr><td colspan='2'></td></tr>
+      <? show_transaction( $k_id ); ?>
       <tr>
         <td colspan='2' align='center'><input type='submit' value='&Auml;ndern'></td>
       </tr>
