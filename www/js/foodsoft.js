6b73e84552a83376e03ab29a63e112ab7fb04b6a
Timo Felbinger <gittf@qipc.org>
2008-07-08 17:00:22 +0000

    - Fensterverwaltung - Umlaute korrigiert

--- www/js/foodsoft.js
+++ www/js/foodsoft.js
@@ -2,62 +2,60 @@
 //  das  javascript der foodsoft  
 // copyright Fc Schinke09 2006 
 
-			
-	    function deleteLieferant(lieferantID)
-			{
- 	       if (confirm('Soll der Lieferant wirklich GEL�SCHT werden?')) { 
-				    document.forms['reload_form'].action.value="delete";
-						document.forms['reload_form'].lieferanten_id.value=lieferantID;
-						document.forms['reload_form'].submit();
-				 }
-			}			
-			
-	    function deleteProdukt(produktID)
-			{
- 	       if (confirm('Soll das Produkt wirklich GELÖSCHT werden?')) { 
-				    document.forms['reload_form'].action.value="delete";
-						document.forms['reload_form'].produkt_id.value=produktID;
-						document.forms['reload_form'].submit();
-				 }
-			}	
-	    function deleteProduktpreis(id)
-			{
-			   document.forms['reload_form'].action.value="delete_price";
-			   document.forms['reload_form'].preis_id.value=id;
-				 document.forms['reload_form'].submit();
-			}	
-			
-					function checkAll(thisForm,elmnt,tf,x) {
-						var o = document.forms[thisForm].elements
-						if (o){
-							for (i=0; i<o.length; i++){
-								if (elmnt != ''){
-									if ((o[i].type == 'checkbox')&&(o[i].name.indexOf(elmnt+"") != -1)){
-										o[i].checked = tf
-									}
-								}
-								else {
-									if (o[i].type == 'checkbox'){
-										o[i].checked = tf
-									}
-								}			
-							}
-						}	
-						for (var j = 0; j < document.links.length; j++){
-							if ((document.links[j].href.indexOf(thisForm) != -1) && (document.links[j].href.indexOf('checkAll') != -1)){
-								if (tf == true){
-									document.links[j].href = "javascript:checkAll('"+thisForm+"','"+elmnt+"',false)";
-									//document.links[j].innerText = "- all";
-								}
-								else {
-									document.links[j].href = "javascript:checkAll('"+thisForm+"','"+elmnt+"',true)";
-									//document.links[j].innerText = "+ all";
-								}
-							}
-						}
-					}
 
-// neuesfenster: neues (grosses) Fenster oeffnen: fuer Lieferschein, Produktpreisdetails, ...
+function deleteLieferant(lieferantID) {
+  if (confirm('Soll der Lieferant wirklich GELÖSCHT werden?')) { 
+    document.forms['reload_form'].action.value="delete";
+    document.forms['reload_form'].lieferanten_id.value=lieferantID;
+    document.forms['reload_form'].submit();
+ }
+}
+function deleteProdukt(produktID)
+{
+    if (confirm('Soll das Produkt wirklich GELÖSCHT werden?')) { 
+      document.forms['reload_form'].action.value="delete";
+      document.forms['reload_form'].produkt_id.value=produktID;
+      document.forms['reload_form'].submit();
+   }
+}	
+function deleteProduktpreis(id)
+{
+   document.forms['reload_form'].action.value="delete_price";
+   document.forms['reload_form'].preis_id.value=id;
+   document.forms['reload_form'].submit();
+}	
+
+function checkAll(thisForm,elmnt,tf,x) {
+  var o = document.forms[thisForm].elements
+  if (o){
+    for (i=0; i<o.length; i++){
+      if (elmnt != ''){
+        if ((o[i].type == 'checkbox')&&(o[i].name.indexOf(elmnt+"") != -1)){
+          o[i].checked = tf
+        }
+      }
+      else {
+        if (o[i].type == 'checkbox'){
+          o[i].checked = tf
+        }
+      }			
+    }
+  }	
+  for (var j = 0; j < document.links.length; j++){
+    if ((document.links[j].href.indexOf(thisForm) != -1) && (document.links[j].href.indexOf('checkAll') != -1)){
+      if (tf == true){
+        document.links[j].href = "javascript:checkAll('"+thisForm+"','"+elmnt+"',false)";
+        //document.links[j].innerText = "- all";
+      }
+      else {
+        document.links[j].href = "javascript:checkAll('"+thisForm+"','"+elmnt+"',true)";
+        //document.links[j].innerText = "+ all";
+      }
+    }
+  }
+}
+
+// neuesfenster: neues (grosses) Fenster oeffnen (fuer wiki)
 //
 function neuesfenster(url,name) {
   f=window.open(url,name,"dependent=yes,toolbar=yes,menubar=yes,location=yes,resizable=yes,scrollbars=yes");
@@ -79,13 +77,11 @@ function select_group(self) {
   s = document.getElementById('select_group').options[i].value;
   window.location.href = self + '&gruppen_id=' + s;
 }
-
 function select_lieferant(self) {
   i = document.getElementById('select_lieferant').selectedIndex;
   s = document.getElementById('select_lieferant').options[i].value;
   window.location.href = self + '&lieferanten_id=' + s;
 }
-
 function select_auszug(self) {
   i = document.getElementById('select_auszug').selectedIndex;
   s = document.getElementById('select_auszug').options[i].value;
@@ -94,7 +90,7 @@ function select_auszug(self) {
 
 function closeCurrentWindow() {
   // this function is a workaround for the " 'window.close()' is not a function" -bug
-  // (occurring in  some uses of onClick='window.close();'):
+  // (occurring in some uses of onClick='window.close();'):
   window.close();
 }
 
