7fc735392ac1e2d5d84c594cb5d291fcae67359f
Timo Felbinger <gittf@qipc.org>
2008-09-15 09:55:04 +0000

    getenv statt apache_getenv benutzen

--- www/code/config.php
+++ www/code/config.php
@@ -5,10 +5,10 @@
 // the following lines assume that the information is set in the web server configuraton:
 // (see apache.sample.conf for how to do this):
 //
-$db_server =  apache_getenv( 'mysql_db_server' );   // server address: hostname or IP number
-$db_name   =  apache_getenv( 'mysql_db_name' );     // name of MySQL database
-$db_user   =  apache_getenv( 'mysql_db_user' );     // user to connect as
-$db_pwd    =  apache_getenv( 'mysql_db_password' ); // password to authenticate with
+$db_server =  getenv( 'mysql_db_server' );   // server address: hostname or IP number
+$db_name   =  getenv( 'mysql_db_name' );     // name of MySQL database
+$db_user   =  getenv( 'mysql_db_user' );     // user to connect as
+$db_pwd    =  getenv( 'mysql_db_password' ); // password to authenticate with
 
 // alternatively, store the information in the following lines:
 //
