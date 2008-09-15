da208cec8ac0055fa2ae2eec29301ea7f6ee15c6
Timo Felbinger <gittf@qipc.org>
2008-09-15 11:48:21 +0000

    head.php und windows/head.php aufgeraeumt, code vereinfacht

--- www/head.php
+++ www/head.php
@@ -2,75 +2,69 @@
   global $angemeldet, $login_gruppen_name, $coopie_name, $dienst
        , $readonly, $kopf_schon_ausgegeben, $print_on_exit, $foodsoftdir;
 
-  if( ! $kopf_schon_ausgegeben ) {
-    echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
-      <html>
-      <head>
-        <title>FC Potsdam  - Foodsoft</title>
-        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' >
-        <link rel='stylesheet' type='text/css' href='$foodsoftdir/css/foodsoft.css'>
-        <script type='text/javascript' src='$foodsoftdir/js/foodsoft.js' language='javascript'></script>	 
-      </head>
-      <body
-    ";
-    if( $readonly ) {
-      echo " class='ro'";
-    }
-    ?>
-      >
-      <table width='100%'
-    <?
-    if( $readonly ) {
-      echo " class='headro'>";
-    } else {
-      echo " class='head'>";
-    }
-    echo "
-      <tr>
-      <td class='logo'><a class='logo' href='index.php'><span class='logoinvers'>FC</span>Nahrungskette... Foodsoft</a></td>
-      <td class='head' style='padding-top:1em;'>
-    ";
-    if( $angemeldet ) {
-      if( $dienst > 0 ) {
-        echo "Hallo $coopie_name ($login_gruppen_name) vom Dienst $dienst!";
-      } else {
-        echo "Hallo Gruppe $login_gruppen_name!";
-      }
-    }
-    echo "</td><td class='head' style='text-align:right;padding-top:1em;'>";
-    if( $angemeldet ) {
-      if( $dienst > 0 ) {
-        // fuer dienste: noch dienstkontrollblatteintrag aktualisieren:
-        echo "<a class='button' href='index.php?window=dienstkontrollblatt&action=abmelden'>Abmelden</a>";
-      } else {
-        echo "<a class='button' href='index.php?action=logout'>Abmelden</a>";
-      }
-    } else {
-      echo "(nicht angemeldet)";
-    }
-    echo "
-        </td>
-      </tr>
-     </table>
-  
-     <ul id='menu' style='margin-bottom:1em;'>";
-      foreach(possible_areas() as $menu_area){
-        areas_in_head($menu_area);
-      }
-     echo "<li>";
+if( isset( $kopf_schon_ausgegeben ) && $kopf_schon_ausgegeben )
+  return;
 
-     wikiLink( isset($window) ? "foodsoft:$window" : "", "Hilfe-Wiki", true );
+if( $readonly ) {
+  $bodyclass='ro';
+  $headclass='headro';
+  $payloadclass='payloadro';
+} else {
+  $bodyclass='';
+  $headclass='head';
+  $payloadclass='payload';
+}
 
-     echo " </li>
-     </ul>
-<div id='payload'
-    ";
-    if($readonly) {
-      echo " class='payloadro'>";
-    } else {
-      echo " class='payload'>";
-    }
-    $print_on_exit = "</div></body></html>";
-    $kopf_schon_ausgegeben = true;
+?><!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
+<html>
+<head>
+  <title>FC Potsdam  - Foodsoft</title>
+  <meta http-equiv='Content-Type' content='text/html; charset=utf-8' >
+  <link rel='stylesheet' type='text/css' href='<? echo $foodsoftdir; ?>/css/foodsoft.css'>
+  <script type='text/javascript' src='<? echo $foodsoftdir; ?>/js/foodsoft.js' language='javascript'></script>	 
+</head>
+<body class='<? echo $bodyclass; ?>'>
+<table width='100%' class='<? echo $headclass; ?>'>
+  <tr>
+    <td class='logo'><a class='logo' href='index.php'><span class='logoinvers'>FC</span>Nahrungskette... Foodsoft</a></td>
+    <td class='head' style='padding-top:1em;'>
+      <?
+        if( $angemeldet ) {
+          if( $dienst > 0 ) {
+            echo "Hallo $coopie_name ($login_gruppen_name) vom Dienst $dienst!";
+          } else {
+            echo "Hallo Gruppe $login_gruppen_name!";
+          }
+        }
+      ?>
+    </td>
+    <td class='head' style='text-align:right;padding-top:1em;'>
+      <?
+         if( $angemeldet ) {
+           if( $dienst > 0 ) {
+             // fuer dienste: noch dienstkontrollblatteintrag aktualisieren:
+             echo "<a class='button' href='index.php?window=dienstkontrollblatt&action=abmelden'>Abmelden</a>";
+           } else {
+             echo "<a class='button' href='index.php?login=logout'>Abmelden</a>";
+           }
+         } else {
+           echo "(nicht angemeldet)";
+         }
+      ?>
+    </td>
+  </tr>
+</table>
+<ul id='menu' style='margin-bottom:1em;'>
+<?
+  foreach(possible_areas() as $menu_area){
+    areas_in_head($menu_area);
   }
 ?>
+  <li><? wikiLink( isset($window) ? "foodsoft:$window" : "", "Hilfe-Wiki", true ); ?></li>
+</ul>
+<div id='payload' class='<? echo $payloadclass; ?>'>
+<?
+$print_on_exit = "</div></body></html>";
+$kopf_schon_ausgegeben = true;
+?>
+
--- www/windows/head.php
+++ www/windows/head.php
@@ -10,58 +10,64 @@
        , $kopf_schon_ausgegeben, $print_on_exit
        , $foodsoftpath, $area;
 
-  if( ! $kopf_schon_ausgegeben ) {
-    if( ! $title ) $title = "FC Nahrungskette - Foodsoft";
-    if( ! $subtitle ) $subtitle = "FC Nahrungskette - Foodsoft";
-    $img = "$foodsoftdir/img/close_black_trans.gif";
-  
-    if( $readonly ) {
-      $ro='ro';
-    } else {
-      $ro='';
-    }
-    echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
-      <html>
-      <head>
-        <title id='title'>$title</title>
-        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' >
-        <link rel='stylesheet' type='text/css' href='$foodsoftdir/css/foodsoft.css'>
-        <script type='text/javascript' src='$foodsoftdir/js/foodsoft.js' language='javascript'></script>	 
-      </head>
-      <body onload='$onload_str' class='$ro'>
-      <div style='padding:0.5ex 1em 0.5ex 1ex;margin:0pt 0pt 1em 0pt;' class='head$ro'>
-      <table width='100%' class='head$ro'>
+if( isset( $kopf_schon_ausgegeben ) && $kopf_schon_ausgegeben )
+  return;
+
+if( ! $title ) $title = "FC Nahrungskette - Foodsoft";
+if( ! $subtitle ) $subtitle = "FC Nahrungskette - Foodsoft";
+$img = "$foodsoftdir/img/close_black_trans.gif";
+
+if( $readonly ) {
+  $bodyclass='ro';
+  $headclass='headro';
+  $payloadclass='payloadro';
+} else {
+  $bodyclass='';
+  $headclass='head';
+  $payloadclass='payload';
+}
+?><!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
+<html>
+<head>
+  <title id='title'><? echo $title; ?></title>
+  <meta http-equiv='Content-Type' content='text/html; charset=utf-8' >
+  <link rel='stylesheet' type='text/css' href='<? echo $foodsoftdir; ?>/css/foodsoft.css'>
+  <script type='text/javascript' src='<? echo $foodsoftdir; ?>/js/foodsoft.js' language='javascript'></script>	 
+</head>
+<body class='<? echo $bodyclass; ?>'>
+  <div style='padding:0.5ex 1em 0.5ex 1ex;margin:0pt 0pt 1em 0pt;' class='<? echo $headclass; ?>'>
+    <table width='100%' class='<? echo $headclass; ?>'>
       <tr>
-        <td style='padding-right:0.5ex;' class='head$ro'>
-          <img src='$img' class='button' alt='Schlie&szlig;en' title='Schlie&szlig;en'
+        <td style='padding-right:0.5ex;' class='<? echo $headclass; ?>'>
+          <img src='<? echo $img; ?>' class='button' alt='Schlie&szlig;en' title='Schlie&szlig;en'
            width='15' onClick='if(opener) opener.focus(); window.close();'></img>
         </td>
-        <td id='subtitle'>Foodsoft: $subtitle</td>
+        <td id='subtitle'>Foodsoft: <? echo $subtitle; ?></td>
         <td>
-    ";
-    wikiLink( ( $area ? "foodsoft:$area" : 'start' ) , "Hilfe-Wiki...", true );
-    echo "
+          <? wikiLink( ( $area ? "foodsoft:$area" : 'start' ) , "Hilfe-Wiki...", true ); ?>
         </td>
       </tr>
       <tr>
-      <td>&nbsp;</td>
-      <td style='font-size:11pt;'>
-    ";
-    if( $angemeldet ) {
-      if( $dienst > 0 ) {
-        echo "$coopie_name ($login_gruppen_name) / Dienst $dienst";
-      } else {
-        echo "angemeldet: $login_gruppen_name";
-      }
-    }
-    if( $readonly ) {
-      echo "<span style='padding-left:3em;'>schreibgeschuetzt!</span>";
-    }
-    echo "
-      </td></tr>
-      </table></div><div class='payload$ro'>
-    ";
-    $print_on_exit = "</div></body></html>";
-    $kopf_schon_ausgegeben = true;
-  }
+        <td>&nbsp;</td>
+        <td style='font-size:11pt;'>
+          <?
+            if( $angemeldet ) {
+              if( $dienst > 0 ) {
+                echo "$coopie_name ($login_gruppen_name) / Dienst $dienst";
+              } else {
+                echo "angemeldet: $login_gruppen_name";
+              }
+            }
+            if( $readonly ) {
+              echo "<span style='padding-left:3em;'>schreibgeschuetzt!</span>";
+            }
+          ?>
+        </td>
+      </tr>
+    </table>
+  </div>
+  <div class='<? echo $payloadclass; ?>'>
+<?
+$print_on_exit = "</div></body></html>";
+$kopf_schon_ausgegeben = true;
 ?>
