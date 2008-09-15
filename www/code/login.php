a9c68d2b69c7ab72ea1c403685e07332f64b07cc
Timo Felbinger <gittf@qipc.org>
2008-09-15 10:34:12 +0000

    login.php runderneuert: tabelle sessions benutzen, COOKIE enthaelt keine Klartext-Passwoerter mehr, sondern nur session_id und Zufallsstring.

--- www/code/login.php
+++ www/code/login.php
@@ -2,22 +2,26 @@
 //
 // login.php
 //
-// anmeldeformular:
+// anmeldescript:
+//  - prueft, ob benutzer schon angemeldet (per cookie)
 //  - verarbeitet neuanmeldungen
-//  - andernfalls: prueft, ob benutzer schon angemeldet (per cookie)
-//  - falls auch nicht: anmeldeformular ausgeben
-//  - per "action=logout" wird ein logout (loeschen des cookie) erzwungen
+//  - per "login=logout" wird ein logout (loeschen des cookie) erzwungen
+//  - falls nicht angemeldet: anmeldeformular wird ausgegeben
 //  - falls $from_dokuwiki==true wird index.php?window=wiki aufgerufen
 //
-// bei erfolgreicher anmeldung werden gesetzt:
+// bei erfolgreicher anmeldung werden global gesetzt:
 //  - $angemeldet == TRUE
 //  - $login_gruppen_id
 //  - $login_gruppen_name
+//  - $session_id
 //  - $dienst (0, 1, 3, 4 oder 5)
 // falls $dienst > 0 ausserdem:
 //  - $coopie_name
 //  - $dienstkontrollblatt_id
 
+// Bei aufruf aus dem wiki sind wir in einer function, wir brauchen daher 
+// die global-deklarationen:
+//
   global $angemeldet,
          $login_gruppen_id,
          $login_gruppen_name,
@@ -26,15 +30,22 @@
          $dienstkontrollblatt_id,
          $foodsoftpath,
          $foodsoftdir,
-         $action;
+         $action,
+         $session_id,
+         $motd;
 
   function init_login() {
-    global $angemeldet, $login_gruppen_id, $login_gruppen_name, $dienst, $dienstkontrollblatt_id;
+    global $angemeldet, $login_gruppen_id, $login_gruppen_name
+         , $session_id, $dienst, $dienstkontrollblatt_id;
+    // echo "init_login called";
     $angemeldet=FALSE;
     $login_gruppen_id = FALSE;
     $login_gruppen_name = FALSE;
     $dienst = 0;
     $dienstkontrollblatt_id = FALSE;
+    $coopie_name= FALSE;
+    $session_id = 0;
+    set_privileges();
   }
   function logout() {
     init_login();
@@ -43,17 +54,64 @@
   }
 
   init_login();
+  $problems = '';
 
   $telefon ='';
   $name ='';
   $notiz ='';
-  $problems = '';
+
+  // pruefen, ob schon eingeloggt:
+  //
+  if( isset( $_COOKIE['foodsoftkeks'] ) && ( strlen( $_COOKIE['foodsoftkeks'] ) > 1 ) ) {
+
+    sscanf( $_COOKIE['foodsoftkeks'], "%u_%s", &$session_id, &$cookie );
+
+    $row = sql_select_single_row( "SELECT * FROM sessions WHERE id=$session_id", true );
+    if( ! $row ) {
+      $problems .= "<div class='warn'>nicht angemeldet</div>";
+    } elseif( $cookie != $row['cookie'] ) {
+      $problems .= "<div class='warn'>Fehler im Keks: nicht angemeldet</div>";
+    } else {
+      // anmeldung ist gueltig:
+      $login_gruppen_id = $row['login_gruppen_id'];
+      $dienst = $row['dienst'];
+      $dienstkontrollblatt_id = $row['dienstkontrollblatt_id'];
+      $login_gruppen_name = sql_gruppenname( $login_gruppen_id );
+    }
+    switch( $dienst ) {
+      case 0:
+      case 1:
+      case 3:
+      case 4:
+      case 5:
+        break;
+      default:
+        $problems = $problems .  "<div class='warn'>interner fehler: ungueltiger dienst</div>";
+    }
+    if( $dienst > 0 ) {
+      if( $dienstkontrollblatt_id > 0 ) {
+        $result = dienstkontrollblatt_select( $dienstkontrollblatt_id );
+        $row = mysql_fetch_array($result)
+          or $problems = $problems .  "<div class='warn'>Dienstkontrollblatt-Eintrag nicht gefunden</div>";
+        $coopie_name = $row['name'];
+      } else {
+        $problems = $problems .  "<div class='warn'>interner fehler: ungueltige dienstkontrollblatt_id</div>";
+      }
+    }
+
+    if( ! $problems ) {  // login ok, weitermachen...
+      $angemeldet = TRUE;
+      set_privileges();
+    } else {  // irgendwas war falsch... zurueck auf los:
+      logout();
+    }
+  }
 
   // pruefen, ob neue login daten uebergeben werden:
   //
-  get_http_var( 'action', 'w', '' );
-
-  if( $action == 'login' ) {
+  get_http_var( 'login', 'w', '' );
+  switch( $login ) {
+  case 'login': 
     get_http_var( 'login_gruppen_id', 'u' )
       or $problems = $problems . "<div class='warn'>FEHLER: keine Gruppe ausgewaehlt</div>";
     get_http_var( 'passwort','R' )
@@ -82,7 +140,6 @@
     }
 
     if( ! $problems ) {
-
       if( $gruppe = check_password( $login_gruppen_id, $passwort ) ) {
         $login_gruppen_name = $gruppe['name'];
       } else {
@@ -94,259 +151,153 @@
       $dienstkontrollblatt_id = dienstkontrollblatt_eintrag(
         false, $login_gruppen_id, $dienst, $coopie_name, $telefon, $notiz 
       );
-      // echo "<div class='ok'>neue dienstkontrollblatt_id: $dienstkontrollblatt_id</div>";
     } else {
       $dienstkontrollblatt_id = 0;
     }
 
-    // ggf. passwort aendern:
-    //
-    get_http_var( 'pwneu1', 'R', '' );
-    get_http_var( 'pwneu2', 'R', '' );
-    if( isset($pwneu1) && ( strlen( $pwneu1 ) >= 1 ) ) {
-      if( strlen( $pwneu1 ) < 2 ) {
-        $problems = $problems . "<div class='warn'>FEHLER: neues Passwort zu kurz</div>";
-      }
-      if( $pwneu1 != $pwneu2 ) {
-        $problems = $problems . "<div class='warn'>FEHLER: Eingaben fuer neues PW stimmen nicht ueberein</div>";
-      }
-      if( ! $problems ) {
-        setcookie( 'foodsoftkeks', '0', 0, '/' );
-        set_password( $login_gruppen_id, $pwneu1 )
-          or error(__LINE__,__FILE__,"Konnte das Gruppenpasswort nicht setzen: ",mysql_error());
-        include( 'head.php' );
-        echo "
-          <div class='ok'>
-           Passwort erfolgreich geaendert!  &nbsp; <a href='$foodsoftdir/index.php'>weiter...</a>
-          </div>
-          $print_on_exit
-        ";
-        exit();
-      }
-    }
-
     if( ! $problems ) {
-      $klarkeks = "$login_gruppen_id-$dienst-$dienstkontrollblatt_id-$passwort";
-      $keks = base64_encode( "$klarkeks" );
-      setcookie( 'foodsoftkeks', $keks, 0, '/' ) 
-        or error( __LINE__, __FILE__, "setcookie() fehlgeschlagen" );
+      // alles ok: neue session erzeugen:
+      $cookie = random_hex_string( 5 );
+      $session_id = sql_insert( 'sessions', array( 
+        'cookie' => $cookie
+      , 'login_gruppen_id' => $login_gruppen_id
+      , 'dienst' => $dienst
+      , 'dienstkontrollblatt_id' => $dienstkontrollblatt_id
+      ) );
+      $keks = $session_id.'_'.$cookie;
+      need( setcookie( 'foodsoftkeks', $keks, 0, '/' ), "setcookie() fehlgeschlagen" );
       $angemeldet = TRUE;
       set_privileges();
-      return;
+      logger( 'successful login' );
     }
-
-  // falls kein neues login:
-  // pruefen, ob schon eingeloggt:
-  //
-  } elseif( isset( $_COOKIE['foodsoftkeks'] ) && ( strlen( $_COOKIE['foodsoftkeks'] ) > 1 ) ) {
-
-    // echo "keks: {$_COOKIE['foodsoftkeks']} ";
-
-    $keks = base64_decode( $_COOKIE['foodsoftkeks'] );
-    sscanf( $keks, "%u-%u-%u-%s", &$login_gruppen_id, &$dienst, &$dienstkontrollblatt_id, &$passwort );
-
-    if( ! ( $login_gruppen_id > 0 ) ) {
-      $problems = $problems .  "<div class='warn'>fehler im keks: ungueltige login_gruppen_id</div>";
-    } else {
-      // echo "<!-- from cookie: :$login_gruppen_id,$passwort: -->";
-      $bestellgruppen_row = check_password( $login_gruppen_id, $passwort );
-      if( $bestellgruppen_row ) {
-        $login_gruppen_name = $bestellgruppen_row['name'];
-      } else {
-        $problems = $problems .  "<div class='warn'>fehler im keks: ungueltiges passwort</div>";
-      }
+    break;
+  case 'logout':
+    // ggf. noch  dienstkontrollblatt-Eintrag aktualisieren:
+    if( $dienst > 0 and $dienstkontrollblatt_id > 0 ) {
+      get_http_var('coopie_name','H','');
+      get_http_var('telefon','H','');
+      get_http_var('notiz','H','');
+      dienstkontrollblatt_eintrag(
+        $dienstkontrollblatt_id, $login_gruppen_id, $dienst, $coopie_name, $telefon, $notiz 
+      );
     }
-    switch( $dienst ) {
-      case 0:
-      case 1:
-      case 3:
-      case 4:
-      case 5:
-        break;
-      default:
-        $problems = $problems .  "<div class='warn'>fehler im keks: ungueltiger dienst</div>";
-    }
-    if( $dienst > 0 ) {
-      if( $dienstkontrollblatt_id > 0 ) {
-        $result = dienstkontrollblatt_select( $dienstkontrollblatt_id );
-        $row = mysql_fetch_array($result)
-          or $problems = $problems .  "<div class='warn'>Dienstkontrollblatt-Eintrag nicht gefunden</div>";
-        $coopie_name = $row['name'];
-      } else {
-        $problems = $problems .  "<div class='warn'>fehler im keks $keks: ungueltige dienstkontrollblatt_id: $dienstkontrollblatt_id</div>";
-      }
-    }
-
-    if( ! $problems ) {  // login ok, weitermachen...
-
-      if( ( $action == 'logout' ) ) {
-        // ggf. noch  dienstkontrollblatt-Eintrag aktualisieren:
-        if( $dienst > 0 and $dienstkontrollblatt_id > 0 ) {
-          get_http_var('coopie_name','M','');
-          get_http_var('telefon','M','');
-          get_http_var('notiz','M','');
-          dienstkontrollblatt_eintrag(
-            $dienstkontrollblatt_id, $login_gruppen_id, $dienst, $coopie_name, $telefon, $notiz 
-          );
-        }
-        logout();
-        $problems = "<div class='ok'>Abgemeldet!</div>";
-      } else {
-        $angemeldet = TRUE;
-        set_privileges();
-        return;
-      }
-    } else {  // fehlerhafter keks, besser loeschen:
-      logout();
-    }
+    logout();
+    $problems .= "<div class='ok'>Abgemeldet!</div>";
+    break;
   }
 
-  // ab hier: benutzer ist noch nicht eingeloggt, und hat keine gueltigen
-  // login daten uebergeben. Wir setzen alles zurueck und zeigen das
-  // anmeldeformular:
+  if( $angemeldet )
+    return;
+
+  // ab hier: benutzer ist nicht eingeloggt; wir setzen alles zurueck und zeigen das anmeldeformular:
 
   logout();  // nicht korrekt angemeldet: alles zuruecksetzen...
-
-  set_privileges(); // im moment: keine...
   // setWikiHelpTopic( ':' );
   require_once("head.php");
   setWikiHelpTopic( ':' );
 
-  get_http_var( 'area', 'w', '' );
-  if( isset( $from_dokuwiki ) && $from_dokuwiki or ( $area == 'wiki' ) ) {
+  if( isset( $from_dokuwiki ) && $from_dokuwiki ) {
     $form_action="$foodsoftdir/index.php?window=wiki";
   } else {
     $form_action="$foodsoftdir/index.php";
   }
 
-?>
-       <div class='kommentar'>
-  <h2> Erstmal „Hallo“ an alle, </h2>
-	   die sicherlich nicht ohne Grund auf dieser Seite gelandet sind. Aber auch allen Wellenreiter des Internets, die zufällig hier gestrandet sind ein „Herzliches Willkommen“ !
-<br>
-Ihr interessiert euch auch für den kleinen Geldbeutel erschwingliche, biologisch und wenn möglich regional erzeugte Lebensmittel!
-<br>
-Dann seid ihr hier richtig! Genau aus diesem Grund wurde unsere Gruppe 2003 aus der Taufe gehoben!
-<br>
-<a href=/wiki>mehr Informationen...</a>
+if( isset( $motd ) ) {
+  ?> <div class='kommentar'> echo $motd </div> <?
+}
 
-</div>
-<?
-  echo "
-    <form action='$form_action' method='post' class='small_form'>
-      <input type='hidden' name='action' value='login'>
-      <fieldset class='small_form' style='padding:2em;'>
-        <legend>
-          Anmelden
-        </legend>
-  ";
-  if( "$problems" ) echo "$problems";
-  echo "
-       <div class='kommentar'>
-         <br>
-         Anmeldung für die Foodsoft und fürs Doku-Wiki der Foodsoft:
-         <br>
-       </div>
-       <div class='newfield'>
-         <label>Gruppe:</label>
-         <select size='1' name='login_gruppen_id'>
-  ";
-  echo optionen_gruppen();
-  echo "
-         </select>
-         <label style='padding-left:4em;'>Passwort:</label>
-         <input type='password' size='8' name='passwort' value=''>
-         <a id='pwneu_knopf' style='margin-left:2em;font-size:10pt;'
-          onclick='pwneu_on();'>Passwort &auml;ndern...</a>
-       </div>
-       <div class='newfield' style='display:none;' id='pwneu_form'>
-         <fieldset class='small_form'>
-         <legend>
-         <img src='img/close_black_trans.gif' style='padding:0pt;margin:0pt;' class='button'
-          onclick='pwneu_off()' title='Ausblenden...' alt='Ausblenden...'>
-          Passwort &auml;ndern
-          </legend>
-         <label style='padding-left:2em;'>neues Passwort:</label>
-         <input type='password' size='8' name='pwneu1' value=''>
-         <label style='padding-left:4em;'>nochmal das neue Passwort:</label>
-         <input type='password' size='8' name='pwneu2' value=''>
-         </fieldset>
-       </div>
-       <div class='newfield'>
-         <label>Ich mache gerade...</label>
-       </div>
-       <table>
-         <tr>
-           <td>
-             <input class='checkbox' type='radio' name='dienst' value='0'
-             onclick='dienstform_off();' ";
-  if (!$dienst) echo ' checked';
-  echo ">
-             <label>keinen Dienst</label>
-           </td>
-           <td>
-             <input class='checkbox' type='radio' name='dienst' value='1'
-             onclick='dienstform_on();' ";
-  if ($dienst==1) echo ' checked';
-  echo ">
-             <label title='Verteiler'>Dienst I/II</label>
-           </td>
-           <td>
-             <input class='checkbox' type='radio' name='dienst' value='3'
-             onclick='dienstform_on();' ";
-  if ($dienst==3) echo ' checked';
-  echo ">
-             <label title='Kellerdienst'>Dienst III</label>
-           </td>
-           <td>
-             <input class='checkbox' type='radio' name='dienst' value='4'
-             onclick='dienstform_on();' ";
-  if ($dienst==4) echo ' checked';
-  echo ">
-             <label title='Abrechnung'>Dienst IV</label>
-           </td>
-           <td>
-             <input class='checkbox' type='radio' name='dienst' value='5'
-             onclick='dienstform_on();' ";
-  if ($dienst==5) echo ' checked';
-  echo ">
-             <label title='Mitgliederverwaltung'>Dienst V</label>
-           </td>
-         </tr>
-       </table>
-       <div id='dienstform' style='display:";
-       echo $dienst ? 'block' : 'none';
-  echo ";'>
-         <div class='kommentar'>
+?>
+<form action='<? echo $form_action; ?>' method='post' class='small_form'>
+  <? echo self_post(); ?>
+  <input type='hidden' name='login' value='login'>
+    <fieldset class='small_form' style='padding:2em;'>
+      <legend>Anmelden</legend>
+    <? if( "$problems" ) echo "$problems"; ?>
+    <div class='kommentar' style='padding:1em;'>
+      Anmeldung für die Foodsoft und fürs Doku-Wiki der Foodsoft:
+    </div>
+    <div class='newfield'>
+      <label>Gruppe:</label>
+      <select size='1' name='login_gruppen_id'>
+        <? echo optionen_gruppen(); ?>
+      </select>
+      <label style='padding-left:4em;'>Passwort:</label>
+        <input type='password' size='8' name='passwort' value=''>
+    </div>
+    <div class='newfield'>
+      <label>Ich mache gerade...</label>
+    </div>
+    <table>
+      <tr>
+        <td>
+          <input class='checkbox' type='radio' name='dienst' value='0'
+            onclick='dienstform_off();'
+            <? if (!$dienst) echo ' checked'; ?> >
+          <label>keinen Dienst</label>
+        </td>
+        <td>
+          <input class='checkbox' type='radio' name='dienst' value='1'
+            onclick='dienstform_on();'
+            <? if ($dienst==1) echo ' checked'; ?> >
+          <label title='Verteiler'>Dienst I/II</label>
+        </td>
+        <td>
+          <input class='checkbox' type='radio' name='dienst' value='3'
+            onclick='dienstform_on();'
+            <? if ($dienst==3) echo ' checked'; ?> >
+          <label title='Kellerdienst'>Dienst III</label>
+        </td>
+        <td>
+          <input class='checkbox' type='radio' name='dienst' value='4'
+            onclick='dienstform_on();'
+            <? if ($dienst==4) echo ' checked'; ?> >
+          <label title='Abrechnung'>Dienst IV</label>
+        </td>
+        <td>
+          <input class='checkbox' type='radio' name='dienst' value='5'
+            onclick='dienstform_on();'
+            <? if ($dienst==5) echo ' checked'; ?> >
+          <label title='Mitgliederverwaltung'>Dienst V</label>
+        </td>
+      </tr>
+    </table>
+    <div id='dienstform' style='display:<? echo $dienst ? 'block' : 'none';  ?>;'>
+      <div class='kommentar'>
            Wenn Du Dich fuer einen Dienst anmeldest, kannst Du zusaetzliche
            Funktionen der Foodsoft nutzen; ausserdem wirst Du 
            automatisch ins Dienstkontrollblatt eingetragen:
-         </div>
+      </div>
          <fieldset class='small_form'>
            <legend>
              Dienstkontrollblatt
            </legend>
            <div class='newfield'>
              <label>Dein Name:</label>
-             <input type='text' size='20' name='coopie_name' value='$coopie_name'>
+             <input type='text' size='20' name='coopie_name' value='<? echo $coopie_name; ?>'>
              <label style='padding-left:4em;'>Telefon:</label>
-             <input type='text' size='20' name='telefon' value='$telefon'>
+             <input type='text' size='20' name='telefon' value='<? $telefon; ?>'>
            </div>
            <div class='newfield'>
              <label>Notiz fuers Dienstkontrollblatt:</label>
              <br>
-             <textarea cols='80' rows='3' name='notiz'>$notiz</textarea>
+             <textarea cols='80' rows='3' name='notiz'><? echo $notiz; ?></textarea>
            </div>
          </fieldset>
        </div>
-       ";
-  echo "
        <div class='newfield'>
          <input type='submit' name='submit' value='OK'>
        </div>
      </fieldset>
    </form>
-  ";
+   <script type='text/javascript'>
+     function dienstform_on() {
+       document.getElementById('dienstform').style.display = 'block';
+     }
+     function dienstform_off() {
+       document.getElementById('dienstform').style.display = 'none';
+     }
+   </script>
+<?
 
   function set_privileges() {
     global $dienst, $hat_dienst_I, $hat_dienst_III, $hat_dienst_IV, $hat_dienst_V;
@@ -415,25 +366,7 @@ Dann seid ihr hier richtig! Genau aus diesem Grund wurde unsere Gruppe 2003 aus 
     }
   }
 
-  ?>
-    <script type='text/javascript'>
-      function dienstform_on() {
-        document.getElementById('dienstform').style.display = 'block';
-      }
-      function dienstform_off() {
-        document.getElementById('dienstform').style.display = 'none';
-      }
-      function pwneu_on() {
-        document.getElementById('pwneu_knopf').style.display = 'none';
-        document.getElementById('pwneu_form').style.display = 'block';
-      }
-      function pwneu_off() {
-        document.getElementById('pwneu_knopf').style.display = 'inline';
-        document.getElementById('pwneu_form').style.display = 'none';
-      }
-    </script>
-  <?
-  echo $print_on_exit;
+echo $print_on_exit;
 
 exit();
 
