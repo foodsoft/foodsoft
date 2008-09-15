1f2b6fb09935b593edb29b92b7c512fd363c079a
Timo Felbinger <gittf@qipc.org>
2008-09-15 14:49:50 +0000

    setup.php erweitert: Fehler korrigiert, Anfang leitvariablen-Editor

--- www/setup.php
+++ www/setup.php
@@ -15,85 +15,212 @@
 require_once('code/config.php');
 require_once('structure.php');
 
+$problems = false;
 
+//
+// (1) check server runtime environment:
+//
 
-// $db_pwd = 
+$foodsoft_path = realpath( dirname( __FILE__ ) );
+$ruid = posix_getuid();
+$euid = posix_geteuid();
+$rgid = posix_getgid();
+$egid = posix_getegid();
 
 ?>
-<h1>Database settings</h1>
+<h2 style='padding:1em 0em 0ex 0em;'>HTTP Server:</h2>
   <table>
     <tr>
-      <th>Server:</th>
-      <td><? echo $db_server; ?></td>
+      <th>Name / Port:</th>
+      <td><? echo getenv( 'SERVER_NAME' ) . ' / ' . getenv( 'SERVER_PORT' );  ?></td>
     </tr>
     <tr>
-      <th>Database:</th>
-      <td><? echo $db_name; ?></td>
+      <th>Software:</th>
+      <td><? echo getenv( 'SERVER_SOFTWARE' ); ?></td>
     </tr>
     <tr>
-      <th>User:</th>
-      <td><? echo $db_user; ?></td>
+      <th>Foodsoft Pfad:</th>
+      <td><? echo $foodsoft_path; ?></td>
     </tr>
     <tr>
-      <th>Password:</th>
-      <td><input type='password' size='20' value='12345' name='db_pwd'></td>
+      <th>ruid / euid:</th>
+      <td><? echo $ruid . ' / ' . $euid; ?></td>
     </tr>
+    <tr>
+      <th>rgid / egid:</th>
+      <td><? echo $rgid . ' / ' . $egid; ?></td>
+    </tr>
+  </table>
+ <?
+
+//
+// (2) check file system layout, permissions, ... (TODO: this is incomplete!)
+//
+
+function check_dir( $path ) {
+  echo "check_dir: $path<br>";
+  if( $path == 'CVS' or $path == 'attic' ) {
+
+  }
+  return true;
+}
+
+function check_file( $path ) {
+
+  echo "check_file: $path<br>";
+
+  return true;
+}
+
+function recurse_dir( $path ) {
+  global $foodsoftdir, $ruid, $rgid, $euid, $egid;
+  $dir = opendir( $path );
+  if( $dir === FALSE ) {
+    ?>
+      <tr>
+        <th class='warn'>
+          Problem: cannot access directory
+        </th>
+        <td><kbd><? echo $path; ?></kbd></td>
+      </tr>
+      <tr>
+        <td colspan='2' class='alert'>
+          Suggestion:
+              <? echo $foodsoftdir; ?> and all subdirectories below should have read and execute permission,
+              but no write permissions, for the apache server process.
+        </td>
+      </tr>
+    <?
+    return false;
+  }
+  echo "hello";
+  $ok = check_dir( $path );
+  while( $path = readdir( $dir ) ) {
+    echo "readdir: $path<br>";
+    if( $path == '.' or $path == '..' )
+      continue;
+    if( is_dir( $path ) ) {
+      $ok &= recurse_dir( $path );
+    } else {
+      $ok &= check_file( $path );
+    }
+  }
+  return $ok;
+}
+
+if( ! $problems ) {
+// funktioniert noch nicht:
+// recurse_dir( $foodsoft_path );
+
+}
+
+
+
+//
+// (3) check MySQL server connection
+//
+
+if( ! $problems ) {
+do {
+  ?>
+    <h2 style='padding:1em 0em 0ex 0em;'>MySQL db Einstellungensettings:</h2>
+      <table>
+        <tr>
+          <th>Server:</th>
+            <? if( isset( $db_server ) ) { ?>
+              <td class='ok'><? echo $db_server; ?></td>
+            <? } else { $problems = true; ?>
+              <td class='warn'>$db_server nicht gesetzt</td>
+            <? } ?>
+          </td>
+        </tr>
+        <tr>
+          <th>Datenbank:</th>
+            <? if( isset( $db_name ) ) { ?>
+              <td class='ok'><? echo $db_name; ?></td>
+            <? } else { $problems = true; ?>
+              <td class='warn'>$db_ name nicht gesetzt</td>
+            <? } ?>
+          </td>
+        </tr>
+        <tr>
+          <th>Benutzer:</th>
+            <? if( isset( $db_user ) ) { ?>
+              <td class='ok'><? echo $db_user; ?></td>
+            <? } else { $problems = true; ?>
+              <td class='warn'>$db_user nicht gesetzt</td>
+            <? } ?>
+          </td>
+        </tr>
+        <tr>
+          <th>Password:</th>
+          <? if( isset( $db_pwd ) ) { ?>
+            <td class='ok'>(ein password ist gesetzt)</td>
+          <? } else { $problems = true; ?>
+            <td class='warn'>$db_pwd nicht gesetzt</td>
+          <? } ?>
+        </tr>
+  <?
+  if( $problems )
+    break;
+  ?>
     <tr>
       <th>mysql_connect():</th>
-<?
-
-$db = mysql_connect($db_server,$db_user,$db_pwd);
-if( ! $db ) {
-  ?>
-    <td class='warn'>Failed!</td>
-    </tr>
-    </table>
-    <div class='alert'>
-      mysql_connect() failed: could not connect to MySQL server.
-      Please check and correct your settings in code/config.php and try again!
-    </div>
-    </body>
-    </html>
   <?
-  exit(1);
+  $db = mysql_connect($db_server,$db_user,$db_pwd);
+  if( $db ) {
+    ?> <td class='ok'>Verbindung zum MySQL Server OK </td></tr> <?
+  } else {
+    ?>
+      <td class='warn'>
+        Verbindung zum MySQL Server fehlgeschlagen:
+        <div class='warn'><? echo mysql_error(); ?></div>
+      </dt>
+    <?
+    $problems = true;
+  }
+  ?> </tr> <?
+  if( $problems )
+    break;
+
+  ?>
+    <tr>
+      <th>mysql_select_db():</th>
+  <?
+  $db_selected = mysql_select_db( $db_name, $db );
+  if( $db_selected ) {
+    ?> <td class='ok'>Verbindung zur Datenbank OK </td></tr> <?
+  } else {
+    ?>
+      <td class='warn'>
+        Verbindung zur Datenbank fehlgeschlagen:
+        <div class='warn'><? echo mysql_error(); ?></div>
+      </dt>
+    <?
+    $problems = true;
+  }
+  ?> </tr> <?
+} while( 0 );
+
+?> </table> <?
+
+if( $problems ) {
+  ?>
+    <div class='alert'>
+      Zugriff auf die Datenbank funktioniert nicht richtig.
+      Bitte überprüfe die Einstellungen in code/config.php!
+    </div>
+  <?
 }
 
-?>
-  <td class='ok'>
-    OK
-  </td>
-  </tr>
-  <tr>
-    <th>mysql_select_db():</th>
-<?
-
-$db_selected = mysql_select_db( $db_name, $db );
-if( ! $db_selected ) {
-  ?>
-    <td class='warn'>Failed!</td>
-    </tr>
-    </table>
-    <div class='alert'>
-      mysql_select_db() failed: could not select database <? echo $db_name; ?>.
-      Please check and correct your settings in code/config.php and try again!
-    </div>
-    </body>
-    </html>
-  <?
-  exit(1);
 }
-?>
-  <td class='ok'>
-    OK
-  </td>
-  </tr>
-  </table>
-<?
 
 
 //
-// database connection established; we can now save settings, fix tables, etc....
+// (4) database connection established: check tables, columns, indices:
 //
+
+if( ! $problems ) {
 
 $sql = array();
 
@@ -108,10 +235,13 @@ function add_table( $want_table ) {
     } else {
       $s .= 'NULL ';
     }
-    if( $props['default'] ) {
-      $s .= "default '{$props['default']}' ";
+    if( isset( $props['default'] ) && ( $props['default'] !== '' ) ) {
+      $s .= "default {$props['default']} ";
     }
-    $s .= $props['extra'] . "\n";
+    if( isset( $props['extra'] ) ) {
+      $s .= $props['extra'];
+    }
+    $s .= "\n";
     $komma = ',';
   }
   foreach( $tables[$want_table]['indices'] as $want_index => $props ) {
@@ -134,8 +264,8 @@ function add_col( $want_table, $want_col, $op = 'ADD' ) {
   $col = $tables[$want_table]['cols'][$want_col];
   $type = $col['type'];
   $null = ( $col['null'] == 'NO' ? 'NOT NULL' : 'NULL' );
-  $default = ( $col['default'] !== '' ? "default " . $col['default'] : '' );
-  $extra = $col['extra'];
+  $default = ( ( isset( $col['default'] ) && ( $col['default'] !== '' ) ) ? "default " . $col['default'] : '' );
+  $extra = ( isset( $col['extra'] ) ? $col['extra'] : '' );
   $s = " ALTER TABLE $want_table $op COLUMN `$want_col` $type $null $default $extra;";
   $sql[] = $s;
 }
@@ -176,8 +306,6 @@ function fix_index( $table, $index ) {
   delete_index( $table, $index );
   add_index( $table, $index );
 }
-
-
 
 
 if( $HTTP_POST_VARS['action'] == 'setup' ) {
@@ -223,15 +351,14 @@ if( $HTTP_POST_VARS['action'] == 'setup' ) {
     }
   }
 }
-            
-          
-    
+
 if( count( $sql ) > 0 ) {
   ?>
+    <h3 clas='alert' style='padding-top:2em;'>Korrekturen an der Datenbank:</h3>
     <table>
       <tr>
-        <th>SQL instruction</th>
-        <th>Result</th>
+        <th>SQL Befehl:</th>
+        <th>Ergebnis:</th>
       </tr>
   <?
   foreach( $sql as $s ) {
@@ -248,11 +375,12 @@ if( count( $sql ) > 0 ) {
     } else {
       ?>
         <td class='warn'>
-          failed:
+          fehlgeschlagen:
           <? echo mysql_error(); ?>
         </td>
         </tr>
       <?
+      $problems = true;
       break;
     }
   }
@@ -260,16 +388,15 @@ if( count( $sql ) > 0 ) {
 }
 
 
-
 ?>
-  <h1>Tables:</h1>
+  <h2 style='padding:1em 0em 0ex 0em;'>MySQL Tabellen:</h2>
   <table>
 <?
 
 $thead = "
   <tr>
-    <th>Column</th>
-    <th>Type</th>
+    <th>Spalte</th>
+    <th>Typ</th>
     <th>Null</th>
     <th>Default</th>
     <th>Extra</th>
@@ -279,7 +406,7 @@ $thead = "
 $ihead = "
   <tr>
     <th>Name</th>
-    <th colspan='3'>Column(s)</th>
+    <th colspan='3'>Spalte(n)</th>
     <th>Unique</th>
     <th>Status</th>
   </tr>
@@ -294,19 +421,22 @@ foreach( $tables as $table => $want ) {
       <th colspan='6' style='padding-top:1em;text-align:center;'>table: <? echo $table; ?></th>
     </tr>
   <?
-  $result = mysql_query( "SHOW COLUMNS FROM $table; " );
+  
+  $sql = "SHOW COLUMNS FROM $table; ";
+  $result = mysql_query( $sql );
   if( ! $result ) {
     ?>
       <tr>
         <td class='warn' colspan='5'>
-          failed: <code>SHOW COLUMNS FROM <? echo $table; ?></code>
+          fehlgeschlagen: <code><? echo $sql; ?></code>
         </td>
         <td class='warn' style='text-align:right;'>
-          create table? <input type='checkbox' name='add_table_<? echo $id; ?>'>
-          create table? <input type='hidden' name='table_<? echo $id; ?>' value='<? echo $table; ?>'>
+          Tabelle anlegen? <input type='checkbox' name='add_table_<? echo $id; ?>'>
+          <input type='hidden' name='table_<? echo $id; ?>' value='<? echo $table; ?>'>
         </td>
       </tr>
     <?
+    $problems = true;
     $id++;
     continue;
   }
@@ -353,18 +483,19 @@ foreach( $tables as $table => $want ) {
       }
       if( $mismatch ) {
         ?>
-            <td class='warn'>mismatch</td>
+            <td class='warn'>Fehler</td>
           </tr>
           <tr>
-            <td class='alert' style='text-align:right;'>should be:</td>
+            <td class='alert' style='text-align:right;'>Sollwert:</td>
             <? echo $s; ?>
             <td class='alert' style='text-align:right;'>
-              fix column?  <input type='checkbox' name='fix_col_<? echo $id; ?>'>
+              Spalte korrigieren? <input type='checkbox' name='fix_col_<? echo $id; ?>'>
             <input type='hidden' name='table_<? echo $id; ?>' value='<? echo $table; ?>'>
             <input type='hidden' name='col_<? echo $id; ?>' value='<? echo $field; ?>'>
             </td>
           </tr>
         <?
+        $problems = true;
         $id++;
       } else {
         ?>
@@ -376,7 +507,7 @@ foreach( $tables as $table => $want ) {
     } else {
       ?>
           <td class='alert' style='text-align:right;'>
-            obsolete column; delete? <input type='checkbox' name='delete_col_<? echo $id; ?>'>
+            Spalte nicht benötigt; löschen? <input type='checkbox' name='delete_col_<? echo $id; ?>'>
             <input type='hidden' name='table_<? echo $id; ?>' value='<? echo $table; ?>'>
             <input type='hidden' name='col_<? echo $id; ?>' value='<? echo $field; ?>'>
           </td>
@@ -394,16 +525,16 @@ foreach( $tables as $table => $want ) {
         <td class='warn'><? echo $want_props['default']; ?></td>
         <td class='warn'><? echo $want_props['extra']; ?></td>
         <td class='alert' style='text-align:right;'>
-          missing column; add? <input type='checkbox' name='add_col_<? echo $id; ?>'>
+          fehlende Spalte; hinzufügen? <input type='checkbox' name='add_col_<? echo $id; ?>'>
           <input type='hidden' name='table_<? echo $id; ?>' value='<? echo $table; ?>'>
           <input type='hidden' name='col_<? echo $id; ?>' value='<? echo $want_col; ?>'>
         </td>
       </tr>
     <?
+    $problems = true;
     $id++;
   }
 
-  
   ?>
     <tr>
       <th colspan='6' style='text-align:left;'>indices:</th>
@@ -442,10 +573,10 @@ foreach( $tables as $table => $want ) {
           }
           if( $mismatch ) {
             ?>
-                <td class='warn'>mismatch</td>
+                <td class='warn'>Fehler</td>
               </tr>
               <tr>
-                <td class='alert' style='text-align:right;'>should be:</td>
+                <td class='alert' style='text-align:right;'>Sollwert:</td>
                 <? echo $s; ?>
                 <td class='alert' style='text-align:right;'>
                   fix index?  <input type='checkbox' name='fix_index_<? echo $id; ?>'>
@@ -454,6 +585,7 @@ foreach( $tables as $table => $want ) {
                 </td>
               </tr>
             <?
+            $problems = true;
             $id++;
           } else {
             ?>
@@ -465,7 +597,7 @@ foreach( $tables as $table => $want ) {
         } else {
           ?>
               <td class='alert' style='text-align:right;'>
-                obsolete index; delete? <input type='checkbox' name='delete_index_<? echo $id; ?>'>
+                Index nicht benötigt; löschen? <input type='checkbox' name='delete_index_<? echo $id; ?>'>
                 <input type='hidden' name='table_<? echo $id; ?>' value='<? echo $table; ?>'>
                 <input type='hidden' name='index_<? echo $id; ?>' value='<? echo $iname; ?>'>
               </td>
@@ -490,16 +622,15 @@ foreach( $tables as $table => $want ) {
         <td class='warn' colspan='3'><? echo $want_props['collist']; ?></td>
         <td class='warn'><? echo $want_props['unique']; ?></td>
         <td class='alert' style='text-align:right;'>
-          missing index; create? <input type='checkbox' name='add_index_<? echo $id; ?>'>
+          fehlender Index; hinzufügen? <input type='checkbox' name='add_index_<? echo $id; ?>'>
           <input type='hidden' name='table_<? echo $id; ?>' value='<? echo $table; ?>'>
           <input type='hidden' name='index_<? echo $id; ?>' value='<? echo $want_index; ?>'>
         </td>
       </tr>
     <?
+    $problems = true;
     $id++;
   }
-
-
 
   ?>
     <tr>
@@ -509,8 +640,82 @@ foreach( $tables as $table => $want ) {
 }
 
 
+?> </table> <?
+
+}
+
+
+//
+// (5) setup leitvariable database:
+//
+
+if( ! $problems ) {
+
+$leitvariable = array(
+  'readonly' => array(
+    'meaning' => 'Datenbank schreibgeschuetzt setzen (sehr eingeschränkte bestimmte sind dennoch moeglich)'
+  , 'default' => '0'
+  )
+, 'foodsoftserver' => array(
+    'meaning' => 'Spitzname des Servers (default: $SERVER_NAME, siehe oben)'
+  , 'default' => ''
+  )
+, 'sockelbetrag' => array(
+    'meaning' => 'Sockeleinlage pro Gruppenmitglied'
+  , 'default' => '6.00'
+  )
+);
+
+  ?>
+  <h2>Konfigurationsvariable</h2>
+  <table>
+    <tr>
+      <th>Variable</th>
+      <th>Bedeutung</th>
+      <th>Wert</th>
+    </tr>
+  <?
+  foreach( $leitvariable as $var => $props ) {
+    ?>
+      <tr>
+        <th><? echo $var; ?></th>
+        <td><? echo $props['meaning']; ?></td>
+        <td>
+    <?
+      $result = mysql_query( "SELECT * FROM leitvariable WHERE name='$var'" );
+      $row = mysql_fetch_array( $result );
+      if( $row ) {
+        $value = $row['value'];
+      } else{
+        $value = $props['default'];
+        ?>
+          <div class='alert'>Noch nicht in der Datenbank gespeichert!</div>
+        <?
+      }
+    ?>
+        <input type='text' size='20' value='<? echo $value; ?>'></td>
+      </tr>
+    <?
+  }
+  $result = mysql_query( "SELECT * FROM leitvariable" );
+  while( $row = mysql_fetch_array( $result ) ) {
+    if( isset( $leitvariable[$row['name']] ) )
+      continue;
+    ?>
+      <tr>
+        <th><? echo $var; ?></th>
+        <td class='alert'>keine aktuell benutzte Konfigurationsvariable - löschen?</td>
+        <td><input type='text' size='20' value='<? echo $value; ?>'></td>
+      </tr>
+    <?
+  }
+  
+
+
+  ?> </table> <?
+}
+
 ?>
-</table>
 
 <div style='text-align:left;padding:1em 1em 2em 1em;'>
   <input type='submit' style='padding:1ex;' value='Submit'>
