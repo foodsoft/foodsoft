0da06e1d554ac204c243cbb9de136baf6f374c6f
Timo Felbinger <gittf@qipc.org>
2008-09-17 14:43:04 +0000

    low-level Fehlerbehanglung nach err_finctions.php, vorhandene Funktionen aufgeraeumt

--- www/code/err_functions.php
+++ www/code/err_functions.php
@@ -1,49 +1,53 @@
 <?PHP
+//
+// low-level error handling and logging
+// (these functions must not depend on a working database connection)
+//
 
-   // DATEI: Diese Datei enthält Funktionen zur Fehlerbehandlung und zum Erstellen von Log-Files
+function log_error($line,$file,$string,$error="",$stack) {
+  global $logfile;
+  if( isset($logfile) and $logfile ) {
+    $fp = fopen( $logfile,"a" );
+    // Fehler rausschreiben und dabei Leerzeichen maskieren (" " => %20)
+    fputs( $fp, $line." ".$file.": ".str_replace(" ", "_", $string)." ".str_replace(" ", "_", $error)." ".var_export($stack, TRUE)."\n" );
+    fclose($fp);
+  }
+}
 
+function error( $line, $file, $string, $error="", $stack="" ){
+  // global $test_title;
 
-	 function log_error($line,$file,$string,$error="",$stack) {
-	 
-	    global $logfile_errs, $log_win_format;
-			
-	                if($logfile_errs == NULL){
-				echo "<p> <b> Warning</b> \$logfile_errs not set, not writing to logfile </p>";
-			} else {
-				$new_line = $log_win_format ? "\r\n" : "\n";
-				$filename = $logfile_errs;
-				$exists     = file_exists($filename);
-				
-				$fp = fopen($filename,"a");
-				
-				// Wenn Log-Datei neu erstellt wurde, dann erstmal "Kopzeile" schreiben
-				if (!$exists) fputs($fp, "line file err_msg mysql_err_msg".$new_line);
-				
-				// Fehler rausschreiben und dabei Leerzeichen maskieren (" " => %20)
-				fputs($fp, $line." ".str_replace(" ", "_", $file)." ".str_replace(" ", "_", $string)." ".str_replace(" ", "_", $error)." ".var_export($stack, TRUE).$new_line);
-				
-				fclose($fp);
-			}
-	 
-	 }
-	 
-	 
+  log_error($line,$file,$string,$error,$stack);
 
-   function error($line,$file,$string,$error="",$stack=""){
-	   global $error_report_adress, $test_title;
-		 
-		 log_error($line,$file,$string,$error,$stack);
-	 
-      $fehler = "<div class='warn'><b>Fehler in Zeile ".$line." in ".$file."</b> ";
-      $fehler .= "<br>" . $string . "<br>";
-      if($error) $fehler .= "<b>Error:</b> ". $error;
-      if($stack) $fehler .= "<br><b>Stack:</b><br><pre><code>".var_export($stack, TRUE)."</code></pre>";
-      $fehler .= "</div>";
-			
-			if ($error_report_adress != "") mail($error_report_adress,$test_title." - Error mail!!",$fehler);
-			
-      die($fehler);
-   }
-	 
+  $fehler = "<div class='warn'><b>Fehler in Zeile ".$line." in ".$file."</b> ";
+  $fehler .= "<br>" . htmlspecialchars($string) . "<br>";
+  if($error) $fehler .= "<b>Error:</b> ". $error;
+  if($stack) $fehler .= "<br><b>Stack:</b><br><pre><code>".htmlspecialchars(var_export($stack, TRUE))."</code></pre>";
+  $fehler .= "</div>";
+
+  // if ($error_report_adress != "") mail($error_report_adress,$test_title." - Error mail!!",$fehler);
+
+  die($fehler);
+}
+
+function need( $exp, $comment = "Fataler Fehler" ) {
+  global $print_on_exit;
+  if( ! $exp ) {
+    ?> <div class='warn'><? echo htmlspecialchars( "$comment: $exp" ); ?> <a href='<? echo self_url(); ?>'>weiter...</a></div> <?
+    echo "$print_on_exit";
+    die();
+  }
+  return true;
+}
+
+function fail_if_readonly() {
+  global $readonly, $print_on_exit;
+  if( $readonly ) {
+    ?> <div class='warn'>Datenbank ist schreibgesch&uuml;tzt - Operation nicht m&ouml;glich!</div> <?
+    echo $print_on_exit;
+    die();
+  }
+  return true;
+}
 
 ?>
