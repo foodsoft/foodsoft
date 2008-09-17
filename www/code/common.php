f611381e939120153e94956c7b13b51de60dd6db
Timo Felbinger <gittf@qipc.org>
2008-09-17 14:59:42 +0000

    index.php und common.php geordnet, Deklarationen globaler variable nach common.php, leitvariable jetzt per positiv-Liste leitvariable.php einlesen

--- www/code/common.php
+++ www/code/common.php
@@ -1,8 +1,24 @@
 <?php
 
 global $foodsoftpath, $foodsoftdir;   // noetig wenn aufruf aus wiki
+if( ! isset( $foodsoftpath ) ) {
+  $foodsoftpath = realpath( dirname( __FILE__ ) . '/../' );
+}
+if( ! isset( $foodsoftdir ) ) {
+  $foodsoftdir = ereg_replace( '/[^/]+$', '', $_SERVER['SCRIPT_NAME'] );
+  // ausnahme: aufruf aus dem wiki heraus:
+  $foodsoftdir = ereg_replace( '/wiki$', '/foodsoft', $foodsoftdir );
+}
 
 require_once('code/config.php');
+if( 0 or $allow_setup ) { // TODO: warnen, wenn setup.php zugreifbar ist?
+  ?><html><body> Fehler: bitte <code>setup.php</code> deaktivieren in <code>code/config.php</code>!</body></html><?
+  exit(1);
+}
+
+global $print_on_exit;
+$print_on_exit = '';
+require_once('code/err_functions.php');
 
 // schinke-server fuer (Terra-)kataloge    *** EXPERIMENTELL ***
 // (bisher nicht sinnvoll, da keine bestellnummern geliefert werden!)
@@ -21,65 +37,50 @@ require_once('code/config.php');
 // }
 
 // verbindung gleich aufbauen:
-if (!($db = mysql_connect($db_server,$db_user,$db_pwd)) || !@MYSQL_SELECT_DB($db_name)) {
+global $db_handle;
+if( ! ( $db_handle = mysql_connect($db_server,$db_user,$db_pwd ) ) || !mysql_select_db( $db_name ) ) {
   echo "<html><body><h1>Datenbankfehler!</h1>Konnte keine Verbindung zur Datenbank herstellen... Bitte später nochmal versuchen.</body></html>";
   exit();
 }
 
-  // die restliche konfiguration koennen wir aus der leitvariablen-tabelle lesen
-  // (skripte koennen dann persistente variable einfach speichern, aendern, und
-  //  an slave (im keller) uebertragen)
-  //
-  $leitvariable = mysql_query( "SELECT * FROM leitvariable" );
-  if( ! $leitvariable ) { 
-    echo "<html><body><h1>Datenbankfehler!</h1>Leitvariabeln nicht gefunden</body></html>";
-    exit();
+// die restliche konfiguration koennen wir aus der leitvariablen-tabelle lesen
+// (skripte koennen dann persistente variable einfach speichern, aendern, und
+//  an slave (im keller) uebertragen)
+//
+global $leitvariable;
+require_once('leitvariable.php');
+foreach( $leitvariable as $name => $props ) {
+  global $$name;
+  $result = mysql_query( "SELECT * FROM leitvariable WHERE name='$name'" );
+  if( $result and ( $row = mysql_fetch_array( $result ) ) ) {
+    $$name = $row['value'];
+  } else {
+    $$name = $props['default'];
   }
-  while( $row = mysql_fetch_array( $leitvariable ) ) {
-    global $$row['name'];
-    $$row['name'] = "{$row['value']}";
-  }
-  $foodsoftserver or $foodsoftserver = "unbekannt";
+}
 
-  // foodsoftpath: absoluter pfad im Server-Filesystem:
-  //
-  isset( $foodsoftpath ) or $foodsoftpath = realpath( dirname( __FILE__ ) . '/../' );
+global $mysqlheute, $mysqljetzt;
+// $mysqljetzt: Alternative zu NOW(), Vorteile:
+//  - kann quotiert werden
+//  - in einem Skriptlauf wird garantiert immer dieselbe Zeit verwendet
+$mysqlheute = date('Y') . '-' . date('m') . '-' . date('d');
+$mysqljetzt = $mysqlheute . ' ' . date('H') . ':' . date('i') . ':' . date('s');
 
-  // foodsoftdir: pfad relativ zur DOCUMENT_ROOT:
-  // (hier benutzt: alle skripte werden per index.php?... aufgerufen!)
-  //
-  if( ! isset( $foodsoftdir ) ) {
-    $foodsoftdir = ereg_replace( '/[^/]+$', '', $_SERVER['SCRIPT_NAME'] );
-    //
-    // ausnahme: aufruf aus dem wiki heraus:
-    //
-    $foodsoftdir = ereg_replace( '/wiki$', '/foodsoft', $foodsoftdir );
-  }
+// gruppen mit sonderbedeutung merken:
+global $specialgroups;
+$specialgroups = array();
+$specialgroups[] = $basar_id;
+$specialgroups[] = $muell_id;
 
-  // $mysqljetzt: Alternative zu NOW(), Vorteile:
-  //  - kann quotiert werden
-  //  - in einem Skriptlauf wird garantiert immer dieselbe Zeit verwendet
-  $mysqlheute = date('Y') . '-' . date('m') . '-' . date('d');
-  $mysqljetzt = $mysqlheute . ' ' . date('H') . ':' . date('i') . ':' . date('s');
+// $self_fields: variable, die in der url uebergeben werden, werden hier gesammelt:
+global $self_fields;
+$self_fields = array();
 
-  // gruppen mit sonderbedeutung merken:
-  global $specialgroups, $basar_id, $muell_id;
-  $specialgroups = array();
-  if( isset( $basar_id ) )
-    $specialgroups[] = $basar_id;
-  if( isset( $muell_id ) )
-    $specialgroups[] = $muell_id;
+require_once('code/views.php');
+require_once('code/inlinks.php');
+require_once('code/zuordnen.php');
+require_once('code/katalogsuche.php');
 
-  global $print_on_exit;
-  $print_on_exit = '';
-
-  // databaseversion
-  $database_version or $database_version = 0;
-  require_once('code/err_functions.php');
-  require_once('code/views.php');
-  require_once('code/inlinks.php');
-  require_once('code/zuordnen.php');
-  require_once('code/katalogsuche.php');
-  update_database($database_version);
+update_database($database_version);
 
 ?>
--- www/index.php
+++ www/index.php
@@ -2,13 +2,12 @@
 
 require_once('code/common.php');
 
-$self_fields = array();
+$window = 'menu';
+$window_id = 'main';
 
-get_http_var( 'window', 'w', 'menu', true );     // eigentlich: name des skriptes
-get_http_var( 'window_id', 'w', 'main', true );  // ID des browserfensters
 require_once( 'code/login.php' );
 if( ! $angemeldet ) {
-  ?> <div class='warn'>Bitte erst <a href='index.php'>Anmelden...</a></div></body></html> <?
+  ?> <div class='warn'>Bitte erst <a href='/foodsoft/index.php'>Anmelden...</a></div></body></html> <?
   exit();
 }
 
@@ -19,72 +18,75 @@ if( get_http_var( 'download','w' ) ) {  // Spezialfall: Datei-Download (.pdf, ..
   exit();
 }
 
-if( $window_id != 'main' ) {
-  require_once( 'windows/head.php' );
-  if( is_readable( "windows/$window.php" ) )
-    include( "windows/$window.php" );
-  else
-    include( "$window.php" );
-  ?>
-    <form name='update_form' method='post' action='<? echo self_url(); ?>'>
-      <? echo self_post(); ?>
-      <input type='hidden' name='message' value=''>
-    </form>
-  <?
-  echo "$print_on_exit";
-  exit();
-}
-
-include('head.php');
-include('dienst_info.php');
-
-global $login_gruppen_id;
-
-switch( $window ) {
-  case "bestellen":
-    // if ( !( $dienst == 4 ) and ( mysql_num_rows(sql_get_dienst_group($login_gruppen_id ,"Vorgeschlagen"))>0 ) ) {
-    //  //darf nur bestellen, wenn Dienste akzeptiert
-    //  ?> <h2> Vor dem Bestellen bitte Dienstvorschl&auml;ge akzeptieren </h2> <?
-    //  include('dienstplan.php');
-    // } else {
-      include('bestellen.php');
-    // }
-    break;
-  case "lieferschein":
-  case "bestellungen_overview":
-    //Fast gleich
-    include('bestellschein.php');
-    break;
-  case "wiki":
-    reload_immediately( "$foodsoftdir/../wiki/doku.php?do=show" );
-    break;
-  default:
-    if( is_readable( "$window.php" ) ) {
-      include( "$window.php" );
-    } elseif( is_readable( "windows/$window.php" ) ) {
-      include( "windows/$window.php" );
-    } else {
-      ?> <div class='warn'>Ung&uuml;ltiger Bereich: <? echo($window); ?></div> <?
-      include('menu.php');
+get_http_var( 'window', 'w', 'menu', true );     // eigentlich: name des skriptes
+get_http_var( 'window_id', 'w', 'main', true );  // ID des browserfensters
+switch( $window_id ) {
+  case 'main':   // anzeige im hauptfenster des browsers
+    include('head.php');
+    include('dienst_info.php');
+    switch( $window ) {
+      case "bestellen":
+        // if ( !( $dienst == 4 ) and ( mysql_num_rows(sql_get_dienst_group($login_gruppen_id ,"Vorgeschlagen"))>0 ) ) {
+        //  //darf nur bestellen, wenn Dienste akzeptiert
+        //  ?><!-- <h2> Vor dem Bestellen bitte Dienstvorschl&auml;ge akzeptieren </h2> --> <?
+        //  include('dienstplan.php');
+        // } else {
+          include('bestellen.php');
+        // }
+        break;
+      case "lieferschein":
+      case "bestellungen_overview":
+        //Fast gleich
+        include('bestellschein.php');
+        break;
+      case "wiki":
+        reload_immediately( "$foodsoftdir/../wiki/doku.php?do=show" );
+        break;
+      default:
+        if( is_readable( "$window.php" ) ) {
+          include( "$window.php" );
+        } elseif( is_readable( "windows/$window.php" ) ) {
+          include( "windows/$window.php" );
+        } else {
+          ?> <div class='warn'>Ung&uuml;ltiger Bereich: <? echo($window); ?></div> <?
+          include('menu.php');
+        }
     }
+    ?>
+      <table width='100%' class='footer'>
+        <tr>
+          <td style='padding-left:1em;text-align:left;'>aktueller Server: <kbd><? echo $foodsoftserver; ?></kbd></td>
+          <td style='padding-right:1em;text-align:right;'>
+          <? echo $mysqljetzt; ?>
+            <?  if( $readonly ) { ?>
+              <span style='font-weight:bold;color:440000;'> --- !!! Datenbank ist schreibgeschuetzt !!!</span>";
+            <? } ?>
+          </td>
+        </tr>
+      </table>
+    <?
+    break;
+  default:   // anzeige in einem unterfenster
+    require_once( 'windows/head.php' );
+    if( is_readable( "windows/$window.php" ) )
+      include( "windows/$window.php" );
+    else
+      include( "$window.php" );
+    ?>
+      <form name='update_form' method='post' action='<? echo self_url(); ?>'>
+        <? echo self_post(); ?>
+        <input type='hidden' name='message' value=''>
+      </form>
+    <?
+    break;
 }
 
 ?>
-
-<table width='100%' class='footer'>
-  <tr>
-    <td style='padding-left:1em;text-align:left;'>aktueller Server: <kbd><? echo $foodsoftserver; ?></kbd></td>
-    <td style='padding-right:1em;text-align:right;'>
-    <? echo $mysqljetzt; ?>
-      <?  if( $readonly ) { ?>
-        <span style='font-weight:bold;color:440000;'> --- !!! Datenbank ist schreibgeschuetzt !!!</span>";
-      <? } ?>
-    </td>
-  </tr>
-</table>
 <form name='update_form' method='post' action='<? echo self_url(); ?>'>
   <? echo self_post(); ?>
   <input type='hidden' name='message' value=''>
 </form>
-<? echo $print_on_exit; ?>
+<?
+echo $print_on_exit;
+exit();
 
