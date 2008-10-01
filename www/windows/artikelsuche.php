fbb51a64511d596ab325a88cf5c823e3e7054829
Timo Felbinger <gittf@qipc.org>
2008-10-01 14:45:47 +0000

    HTML-code vereinfacht (weitgehend von Funktionen korrigiert), Syntax-Fehler im Suchfilter korrigiert, Formular zum A-Nr.-Setzen  korrigiert

--- www/windows/artikelsuche.php
+++ www/windows/artikelsuche.php
@@ -22,7 +22,7 @@ get_http_var( 'anummer', 'w', '' ) or $anummer = '';
 $anummer and $filter .= " AND artikelnummer='$anummer'";
 
 get_http_var( 'name', 'H', '' ) or $name = '';
-$name and $filter .= " AND name like '%'".mysql_real_escape_string($name)."%'";
+$name and $filter .= " AND name like '%".mysql_real_escape_string($name)."%' ";
 
 get_http_var( 'minpreis', 'f', 0 ) or $minpreis = 0;
 ( $minpreis > 0 ) and $filter .= " AND preis >= $minpreis";
@@ -58,190 +58,137 @@ if( $action == 'delete' ) {
   doSql( "DELETE FROM lieferantenkatalog WHERE katalogdatum = '$katalogdatum' and katalogtyp='$katalogtyp'" );
 }
 
-if( $editable and ( ! $produkt_id ) ) { ?>
-  <br>
-  <form class='small_form' action='index.php?window=terrakatalog_upload' method='post' enctype='multipart/form-data'>
-    <fieldset class='small_form'>
-      <legend> Kataloge </legend>
-      <table>
-        <tr>
-          <th> Katalog </th>
-          <th> Typ </th>
-          <th> Aktionen </th>
-        </tr>
-      <?
-        $kataloge = doSql( "
-          SELECT katalogdatum, katalogtyp
-          FROM lieferantenkatalog
-          WHERE lieferanten_id=$lieferanten_id
-          GROUP BY katalogdatum, katalogtyp
-          ORDER BY katalogtyp, katalogdatum
-        " );
-        while( $row = mysql_fetch_array( $kataloge ) ) { ?>
-          <tr>
-            <td>
-              <? echo $row['katalogdatum']; ?>
-            </td>
-            <td>
-              <? echo $row['katalogtyp']; ?>
-            </td>
-            <td>
-              <? echo fc_action( array(
-                'img' => 'img/b_drop.png', 'title' => 'Katalog l&ouml;schen'
-              , 'action' => 'delete', 'confirm' => 'Soll der Katalog wirklich GEL&Ouml;SCHT werden?'
-              , 'katalogdatum' => $row['katalogdatum'], 'katalogtyp' => $row['katalogtyp']
-              ) ); ?>
-            </td>
-          </tr>
-        <? } ?>
-      </table>
+if( $editable and ( ! $produkt_id ) ) {
+   $kataloge = doSql( "
+     SELECT katalogdatum, katalogtyp
+     FROM lieferantenkatalog
+     WHERE lieferanten_id=$lieferanten_id
+     GROUP BY katalogdatum, katalogtyp
+     ORDER BY katalogtyp, katalogdatum
+   " );
+  open_form( 'small_form', "enctype='multipart/form-data'", 'index.php?window=terrakatalog_upload' );
+    open_fieldset( 'small_form', '', 'Kataloge' );
+      open_table( 'list' );
+        open_th( '', '', 'Katalog' );
+        open_th( '', '', 'Typ' );
+        open_th( '', '', 'Aktionen' );
 
-      <h3> Neuen Katalog einlesen: </h3>
-      <table>
-        <tr>
-          <td> Datei (Format: .xls): <input type='file' name='terrakatalog'></input> </td>
-          <td> &nbsp; gueltig ab (Format: JJJJkwWW): <input type='text' name='terrakw' size='8'></input> </td>
-          <td> <input type='submit' value='start'> </td
-        </tr>
-      </table>
-    </fieldset>
-  </form>
-<? }
+        while( $row = mysql_fetch_array( $kataloge ) ) {
+          open_tr();
+            open_td( '', '', $row['katalogdatum'] );
+            open_td( '', '', $row['katalogtyp'] );
+            open_td( '', '', fc_action( array(
+                  'img' => 'img/b_drop.png', 'title' => 'Katalog l&ouml;schen'
+                , 'action' => 'delete', 'confirm' => 'Soll der Katalog wirklich GEL&Ouml;SCHT werden?'
+                , 'katalogdatum' => $row['katalogdatum'], 'katalogtyp' => $row['katalogtyp'] ) )
+            );
+        }
+      close_table();
 
+      ?> <h3> Neuen Katalog einlesen: </h3> <?
+      open_table();
+        open_td( '', '', "Datei (Format: .xls): <input type='file' name='terrakatalog'>" );
+        open_td( '', '', " &nbsp; gueltig ab (Format: JJJJkwWW): <input type='text' name='terrakw' size='8'>" );
+        open_td( '', '', " <input type='submit' value='start'>" );
+      close_table();
+    close_fieldset();
+  close_form();
+}
 
-?>
-<fieldset class='small_form'>
-  <legend>
-    <? if( $produkt_id ) { ?>
-      Katalogsuche nach Artikelnummer fuer <i><? echo $produktname; ?></i>:
-    <? } else { ?>
-      Artikelsuche im Katalog
-    <? } ?>
-  </legend>
-  <form method='post' class='small_form' action='<? echo self_url(); ?>'>
-  <? echo self_post(); ?>
-    <table>
-      <tr>
-        <td>
-          <label>Bestellnummer:</label>
-        </td><td>
-          <input type='text' name='bnummer' value='<? echo $bnummer; ?>' size='10'>
-          &nbsp;
-          <label>Artikelnummer:</label>
-          <input type='text' name='anummer' value='<? echo $anummer; ?>' size='10'>
-          &nbsp;
-          <label>Katalog:</label>
-          <select name='katalogtyp' size='1'>
-          <?
-            $kataloge = array( '', 'OG', 'Fr', 'Tr', 'drog' );
-            foreach ( $kataloge as $option ) {
-              echo "<option value='$option'";
-              if ( $katalogtyp == $option )
-                echo ' selected';
-              echo ">$option</option>";
-            }
+open_fieldset( 'small_form', '', $produkt_id ?  "Katalogsuche nach Artikelnummer fuer <i>$produktname</i>" : "Artikelsuche im Katalog" );
+
+  open_form( 'small_form' );
+    open_table();
+        open_td( '', '', '<label>Bestellnummer:</label>' );
+        open_td();
           ?>
-          </select>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <label>Bezeichnung:</label>
-        </td><td>
-          <input type='text' name='name' value='<? echo $name; ?>' size='60'>
-            (Jokerzeichen ist % (Prozent))
-        </td>
-      </tr>
-      <tr>
-        <td>
-          Preis (netto):
-        </td><td>
-          &nbsp; von: <input type='text' name='minpreis' value='<? printf( "%.2lf", $minpreis ); ?>' size='10'>
-            &nbsp; bis: <input type='text' name='maxpreis' value='<? printf( "%.2lf", $maxpreis ); ?>' size='10'>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <label>Limit:</label>
-        </td><td>
-          maximal <input type='text' name='limit' value='<? printf( "%u", $limit ); ?>' size='8'> Treffer anzeigen
-          <input style='margin-left:4em;' type='submit' value='Suche starten'>
-        </td>
-      </tr>
-    </table>
-  </form>
-
-<?
-
-if( $filter != '' ) {
-
-  $result = doSql( "
-    SELECT * FROM lieferantenkatalog WHERE lieferanten_id = $lieferanten_id
-    $filter
-    limit $limit
-  " );
-
-  if( $produkt_id ) {
-    ?>
-      <b>Zur Übernahme in die Produktdatenbank bitte auf Artikelnummer klicken!</b>
-      <form name='anummer_setzen' action="<? echo fc_url( 'produktpreise', "produkt_id=$produkt_id", '', 'action' ); ?>" method='post'>
-      <input type='hidden' name='action' value='artikelnummer_setzen'>
-      <input type='hidden' name='button_id' value=''>
-    <?
-  }
-
-  ?>
-  <h3> <? echo mysql_num_rows($result); ?> Treffer (Limit: <? echo $limit; ?>)</h3>
-  <table class='list'>
-    <tr>
-      <th>A-Nr.</th>
-      <th>B-Nr.</th>
-      <th>Bezeichnung</th>
-      <th>Gebinde</th>
-      <th>Einheit</th>
-      <th>Land</th>
-      <th>Verband</th>
-      <th>Netto</th>
-      <th>MWSt</th>
-      <th>Brutto</th>
-      <th>Katalog</th>
-    </tr>
-
-    <? while( $row = mysql_fetch_array( $result ) ) {
-      $netto = $row['preis'];
-      $mwst = $row['mwst'];
-      $brutto = $netto * (1 + $mwst / 100.0 );
-      ?>
-      <tr>
-        <td class='mult'>
+            <input type='text' name='bnummer' value='<? echo $bnummer; ?>' size='10'>
+            &nbsp;
+            <label>Artikelnummer:</label>
+            <input type='text' name='anummer' value='<? echo $anummer; ?>' size='10'>
+            &nbsp;
+            <label>Katalog:</label>
+            <select name='katalogtyp' size='1'>
+              <? echo optionen( array( '', 'OG', 'Fr', 'Tr', 'drog' ), $katalogtyp ); ?>
+            </select>
           <?
+      open_tr();
+        open_td( '', '', '<label>Bezeichnung:</label>' );
+        open_td( '', '', " &nbsp; <input type='text' name='name' value='$name' size='60'> &nbsp; (Jokerzeichen ist % (Prozent))" );
+
+      open_tr();
+        open_td( '', '', 'Preis (netto):' );
+        open_td();
+          ?> &nbsp; von: <input type='text' name='minpreis' value='<? printf( "%.2lf", $minpreis ); ?>' size='10'>
+              &nbsp; bis: <input type='text' name='maxpreis' value='<? printf( "%.2lf", $maxpreis ); ?>' size='10'> <?
+      open_tr();
+        open_td( '', '', '<label>Limit:</label>' );
+        open_td();
+          ?> maximal <input type='text' name='limit' value='<? printf( "%u", $limit ); ?>' size='8'> Treffer anzeigen
+            <input style='margin-left:4em;' type='submit' value='Suche starten'> <?
+    close_table();
+  close_form();
+
+  if( $filter != '' ) {
+  
+    $result = doSql( "
+      SELECT * FROM lieferantenkatalog WHERE lieferanten_id = $lieferanten_id
+      $filter
+      limit $limit
+    " );
+
+    if( $produkt_id ) {
+      open_form( '', "name='anummer_setzen'", fc_url( 'produktpreise', "produkt_id=$produkt_id", '', 'action' ) );
+      ?>
+        <input type='hidden' name='postform_id' value='<? echo $self_fields['postform_id']; ?>'>
+        <input type='hidden' name='action' value='artikelnummer_setzen'>
+        <input type='hidden' name='button_id' value=''>
+        <div style='font-weight:bold;'>Zur Übernahme in die Produktdatenbank bitte auf Artikelnummer klicken!</div>
+      <?
+    }
+
+    ?> <h3> <? echo mysql_num_rows($result); ?> Treffer (Limit: <? echo $limit; ?>) </h3><?
+    open_table( 'list' );
+      open_th( '', '', 'A-Nr.' );
+      open_th( '', '', 'B-Nr.' );
+      open_th( '', '', 'Bezeichnung' );
+      open_th( '', '', 'Gebinde' );
+      open_th( '', '', 'Einheit' );
+      open_th( '', '', 'Land' );
+      open_th( '', '', 'Verband' );
+      open_th( '', '', 'Netto' );
+      open_th( '', '', 'MWSt' );
+      open_th( '', '', 'Brutto' );
+      open_th( '', '', 'Katalog' );
+
+      while( $row = mysql_fetch_array( $result ) ) {
+        $netto = $row['preis'];
+        $mwst = $row['mwst'];
+        $brutto = $netto * (1 + $mwst / 100.0 );
+        open_tr();
+          open_td( 'mult' );
             $anummer = $row['artikelnummer'];
             if ( $produkt_id > 0 ) {
               echo fc_button( 'produktdetails', "text=$anummer,class=submit,form=anummer_setzen,button_id=$anummer" );
             } else {
               echo $anummer;
             }
-          ?>
-        </td>
-        <td class='number'><? echo $row['bestellnummer']; ?></td>
-        <td><? echo $row['name']; ?></td>
-        <td class='mult'><? echo $row['gebinde']; ?></td>
-        <td class='unit'><? echo $row['liefereinheit']; ?></td>
-        <td><? echo $row['herkunft']; ?></td>
-        <td><? echo $row['verband']; ?></td>
-        <td class='mult'><? printf( "%.2lf", $netto ); ?></td>
-        <td class='mult'><? printf( "%.2lf", $mwst ); ?></td>
-        <td class='mult'><? printf( "%.2lf", $brutto ); ?></td>
-        <td><? echo "{$row['katalogtyp']} / {$row['katalogdatum']}"; ?></td>
-      </tr>
-    <? } ?>
+          open_td( 'number', '', $row['bestellnummer'] );
+          open_td( '', '', $row['name'] );
+          open_td( '', '', $row['gebinde'] );
+          open_td( 'unit', '', $row['liefereinheit'] );
+          open_td( '', '', $row['herkunft'] );
+          open_td( '', '', $row['verband'] );
+          open_td( '', '', sprintf( "%.2lf", $netto ) );
+          open_td( 'mult', '', sprintf( "%.2lf", $mwst ) );
+          open_td( 'mult', '', sprintf( "%.2lf", $brutto ) );
+          open_td( '', '',  "{$row['katalogtyp']} / {$row['katalogdatum']}" );
+      }
+    close_table();
+    if( $produkt_id )
+      close_form();
+  }
 
-  </table>
-  <?
-  if( $produkt_id )
-    echo "</form>";
-}
+close_fieldset();
+
 ?>
-
-</fieldset>
-
