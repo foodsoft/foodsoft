6b6f43a8cab1124edce266b15caef0aee8a4cdcc
Timo Felbinger <gittf@qipc.org>
2008-10-02 09:22:32 +0000

    lieferanten.php ueberarbeitet: HTML-code ausgelagert

--- www/windows/lieferanten.php
+++ www/windows/lieferanten.php
@@ -16,73 +16,57 @@ if( $action == 'delete' ) {
 }
 $result = sql_lieferanten();
 
-?>
-<table class='menu' style='margin-bottom:2em;'>
-  <? if( $editable ) { ?>
-    <tr>
-      <td><? echo fc_button( 'edit_lieferant', "text=Neuer Lieferant" ); ?></td>
-      <td valign=middle'>einen neuen Lieferanten hinzuf&uuml;gen...</td>
-    </tr>
-  <? } ?>
-  <tr>
-     <td><? echo fc_button( 'self', "text=Aktualisieren" ); ?></td>
-     <td valign=middle'>diese Seite neu laden...</td>
-  </tr><tr>
-     <td><? echo fc_button( 'index', "text=Beenden" ); ?></td>
-     <td valign=middle'>diesen Bereich verlassen...</td>
-  </tr>
-</table>
+open_table( 'menu', "style='margin-bottom:2em;'" );
+  if( $editable ) {
+      open_td( '', '', fc_button( 'edit_lieferant', "text=Neuer Lieferant" ) );
+      open_td( '', '', 'einen neuen Lieferanten hinzuf&uuml;gen...' );
+  }
+  open_tr();
+     open_td( '', '', fc_button( 'self', "text=Aktualisieren" ) );
+     open_td( '', '', 'diese Seite neu laden...' );
+  open_tr();
+     open_td( '', '', fc_button( 'index', "text=Beenden" ) );
+     open_td( '', '', 'diesen Bereich verlassen...' );
+close_table();
 
-<table class='list'>
-  <tr>
-    <th>Name</th>
-    <th>Telefon</th>
-    <th>Fax</th>
-    <th>Mail</th>
-    <th>Webadresse</th>
-    <th>Kontostand</th>
-    <th>Optionen</th>
-  </tr>
+open_table('list');
+  open_th('','','Name');
+  open_th('','','Telefon');
+  open_th('','','Fax');
+  open_th('','','Mail');
+  open_th('','','Webadresse');
+  open_th('','','Kontostand');
+  open_th('','','Optionen');
 
-<?
 while ($row = mysql_fetch_array($result)) {
   $lieferanten_id=$row['id'];
   $kontostand = lieferantenkontostand( $row['id'] );
-  ?>
-    <tr>
-      <td><b><? echo $row['name']; ?></b></td>
-      <td><? echo $row['telefon']; ?></td>
-      <td><? echo $row['fax']; ?></td>
-      <td><? echo $row['mail']; ?></td>
-      <td>
-        <?
-        if( $row['url'] )
-          echo "<a href='{$row['url']}' title='zur Webseite des Lieferanten' target='_new'>{$row['url']}</a>";
-        else
-          echo "-";
-        ?>
-      </td>
-      <td class='number'><? printf( "%.2lf", $kontostand ); ?></td>
-      <td style='white-space:nowrap;'>
-    <?
-    echo fc_alink( 'lieferantenkonto', "lieferanten_id=$lieferanten_id,text=" );
-    echo fc_alink( 'pfandzettel', "lieferanten_id=$lieferanten_id" );
-    if( $editable ) {
-      echo fc_alink( 'edit_lieferant', "lieferanten_id=$lieferanten_id" );
-      if( ( references_lieferant($lieferanten_id) == 0 ) and ( abs($kontostand) < 0.005 ) ) {
-        echo fc_action( array(
-          'img' => 'img/b_drop.png', 'title' => 'Lieferanten l&ouml;schen'
-        , 'confirm' => 'Soll der Lieferant wirklich GEL&Ouml;SCHT werden?'
-        , 'action' => 'delete', 'lieferanten_id' => $lieferanten_id
-        ) );
+  open_tr();
+    open_td('','', $row['name'] );
+    open_td('','', $row['telefon'] );
+    open_td('','', $row['fax'] );
+    open_td('','', $row['mail'] );
+    if( $row['url'] )
+      open_td('','',"<a href='{$row['url']}' title='zur Webseite des Lieferanten' target='_new'>{$row['url']}</a>" );
+    else
+      open_td('','','-');
+    open_td('number','', sprintf( "%.2lf", $kontostand ) );
+    open_td('oneline','');
+      echo fc_alink( 'lieferantenkonto', "lieferanten_id=$lieferanten_id,text=" );
+      echo fc_alink( 'pfandzettel', "lieferanten_id=$lieferanten_id" );
+      if( $editable ) {
+        echo fc_alink( 'edit_lieferant', "lieferanten_id=$lieferanten_id" );
+        if( ( references_lieferant($lieferanten_id) == 0 ) and ( abs($kontostand) < 0.005 ) ) {
+          echo fc_action( array(
+            'img' => 'img/b_drop.png', 'title' => 'Lieferanten l&ouml;schen'
+          , 'confirm' => 'Soll der Lieferant wirklich GEL&Ouml;SCHT werden?'
+          , 'action' => 'delete', 'lieferanten_id' => $lieferanten_id
+          ) );
+        }
+      } else {
+        echo fc_alink( 'edit_lieferant', "lieferanten_id=$lieferanten_id,ro=1,img=img/birne_rot.png" );
       }
-    } else {
-      echo fc_alink( 'edit_lieferant', "lieferanten_id=$lieferanten_id,ro=1,img=img/birne_rot.png" );
-    }
-    ?> </td></tr> <?
-  }
+}
+close_table();
 
 ?>
-
-</table>
-
