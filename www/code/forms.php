f3b37f8a3c418059bb8d37b5437f56b6ecf9d288
Timo Felbinger <gittf@qipc.org>
2008-10-16 15:43:20 +0000

    - Formulare (bisher: Konto-Buchungen und einige andere) und - zugehoerige Aktionen (bisher nur wenige :-) in eigenes skript (code/forms.php) ausgelagert

---
+++ www/code/forms.php
@@ -0,0 +1,331 @@
+<?
+
+// functions to output one row of a form
+//
+// if a $fieldname is alread part of $self_fields (ie, part of the current view), the value
+// will just be printed and cannot be modified (only applies to types that can be in $self_fields).
+// the line will not be closed; so e.g. a submission_button() can be appended to the last row
+//
+
+function form_row_konto( $label = 'Konto:', $fieldname = 'konto_id', $initial = 0 ) {
+  open_tr();
+    open_td( 'label', '', $label );
+    if( ( $konto_id = self_field( $fieldname ) ) === NULL )
+      $konto_id = $initial;
+    else
+      $fieldname = false;
+    open_td( 'kbd' ); echo konto_view( $konto_id, $fieldname );
+}
+
+function form_row_kontoauszug( $label = 'Kontoauszug:', $fieldname = 'auszug', $initial_jahr = 0, $initial_nr = 0 ) {
+  open_tr();
+    open_td( 'label', '', $label );
+    $auszug_jahr = self_field( $fieldname.'_jahr' );
+    $auszug_nr = self_field( $fieldname.'_nr' );
+    if( $auszug_jahr !== NULL and $auszug_nr !== NULL )
+      $fieldname = false;
+    if( $auszug_jahr === NULL )
+      $auszug_jahr = $initial_jahr;
+    if( $auszug_nr === NULL )
+      $auszug_nr = $initial_nr;
+    open_td( 'kbd' ); echo kontoauszug_view( 0, $auszug_jahr, $auszug_nr, $fieldname );
+}
+
+function form_row_gruppe( $label = 'Gruppe:', $fieldname = 'gruppen_id', $initial = 0 ) {
+  open_tr();
+    open_td('label', '', $label );
+    if( ( $gruppen_id = self_field( $fieldname ) ) === NULL )
+      $gruppen_id = $initial;
+    else
+      $fieldname = false;
+    open_td( 'kbd' ); echo gruppe_view( $gruppen_id, $fieldname );
+}
+
+function form_row_lieferant( $label = 'Lieferant:', $fieldname = 'lieferanten_id', $initial = 0 ) {
+  open_tr();
+    open_td('label', '', $label );
+    if( ( $lieferant_id = self_field( $fieldname ) ) === NULL )
+      $lieferant_id = $initial;
+    else
+      $fieldname = false;
+    open_td( 'kbd' ); echo lieferant_view( $lieferant_id, $fieldname );
+}
+
+function form_row_date( $label, $fieldname, $initial = 0 ) {
+  $year = self_field( $fieldname.'_jahr' );
+  $month = self_field( $fieldname.'_monat' );
+  $day = self_field( $fieldname.'_tag' );
+  if( ($year !== NULL) and ($day !== NULL) and ($month !== NULL) ) {
+    $date = "$year-$month-$day";
+    $fieldname = false;
+  } else {
+    $date = $initial;
+  }
+  open_tr();
+    open_td( 'label', '', $label );
+    open_td( 'kbd' ); echo date_view( $date, $fieldname );
+}
+
+function form_row_betrag( $label = 'Betrag:' , $fieldname = 'betrag', $initial = 0.0 ) {
+  open_tr();
+    open_td( 'label', '', $label );
+    open_td( 'kbd number' ); echo price_view( $initial, $fieldname );
+}
+
+function form_row_text( $label = 'Notiz:', $fieldname = 'notiz', $size = 60, $initial = '' ) {
+  open_tr();
+    open_td( 'label', '', $label );
+    open_td( 'kbd' ); echo string_view( $initial, $size, $fieldname );
+}
+
+
+
+function form_finish_transaction( $transaction_id, $konto_id = 0, $auszug_jahr = 0, $auszug_nr = 0 ) {
+  $trans = sql_get_transaction( $transaction_id );
+  open_form('', '', '', "action=finish_transaction,transaction_id=$transaction_id" );
+    open_table('layout');
+      $konto_id or form_row_konto();
+      ( $auszug_jahr and $auszug_nr ) or form_row_auszug();
+      form_row_date( 'Valuta:', 'valuta' );
+      submission_button( 'Best&auml;tigen' );
+    close_table();
+  close_form();
+}
+
+function action_finish_transaction() {
+  global $transaction_id, $konto_id, $auszug_jahr, $auszug_nr, $valuta_day, $valuta_month, $valuta_year;
+  global $dienstkontrollblatt_id;
+  need_http_var( 'transaction_id', 'U' );
+  need_http_var( 'auszug_jahr', 'U' );
+  need_http_var( 'auszug_nr', 'U' );
+  need_http_var( 'konto_id', 'U' );
+  need_http_var( 'valuta_day', 'U' );
+  need_http_var( 'valuta_month', 'U' );
+  need_http_var( 'valuta_year', 'U' );
+
+  fail_if_readonly();
+  nur_fuer_dienst(4);
+
+  $soll_id = -$transaction_id;
+  $soll_transaction = sql_get_transaction( $soll_id );
+
+  $haben_id = sql_bank_transaktion(
+    $konto_id, $auszug_jahr, $auszug_nr
+  , $soll_transaction['soll'], "$valuta_year-$valuta_month-$valuta_day"
+  , $dienstkontrollblatt_id, $notiz, 0
+  );
+
+  sql_link_transaction( $soll_id, $haben_id );
+
+  return sql_update( 'gruppen_transaktion', $transaction_id, array(
+    'dienstkontrollblatt_id' => $dienstkontrollblatt_id
+  ) );
+}
+
+
+
+
+function formular_buchung_gruppe_bank( $notiz_initial = 'Einzahlung' ) {
+  open_form( 'small_form', '', '', 'action=buchung_gruppe_bank' );
+    open_fieldset( 'small_form', '', 'Einzahlung / Auszahlung Gruppe' );
+      open_table('layout');
+        form_row_gruppe();
+        form_row_konto();
+        form_row_kontoauszug();
+        form_row_date( 'Valuta:', 'valuta' );
+        tr_title( 'Betrag: positiv bei Einzahlung, negativ bei Auszahlung' );
+        form_row_betrag( 'Haben Konto:' );
+        form_row_text( 'Notiz:', 'notiz', 60, $notiz_initial );
+        quad();
+        submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+function formular_buchung_lieferant_bank( $notiz_initial = 'Abbuchung Lieferant' ) {
+  open_form( 'small_form', '', '', 'action=buchung_lieferant_bank' );
+    open_fieldset( '', '', 'Überweisung / Lastschrift Lieferant' );
+      open_table('layout');
+        form_row_lieferant();
+        form_row_konto();
+        form_row_kontoauszug();
+        form_row_date( 'Valuta:', 'valuta' );
+        tr_title( 'Betrag: positiv bei Einzahlung, negativ bei Auszahlung/Lastschrift' );
+        form_row_betrag( 'Haben Konto:' );
+        form_row_text( 'Notiz:', 'notiz', 60, $notiz_initial );
+        quad();
+        submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+function formular_buchung_gruppe_lieferant( $notiz_initial = 'Zahlung an Lieferant' ) {
+  open_form( 'small_form', '', '', 'action=buchung_gruppe_lieferant' );
+    open_fieldset( 'small_form', '', 'Zahlung von Gruppe an Lieferant' );
+      open_table('layout');
+        form_row_gruppe();
+        form_row_lieferant();
+        form_row_date( 'Valuta:', 'valuta' );
+        tr_title( 'Betrag: positiv: Zahlung an Lieferant / negativ: Zahlung an Gruppe' );
+        form_row_betrag( 'Haben Lieferant:' );
+        form_row_text( 'Notiz:', 'notiz', 60, $notiz_initial );
+        quad();
+        submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+function formular_buchung_gruppe_gruppe( $notiz_initial = 'Umbuchung' ) {
+  open_form( 'small_form', '', '', 'action=buchung_gruppe_gruppe' );
+    open_fieldset( '', '', 'Umbuchung von Gruppe an Gruppe' );
+      open_table('layout');
+        form_row_gruppe( 'von Gruppe:', 'gruppen_id' );
+        form_row_gruppe( 'an Gruppe:', 'nach_gruppen_id' );
+        form_row_date( 'Valuta:', 'valuta' );
+        form_row_betrag( 'Haben Lieferant:' );
+        form_row_text( 'Notiz:', 'notiz', 60, $notiz_initial );
+        quad();
+        submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+function formular_buchung_bank_bank( $notiz_initial = 'Überweisung' ) {
+  open_form( 'small_form', '', '', 'action=buchung_gruppe_gruppe' );
+    open_fieldset( '', '', 'Überweisung von Konto zu Konto' );
+      open_table('layout');
+        form_row_konto( 'von Konto:', 'konto_id' );
+        form_row_kontoauszug( "<div class='right'>Auszug:</div>", 'auszug' );
+        form_row_konto( 'an Konto:', 'nach_konto_id' );
+        form_row_kontoauszug( "<div class='right'>Auszug:</div>", 'nach_auszug' );
+        form_row_date( 'Valuta:', 'valuta' );
+        form_row_betrag( 'Haben Lieferant:' );
+        form_row_text( 'Notiz:', 'notiz', 60, $notiz_initial );
+        quad();
+        submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+    
+
+function formular_buchung_bank_sonderausgabe() {
+  open_form( 'small_form', '', '', 'action=buchung_bank_sonderausgabe' );
+    open_fieldset( '', '', 'Überweisung Sonderausgabe' );
+      open_table('layout');
+        form_row_konto( 'von Konto:', 'konto_id' );
+        form_row_kontoauszug( "<div class='right'>Auszug:</div>", 'auszug' );
+        form_row_date( 'Valuta:', 'valuta' );
+        tr_title( 'positiv: Gewinn der FC / negativ: Verlust der FC' );
+        form_row_betrag( 'Haben FC:' );
+        form_row_text( 'Notiz:', 'notiz', 60 );
+        quad();
+        submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+function formular_buchung_gruppe_sonderausgabe() {
+  open_form( 'small_form', '', '', 'action=buchung_gruppe_sonderausgabe' );
+    open_fieldset( '', '', 'Sonderausgabe durch eine Gruppe' );
+      open_table('layout');
+        form_row_gruppe();
+        form_row_date( 'Valuta:', 'valuta' );
+        tr_title( 'positiv: Gewinn der FC / negativ: Verlust der FC' );
+        form_row_betrag( 'Haben FC:' );
+        form_row_text( 'Notiz:', 'notiz', 60 );
+        quad();
+        submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+function formular_umbuchung_verlust( $typ = 0 ) {
+  open_form( 'small_form', '', '', "action=umbuchung_verlust,typ=$typ" );
+    open_fieldset( '', '', 'Umbuchung Verlustausgleich' );
+      open_table('layout');
+          open_td( 'label', '', 'von:' );    
+          open_td( 'kbd' );
+            if( $typ ) { 
+              need( in_array( $typ, array( TRANSAKTION_TYP_SPENDE, TRANSAKTION_TYP_UMLAGE ) ) );
+              echo transaktion_typ_string( $typ );
+              hidden_input( 'von_typ', $typ );
+            } else {
+              open_select( 'von_typ' );
+                ?> <option value=''>(bitte Quelle w&auml;hlen)</option> <?
+                foreach( array( TRANSAKTION_TYP_SPENDE , TRANSAKTION_TYP_UMLAGE ) as $t ) {
+                   ?> <option value='<? echo $t; ?>'><? echo transaktion_typ_string($t); ?></option> <?
+                 }
+              close_select();
+            }
+        open_tr();
+          open_td( 'label', '', 'nach:' );    
+          open_td( 'kbd' );
+            open_select( 'nach_typ' );
+              ?> <option value=''>(bitte Ziel w&auml;hlen)</option> <?
+              foreach( array( TRANSAKTION_TYP_AUSGLEICH_ANFANGSGUTHABEN
+                            , TRANSAKTION_TYP_AUSGLEICH_SONDERAUSGABEN
+                            , TRANSAKTION_TYP_AUSGLEICH_BESTELLVERLUSTE ) as $t ) {
+                ?> <option value='<? echo $t; ?>'><? echo transaktion_typ_string($t); ?></option> <?
+              }
+            close_select();
+        form_row_date( 'Valuta:', 'valuta' );
+        form_row_betrag( 'Betrag:' );
+        form_row_text( 'Notiz:', 'notiz', 60 );
+          quad();
+          submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+function formular_gruppen_umlage() {
+  open_form( 'small_form', '', '', 'action=buchung_umlage' );
+    open_fieldset( '', '', 'Verlustumlage auf Gruppenmitglieder' );
+      open_table( 'layout' );
+          open_td( '', "colspan='2'", "Von <span class='bold italic'>allen aktiven Bestellgruppen</span> eine Umlage" );
+        form_row_betrag( 'in Höhe von' );
+          echo " EUR je Gruppenmitglied erheben";
+        form_row_date( 'Valuta:', 'valuta' );
+        form_row_text( 'Notiz:', 'notiz', 60 );
+          quad();
+          submission_button( 'OK' );
+      close_table();
+    close_fieldset();
+  close_form();
+}
+
+
+function mod_onclick( $id ) {
+  return $id ? " onclick=\"document.getElementById('$id').className='modified';\" " : '';
+}
+
+function formular_artikelnummer( $produkt_id, $toggle = false, $mod_id = false ) {
+  $produkt = sql_produkt_details( $produkt_id );
+  $anummer = $produkt['artikelnummer'];
+  $lieferanten_id = $produkt['lieferanten_id'];
+
+  open_fieldset( 'small_form', '', "Artikelnummer ($anummer) &auml;ndern", $toggle );
+    open_table( 'layout' );
+        open_td( '', '', 'neue Artikel-Nr. setzen:' );
+          open_form( 'small_form', '', '', 'action=artikelnummer_setzen' );
+            echo string_view( $anummer, 20, 'anummer' );
+            submission_button();
+          close_form();
+      open_tr();
+        open_td( '', '', '...oder: Katalogsuche nach:' );
+          open_form('small_form','','', "produkt_id=$produkt_id,lieferanten_id=$lieferanten_id");
+            echo string_view( $produkt['name'], 40, 'name' );
+            echo fc_link( 'artikelsuche', 'text=Los!,form,class=button' );
+          close_form();
+    close_table();
+  close_fieldset();
+}
+
+?>
