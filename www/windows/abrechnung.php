46354d179f401d526e91bc9649ba4ce4c76088a1
Timo Felbinger <gittf@qipc.org>
2008-10-21 13:04:45 +0000

    Kosmetik: Code konsistent eingerueckt, Querverweise zum Lieferante eingebaut view_*-Funktionen einsetzen

--- www/windows/abrechnung.php
+++ www/windows/abrechnung.php
@@ -53,13 +53,11 @@ if( $action == 'save' ) {
 
 $bestellung = sql_bestellung( $bestell_id );
 $status = getState( $bestell_id );
-$ro_tag = '';
-if( ! $editable or ( $status >= STATUS_ABGERECHNET ) ) {
-  $ro_tag = 'readonly';
+if( $status >= STATUS_ABGERECHNET ) {
+  $editable = false;
 }
 
-$result = sql_gruppenpfand( $lieferant_id, $bestell_id, "gesamtbestellungen.id" );
-$gruppenpfand = $result[0];
+$gruppenpfand = current( sql_gruppenpfand( $lieferant_id, $bestell_id, "gesamtbestellungen.id" ) );
 
 $lieferanten_soll = sql_bestellung_soll_lieferant( $bestell_id );
 
@@ -68,153 +66,150 @@ $warenwert_muell_brutto = muell_wert_brutto( $bestell_id );
 $warenwert_basar_brutto = basar_wert_brutto( $bestell_id ); 
 
 
-?><h2>Abrechnung: Bestellung <?
-  echo "$bestellung_name ($lieferant_name) ". fc_alink( 'edit_bestellung', "bestell_id=$bestell_id" );
-?></h2><?
+open_fieldset( '', "'style='padding:1em;'", "Abrechnung: Bestellung $bestellung_name"
+                                 . fc_link( 'edit_bestellung', "bestell_id=$bestell_id" )
+                                 . " / Lieferant: " .lieferant_view( $lieferant_id ) );
 
-open_form();
-  ?><input type='hidden' name='action' value='save'><?
+if( $editable )
+  open_form( '', '', '', 'action=save' );
 
-  open_table( 'list' );
-      open_th( '', '', 'Abrechnungsschritt' );
-      open_th( '', '', 'Details' );
-      open_th( '', '', 'Netto' );
-      open_th( '', '', 'Brutto' );
-      open_th( '', '', 'Aktionen' );
+open_table( 'list', "style='width:98%'" );
+  open_th( '', '', 'Abrechnungsschritt' );
+  open_th( '', '', 'Details' );
+  open_th( '', '', 'Netto' );
+  open_th( '', '', 'Brutto' );
+  open_th( '', '', 'Aktionen' );
 
-    //
-    // gruppennteil:
-    //
-    open_tr();
-      open_th( '', "colspan='5' style='padding-top:2em;'", 'Bestellgruppen:' );
+  //
+  // gruppennteil:
+  //
+  open_tr();
+    open_th( '', "colspan='5' style='padding-top:2em;'", 'Bestellgruppen:' );
 
-    open_tr();
-      open_td( '', '', 'Basarkäufe eintragen:' );
-      open_td( '', '', 'Reste im Basar:' );
-      open_td();
-      open_td( 'bold number', sprintf( "%.2lf", $warenwert_basar_brutto ) );
-      open_td( '', "style='vertical-align:bottom;'", fc_alink( 'basar', "text=zum Basar...,img=" ) );
-    open_tr();
-      open_td( '', "rowspan='2'", "Verteilmengen abgleichen:" );
-      open_td( 'right', '', 'Warenwert Gruppen:' );
-      open_td();
-      open_td( 'bold number', '', sprintf( "%.2lf", $warenwert_verteilt_brutto ) );
-      open_td( '', " rowspan='2' style='vertical-align:middle;'",
-               fc_alink( 'verteilliste', "bestell_id=$bestell_id,text=zur Verteilliste...,img=" ) );
-    open_tr();
-      open_td( 'right', '', 'auf den Müll gewandert:' );
-      open_td();
-      open_td( 'bold number', '', sprintf( "%.2lf", $warenwert_muell_brutto ) );
+  open_tr();
+    open_td( '', '', 'Basarkäufe eintragen:' );
+    open_td( '', '', 'Reste im Basar:' );
+    open_td();
+    open_td( 'bold number', '', price_view( $warenwert_basar_brutto ) );
+    open_td( 'bottom', '', fc_link( 'basar', "text=zum Basar...,class=href" ) );
+  open_tr();
+    open_td( '', "rowspan='2'", "Verteilmengen abgleichen:" );
+    open_td( 'right', '', 'Warenwert Gruppen:' );
+    open_td();
+    open_td( 'bold number', '', price_view( $warenwert_verteilt_brutto ) );
+    open_td( 'middle', "rowspan='2'",
+             fc_link( 'verteilliste', "bestell_id=$bestell_id,text=zur Verteilliste...,class=href" ) );
+  open_tr();
+    open_td( 'right', '', 'auf den Müll gewandert:' );
+    open_td();
+    open_td( 'bold number', '', price_view( $warenwert_muell_brutto ) );
 
-    open_tr( 'summe' );
-      open_td( '', "colspan='3'", 'Summe' );
-      open_td( 'number', '', sprintf( "%.2lf", $warenwert_verteilt_brutto + $warenwert_muell_brutto + $warenwert_basar_brutto ) );
-      open_td();
-
-if( $lieferant['anzahl_pfandverpackungen'] > 0 ) {
-    open_tr();
-      open_td( '', "rowspan='2'", 'Pfandabrechnung Bestellgruppen:' );
-      open_td( 'right', '', 'berechnet (Kauf):' );
-      open_td();
-      open_td( 'bold number', '', sprintf( "%.2lf", -$gruppenpfand['pfand_voll_brutto_soll'] ) );
-      open_td( '', "rowspan='2' style='vertical-align:middle;'",
-               fc_alink( 'gruppenpfand', "bestell_id=$bestell_id,img=,text=zur Pfandabrechnung..." ) );
-    open_tr();
-      open_td( 'right', '', 'gutgeschrieben (Rückgabe):' );
-      open_td();
-      open_td( 'bold number', '', sprintf( "%.2lf", -$gruppenpfand['pfand_leer_brutto_soll'] ) );
-}
-
-    //
-    // lieferantenteil:
-    //
-    open_tr();
-      open_th( '', "colspan='5' style='padding-top:2em;'" );
-        echo "Lieferant: $lieferant_name";
-        ?> <div> Rechnungsnummer des Lieferanten: <?
-           if( $readonly ) { 
-             echo $bestellung['rechnungsnummer'];
-           } else {
-             ?> <input type='text' size='40' name='rechnungsnummer' value='<? echo $bestellung['rechnungsnummer']; ?>'> <?
-           }
-        ?> </div> <?
-
-    open_tr();
-      open_td( '', '', 'Liefermengen und -preise abgleichen:' );
-      open_td( 'right', '', 'Warenwert:' );
-      open_td( 'bold number', '', sprintf( "%.2lf", $lieferanten_soll['waren_netto_soll'] ) );
-      open_td( 'bold number', '', sprintf( "%.2lf", $lieferanten_soll['waren_brutto_soll'] ) );
-      open_td( '', "style='vertical-align:bottom;'",
-                fc_alink( 'lieferschein', "bestell_id=$bestell_id,img=,text=zum Lieferschein..." ) );
+  open_tr( 'summe' );
+    open_td( '', "colspan='3'", 'Summe' );
+    open_td( 'number', '', price_view( $warenwert_verteilt_brutto + $warenwert_muell_brutto + $warenwert_basar_brutto ) );
+    open_td();
 
 if( $lieferant['anzahl_pfandverpackungen'] > 0 ) {
-    open_tr();
-      open_td( '', "rowspan='2'", "Pfandabrechnung Lieferant: <div class='small'>(falls zutreffend, etwa bei Terra!)</div>" );
-      open_td( 'right', '', 'berechnet (Kauf):' );
-      open_td( 'bold number', '', sprintf( "%.2lf", $lieferanten_soll['pfand_voll_netto_soll'] ) );
-      open_td( 'bold number', '', sprintf( "%.2lf", $lieferanten_soll['pfand_voll_brutto_soll'] ) );
-      open_td( '', "rowspan='2' style='vertical-align:middle;'",
-               fc_alink( 'pfandzettel', "bestell_id=$bestell_id,lieferanten_id=$lieferant_id,img=,text=zum Pfandzettel..." ) );
-
-    open_tr();
-      open_td( 'right', '', 'gutgeschrieben (Rückgabe):' );
-      open_td( 'bold number', '', sprintf( "%.2lf", $lieferanten_soll['pfand_leer_netto_soll'] ) );
-      open_td( 'bold number', '', sprintf( "%.2lf", $lieferanten_soll['pfand_leer_brutto_soll'] ) );
+  open_tr();
+    open_td( '', "rowspan='2'", 'Pfandabrechnung Bestellgruppen:' );
+    open_td( 'right', '', 'berechnet (Kauf):' );
+    open_td();
+    open_td( 'bold number', '', price_view( -$gruppenpfand['pfand_voll_brutto_soll'] ) );
+    open_td( 'middle', "rowspan='2'",
+             fc_link( 'gruppenpfand', "bestell_id=$bestell_id,class=href,text=zur Pfandabrechnung..." ) );
+  open_tr();
+    open_td( 'right', '', 'gutgeschrieben (Rückgabe):' );
+    open_td();
+    open_td( 'bold number', '', price_view( -$gruppenpfand['pfand_leer_brutto_soll'] ) );
 }
-    open_tr( 'summe' );
-      open_td( '', "colspan='2'", 'Zwischensumme:' );
-      open_td( 'number', '', sprintf( "%.2lf", $lieferanten_soll['waren_netto_soll']
-                                             + $lieferanten_soll['pfand_leer_netto_soll']
-                                             + $lieferanten_soll['pfand_voll_netto_soll'] ) );
-      open_td( 'number', '', sprintf( "%.2lf", $lieferanten_soll['waren_brutto_soll']
-                                             + $lieferanten_soll['pfand_leer_brutto_soll']
-                                             + $lieferanten_soll['pfand_voll_brutto_soll'] ) );
-      open_td( '', "colspan='2'" );
 
-    open_tr();
-      open_td( '', "colspan='3'" );
-        ?> Sonstiges: <br><?
-        if( $readonly ) {
-          echo $bestellung['extra_text'];
-        } else {
-          ?> <input type='text' name='extra_text' size='40' value='<? echo $bestellung['extra_text']; ?>'> <?
-        }
-      open_td( 'number', "style='text-align:right;vertical-align:bottom;'" );
-        if( $readonly ) {
-          printf( "%.2lf", $bestellung['extra_soll'] );
-        } else {
-          ?> <input style='text-align:right;' type='text' name='extra_soll' size='10' value='<? printf( "%.2lf", $bestellung['extra_soll'] ); ?>'> <?
-        }
+  //
+  // lieferantenteil:
+  //
+  open_tr();
+    open_th( 'bigskip', "colspan='5'" );
+      echo "Lieferant: $lieferant_name";
+      open_div( 'oneline' );
+        ?> Rechnungsnummer des Lieferanten: <?  qquad();
+        echo string_view( $bestellung['rechnungsnummer'], 40, ( $editable ? 'rechnungsnummer' : false ) );
+      close_div();
 
-    open_tr( 'summe' );
-      open_td( '', "colspan='3'", 'Summe:' );
-      open_td( 'number', '', sprintf( "%.2lf", sql_bestellung_rechnungssumme( $bestell_id ) ) );
-      open_td();
+  open_tr();
+    open_td( '', '', 'Liefermengen und -preise abgleichen:' );
+    open_td( 'right', '', 'Warenwert:' );
+    open_td( 'bold number', '', price_view( $lieferanten_soll['waren_netto_soll'] ) );
+    open_td( 'bold number', '', price_view( $lieferanten_soll['waren_brutto_soll'] ) );
+    open_td( 'bottom', '', fc_link( 'lieferschein', "bestell_id=$bestell_id,class=href,text=zum Lieferschein..." ) );
 
-    open_tr();
-      if( $status >= STATUS_ABGERECHNET ) {
-        open_td( 'right', "colspan='5' style='padding-top:1em;text-align:right;'" );
-          ?> Abrechnung durchgeführt: <?
-           echo dienstkontrollblatt_name( $bestellung['abrechnung_dienstkontrollblatt_id'] ) .", "
-                . $bestellung['abrechnung_datum'];
-          if( $hat_dienst_IV ) {
-            ?> <span style='padding-left:3em;'>Nochmal öffnen: <input type='checkbox' name='rechnung_abschluss' value='reopen' style='padding-right:4em'>
-                <input type='submit' value='Abschicken'></span> <?
-          }
-      } else {
+if( $lieferant['anzahl_pfandverpackungen'] > 0 ) {
+  open_tr();
+    open_td( '', "rowspan='2'", "Pfandabrechnung Lieferant: <div class='small'>(falls zutreffend, etwa bei Terra!)</div>" );
+    open_td( 'right', '', 'berechnet (Kauf):' );
+    open_td( 'bold number', '', price_view( $lieferanten_soll['pfand_voll_netto_soll'] ) );
+    open_td( 'bold number', '', price_view( $lieferanten_soll['pfand_voll_brutto_soll'] ) );
+    open_td( 'middle', "rowspan='2'"
+      , fc_link( 'pfandzettel', "bestell_id=$bestell_id,lieferanten_id=$lieferant_id,class=href,text=zum Pfandzettel..." ) );
+
+  open_tr();
+    open_td( 'right', '', 'gutgeschrieben (Rückgabe):' );
+    open_td( 'bold number', '', price_view( $lieferanten_soll['pfand_leer_netto_soll'] ) );
+    open_td( 'bold number', '', price_view( $lieferanten_soll['pfand_leer_brutto_soll'] ) );
+}
+  open_tr( 'summe' );
+    open_td( '', "colspan='2'", 'Zwischensumme:' );
+    open_td( 'number', '', price_view( $lieferanten_soll['waren_netto_soll']
+                                       + $lieferanten_soll['pfand_leer_netto_soll']
+                                       + $lieferanten_soll['pfand_voll_netto_soll'] ) );
+    open_td( 'number', '', price_view( $lieferanten_soll['waren_brutto_soll']
+                                       + $lieferanten_soll['pfand_leer_brutto_soll']
+                                       + $lieferanten_soll['pfand_voll_brutto_soll'] ) );
+    open_td( '', "colspan='2'" );
+
+  open_tr();
+    open_td( '', "colspan='3'" );
+      ?> Sonstiges: <? qquad();
+      echo string_view( $bestellung['extra_text'], 40, ( $editable ? 'extra_text' : false ) );
+    open_td( 'number bottom', ''
+      , price_view( $bestellung['extra_soll'], ( $editable ? 'extra_soll' : false ) ) );
+
+  open_tr( 'summe' );
+    open_td( '', "colspan='3'", 'Summe:' );
+    open_td( 'number', '', price_view( sql_bestellung_rechnungssumme( $bestell_id ) ) );
+    open_td();
+
+  open_tr();
+    if( $status >= STATUS_ABGERECHNET ) {
+      open_td( 'right medskip', "colspan='5'" );
+        ?> Abrechnung durchgeführt: <?
+         echo sql_dienstkontrollblatt_name( $bestellung['abrechnung_dienstkontrollblatt_id'] ) .", "
+              . $bestellung['abrechnung_datum'];
         if( $hat_dienst_IV ) {
-          if( abs( $warenwert_basar_brutto ) < 0.05 ) {
-            open_td( '', "colspan='4' style='padding-top:0.8em;text-align:right;border-right:none;'"
-                     , "Rechnung abschliessen: <input type='checkbox' name='rechnung_abschluss' value='yes' style='padding-right:4em'>" );
-          } else {
-            open_td( '', "colspan='4' style='padding-top:1ex;text-align:left;font-size:smaller;'"
-                     , " Reste im Basar --- bitte vor Abschluss leermachen!" );
-          }
-          open_td( 'right', "style='padding-top:1em;text-align:right;border-left:none;'", "<input type='submit' value='Speichern'>" );
+          qquad();
+          echo "Nochmal öffnen:
+            <input type='checkbox' name='rechnung_abschluss' value='reopen' $input_event_handlers>";
+          qquad();
+          submission_button();
         }
+    } else {
+      if( $hat_dienst_IV ) {
+        if( abs( $warenwert_basar_brutto ) < 0.05 ) {
+          open_td( 'medskip right', "colspan='4' style='border-right:none;'"
+                   , "Rechnung abschliessen:
+                     <input type='checkbox' name='rechnung_abschluss' value='yes' $input_event_handlers>" );
+        } else {
+          open_td( 'medskip left smaller', "colspan='4' style='border-right:none;'"
+                   , " Reste im Basar --- bitte vor Abschluss leermachen!" );
+        }
+        open_td( 'right bottom medskip', "style='border-left:none;'" );
+          submission_button();
       }
+    }
 
-  close_table();
-close_form();
+close_table();
+
+if( $editable )
+  close_form();
+
+close_fieldset();
 
 ?>
