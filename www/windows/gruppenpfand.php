5b75f1243a02472697b562770cf14fe099ef2f0d
Timo Felbinger <gittf@qipc.org>
2008-10-21 13:47:57 +0000

    - -Funktionen aus view.php, html.php benutzen benutzen - sql_bestellgruppen liefert array statt mysql-object

--- www/windows/gruppenpfand.php
+++ www/windows/gruppenpfand.php
@@ -9,72 +9,51 @@ get_http_var( 'bestell_id', 'u', 0, true );
 //
 // get_http_var( 'lieferanten_id', 'u', 0, true );
 
-get_http_var( 'optionen', 'u', 0, true );
+get_http_var( 'options', 'u', 0, true );
 
 if( $bestell_id ) {
   $bestellung_name = bestellung_name( $bestell_id );
-  $lieferanten_id = getProduzentBestellID( $bestell_id );
-  $lieferant_name = lieferant_name( $lieferanten_id );
+  $lieferanten_id = sql_bestellung_lieferant_id( $bestell_id );
+  $lieferant_name = sql_lieferant_name( $lieferanten_id );
   $editable = ( getState( $bestell_id ) < STATUS_ABGERECHNET );
 } else {
-  $optionen |= PFAND_OPT_ALLE_BESTELLUNGEN;
+  $options |= PFAND_OPT_ALLE_BESTELLUNGEN;
   $bestellung_name = '';
   $lieferanten_id = 0;
   $editable = false;
 }
 
-if( $optionen & PFAND_OPT_ALLE_BESTELLUNGEN ) {
+if( $options & PFAND_OPT_ALLE_BESTELLUNGEN ) {
   $bestell_id = 0;
   $editable = false;
 }
 
-?>
-<table width='100%' class='layout'>
-<tr>
-  <td>
-    <table class='menu'>
-      <tr>
-        <td><h4>Optionen</h4></td>
-      </tr>
-      <tr>
-        <td>
-          <input style='margin-left:2em;' type='checkbox'
-            <? if( $optionen & PFAND_OPT_GRUPPEN_INAKTIV ) echo " checked"; ?>
-            onclick="window.location.href='<?
-              echo self_url('optionen'), "&optionen=", ($optionen ^ PFAND_OPT_GRUPPEN_INAKTIV );
-            ?>';"
-            title='Auch inaktive Gruppen in Pfandübersicht aufnehmen?'
-          > auch inaktive Gruppen anzeigen?
-        </td>
-      </tr>
-      <? if( $bestellung_name ) { ?>
-      <tr>
-        <td>
-          <input style='margin-left:2em;' type='checkbox'
-            <? if( $optionen & PFAND_OPT_ALLE_BESTELLUNGEN ) echo " checked"; ?>
-            onclick="window.location.href='<?
-              echo self_url('optionen'), "&optionen=", ($optionen ^ PFAND_OPT_ALLE_BESTELLUNGEN );
-            ?>';"
-            title='Pfandsumme ueber alle Bestellungen bei <? echo $lieferant_name; ?> anzeigen'
-          > Summe aller Bestellungen anzeigen?
-        </td>
-      </tr>
-      <? } ?>
-    </table>
-  </td>
-  <td>
-    <? if( $bestell_id ) { ?>
-      <h3>Gruppenpfand: Bestellung <? echo "$bestellung_name ({$lieferant_name})"; ?></h3>
-    <? } else if( $lieferanten_id ) { ?>
-      <h3>Gruppenpfand: alle Bestellungen bei <? echo "$lieferant_name"; ?></h3>
-    <? } else { ?>
-      <h3>Gruppenpfand: alle Bestellungen </h3>
-    <? } ?>
-  </td>
-</tr>
-</table>
-<?
+open_table( 'layout', "width='100%'" );
+  open_td();
+    open_table( 'menu' );
+        open_th('', '', 'Optionen');
+      open_tr();
+        open_td();
+          option_checkbox( 'options', PFAND_OPT_GRUPPEN_INAKTIV, 'auch inaktive Gruppen zeigen?'
+                           , 'Auch inaktive Gruppen in Pfandübersicht aufnehmen?' );
+      if( $bestellung_name ) {
+        open_tr();
+          open_td();
+           option_checkbox( 'options', PFAND_OPT_ALLE_BESTELLUNGEN, 'Summe aller Bestellungen anzeigen?'
+                           , "Pfandsumme ueber alle Bestellungen bei $lieferant_name anzeigen?" );
+      }
+    close_table();
+  open_td();
+    if( $bestell_id ) {
+      ?> <h3>Gruppenpfand: Bestellung <? echo "$bestellung_name ({$lieferant_name})"; ?></h3> <?
+    } else if( $lieferanten_id ) {
+      ?> <h3>Gruppenpfand: alle Bestellungen bei <? echo "$lieferant_name"; ?></h3> <?
+    } else {
+      ?> <h3>Gruppenpfand: alle Bestellungen </h3> <?
+    }
+close_table();
 
+medskip();
 
 /////////////////////////////
 //
@@ -84,13 +63,11 @@ if( $optionen & PFAND_OPT_ALLE_BESTELLUNGEN ) {
 
 get_http_var('action','w','');
 $editable or $action = '';
-
 if( $bestell_id and ( $action == 'save' ) ) {
-  $gruppen = sql_bestellgruppen();
-  while( $row = mysql_fetch_array( $gruppen ) ) {
+  foreach( sql_bestellgruppen() as $row ) {
     $id = $row['id'];
-    if( get_http_var( "anzahl_leer$id", 'u' ) ) {
-      sql_pfandzuordnung_gruppe( $bestell_id, $id, ${"anzahl_leer$id"} );
+    if( get_http_var( "anzahl_leer_$id", 'u' ) ) {
+      sql_pfandzuordnung_gruppe( $bestell_id, $id, ${"anzahl_leer_$id"} );
     }
   }
 }
@@ -102,110 +79,83 @@ if( $bestell_id and ( $action == 'save' ) ) {
 //
 /////////////////////////////
 
-$gruppen = sql_gruppenpfand( $lieferanten_id, $bestell_id );
+if( $bestell_id )
+  open_form( '', '', '', array( 'action' => 'save' ) );
 
-if( $bestell_id ) {
-  ?>
-  <form method='post' action='<? echo self_url(); ?>'>
-  <? echo self_post(); ?>
-  <input type='hidden' name='action' value='save'>
-  <?
-}
+open_table('list');
+  open_th( '', '', 'Gruppe' );
+  open_th( '', '', 'Nr (Id)' );
+  open_th( '', '', 'aktiv' );
+  open_th( '', "title='Pfand für Bestellungen in Rechnung gestellt'", 'Wert berechnet' );
+  open_th( '', "title='Anzahl zurückgegebene Pfandverpackungen'", 'Anzahl gutgeschrieben' );
+  open_th( '', "title='Gutschrift für zurürckgegebene Pfandverpackungen'", 'Wert gutgeschrieben' );
+  open_th( '', '', 'Summe' );
 
-?>
-<table class='list'>
-  <tr>
-    <th>Gruppe</th>
-    <th>Nr (Id)</th>
-    <th>aktiv</th>
-    <th title='Pfand für Bestellungen in Rechnung gestellt'>Wert berechnet</th>
-    <th title='Anzahl zurückgegebene Pfandverpackungen'>Anzahl gutgeschrieben</th>
-    <th title='Gutschrift für zurürckgegebene Pfandverpackungen'>Wert gutgeschrieben</th>
-    <th>Summe</th>
-  </tr>
-<?
 $summe_pfand_leer_brutto = 0;
 $summe_pfand_voll_brutto = 0;
 $summe_pfand_leer_anzahl = 0;
 $muell_row = false;
 $basar_row = false;
-while( $row = mysql_fetch_array( $gruppen ) ) {
+
+foreach( sql_gruppenpfand( $lieferanten_id, $bestell_id ) as $row ) {
   $gruppen_id = $row['gruppen_id'];
-  if( $gruppen_id == $muell_id ) {
-    $muell_row = $row;
-    continue;
+  switch( $gruppen_id ) {
+    case $muell_id:
+      $muell_row = $row;
+      continue 2;
+    case $basar_id:
+      $basar_row = $row;
+      continue 2;
+    default:
+      if( ! ( $row['aktiv'] or ( $options & PFAND_OPT_GRUPPEN_INAKTIV ) ) )
+        continue 2;
   }
-  if( $gruppen_id == $basar_id ) {
-    $basar_row = $row;
-    continue;
-  }
-  if( ! ( $row['aktiv'] or ( $optionen & PFAND_OPT_GRUPPEN_INAKTIV ) ) )
-    continue;
-  ?>
-    <tr>
-      <td><? echo $row['gruppen_name']; ?></td>
-      <td><? echo "{$row['gruppen_nummer']} ($gruppen_id)"; ?></td> 
-      <td><? echo $row['aktiv']; ?></td> 
-      <td class='number'><? printf( "%.2lf", $row['pfand_voll_brutto_soll'] ); ?></td>
-      <td class='number'>
-        <? if( $editable and $bestell_id ) { ?>
-          <input type=text' size='6' name='anzahl_leer<? echo $gruppen_id; ?>'
-                 value='<? printf( "%u", $row['pfand_leer_anzahl'] ); ?>'>
-        <? } else { ?>
-          <? printf( "%u", $row['pfand_leer_anzahl'] ); ?>
-        <? } ?>
-      </td>
-      <td class='number'><? printf( "%.2lf", $row['pfand_leer_brutto_soll'] ); ?></td>
-      <td class='number'><? printf( "%.2lf", $row['pfand_leer_brutto_soll'] + $row['pfand_voll_brutto_soll'] ); ?></td>
-    </tr>
-  <?
+  open_tr();
+    open_td( '', '', $row['gruppen_name'] );
+    open_td( '', '', "{$row['gruppen_nummer']} ($gruppen_id)" );
+    open_td( '', '', $row['aktiv'] );
+    open_td( 'number', '', price_view( $row['pfand_voll_brutto_soll'] ) );
+    open_td( 'number', '', int_view( $row['pfand_leer_anzahl'], ( $editable ? "anzahl_leer_$gruppen_id" : false ) ) );
+    open_td( 'number', '', price_view( $row['pfand_leer_brutto_soll'] ) );
+    open_td( 'number', '', price_view( $row['pfand_leer_brutto_soll'] + $row['pfand_voll_brutto_soll'] ) );
+
   $summe_pfand_voll_brutto += $row['pfand_voll_brutto_soll'];
   $summe_pfand_leer_brutto += $row['pfand_leer_brutto_soll'];
   $summe_pfand_leer_anzahl += $row['pfand_leer_anzahl'];
 }
-?>
-  <tr class='summe'>
-    <td colspan='3'>Summe:</td>
-    <td class='number'><? printf( "%.2lf", $summe_pfand_voll_brutto ); ?></td>
-    <td class='number'><? printf( "%u", $summe_pfand_leer_anzahl ); ?></td>
-    <td class='number'><? printf( "%.2lf", $summe_pfand_leer_brutto ); ?></td>
-    <td class='number'><? printf( "%.2lf", $summe_pfand_voll_brutto + $summe_pfand_leer_brutto ); ?></td>
-  </tr>
-<?
+
+  open_tr('summe');
+    open_td( '', "colspan='3'", 'Summe:' );
+    open_td( 'number', '', price_view( $summe_pfand_voll_brutto ) );
+    open_td( 'number', '', int_view( $summe_pfand_leer_anzahl ) );
+    open_td( 'number', '', price_view( $summe_pfand_leer_brutto ) );
+    open_td( 'number', '', price_view( $summe_pfand_leer_brutto + $summe_pfand_voll_brutto ) );
+
 if( $basar_row ) {
-  ?>
-  <tr class='summe'>
-    <td colspan='3'>Basar:</td>
-    <td class='number'><? printf( "%.2lf", $basar_row['pfand_voll_brutto_soll'] ); ?></td>
-    <td class='number'><? printf( "%u", $row['pfand_leer_anzahl'] ); ?></td>
-    <td class='number'><? printf( "%.2lf", $basar_row['pfand_leer_brutto_soll'] ); ?></td>
-    <td class='number'><? printf( "%.2lf", $basar_row['pfand_voll_brutto_soll'] - $basar_row['pfand_leer_brutto_soll'] ); ?></td>
-  </tr>
-  <?
+  open_tr('summe');
+    open_td( '', "colspan='3'", 'Basar:' );
+    open_td( 'number', '', price_view( $basar_row['pfand_voll_brutto_soll'] ) );
+    open_td( 'number', '', int_view( $basar_row['pfand_leer_anzahl'] ) );
+    open_td( 'number', '', price_view( $basar_row['pfand_leer_brutto_soll'] ) );
+    open_td( 'number', '', price_view( $basar_row['pfand_voll_brutto_soll'] + $basar_row['pfand_leer_brutto_soll'] ) );
 }
 if( $muell_row ) {
-  ?>
-  <tr class='summe'>
-    <td colspan='3'>internes Verrechnungskonto:</td>
-    <td class='number'><? printf( "%.2lf", $muell_row['pfand_voll_brutto_soll'] ); ?></td>
-    <td class='number'><? printf( "%u", $row['pfand_leer_anzahl'] ); ?></td>
-    <td class='number'><? printf( "%.2lf", $muell_row['pfand_leer_brutto_soll'] ); ?></td>
-    <td class='number'><? printf( "%.2lf", $muell_row['pfand_voll_brutto_soll'] - $muell_row['pfand_leer_brutto_soll'] ); ?></td>
-  </tr>
-  <?
+  open_tr('summe');
+    open_td( '', "colspan='3'", 'M&uuml;ll:' );
+    open_td( 'number', '', price_view( $muell_row['pfand_voll_brutto_soll'] ) );
+    open_td( 'number', '', int_view( $muell_row['pfand_leer_anzahl'] ) );
+    open_td( 'number', '', price_view( $muell_row['pfand_leer_brutto_soll'] ) );
+    open_td( 'number', '', price_view( $muell_row['pfand_voll_brutto_soll'] + $muell_row['pfand_leer_brutto_soll'] ) );
 }
 
-if( $bestell_id ) {
-  ?>
-    <tr>
-      <td colspan='6'>
-        <input type='submit' class='button' value='Speichern'>
-      </td>
-    </tr>
-  </table>
-  </form>
-  <?
-} else {
-  ?> </table> <?
+if( $editable ) {
+  open_tr();
+    open_td('right', "colspan='6'" );
+      floating_submission_button();
 }
+close_table();
 
+if( $editable )
+  close_form();
+
+?>
