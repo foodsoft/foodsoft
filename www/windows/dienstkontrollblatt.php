7cab6eccdb1d4e238913ca4aafa816e82f3c78b0
Timo Felbinger <gittf@qipc.org>
2008-10-21 14:04:32 +0000

    html.php-Funktionen benutzen

--- www/windows/dienstkontrollblatt.php
+++ www/windows/dienstkontrollblatt.php
@@ -2,118 +2,88 @@
 
 // dienstkontrollblatt.php
 //
-	assert( $angemeldet ) or exit();
+assert( $angemeldet ) or exit();
 
-  get_http_var('action','w','');
+get_http_var('action','w','');
 
-  if( ( $action == 'abmelden' ) && ( $dienst >= 0 ) )  {
-
-    $row = false;
-    if( $dienst > 0 and $dienstkontrollblatt_id > 0 ) {
-      $result = dienstkontrollblatt_select( $dienstkontrollblatt_id );
-      $row = mysql_fetch_array( $result );
-    }
-    if( $row ) {
-      ?>
-        <form class='small_form' action='index.php?login=logout' method='post'>
-          <? echo self_post(); ?>
-          <fieldset>
-            <legend>
-              Abmeldung im Dienstkontrollblatt
-            </legend>
-            <div class='newfield'>
-              <label>Dein Name:</label>
-              <input type='text' size='20' name='coopie_name' value='<? echo $row['name']; ?>'></input>
-              <label style='padding-left:4em;'>Telefon:</label>
-              <input type='text' size='20' name='telefon' value='<? echo $row['telefon']; ?>'></input>
-            </div>
-            <div class='newfield'>
-              <label>Notiz fuers Dienstkontrollblatt (zum Beispiel: wo die Ordner liegen...):</label>
-              <br>
-              <textarea cols='80' rows='4' name='notiz'><? echo $row['notiz']; ?></textarea>
-            </div>
-            <div class='newfield'>
-              <input type='submit' name='submit' value='Abmelden...'></input>
-            </div>
-          </fieldset>
-        </form>
-      <?
-    } else {
-      // kein eintrag zum aktualisieren, also gleich ausloggen:
-      //
-      reload_immediately( 'index.php?login=logout' );
-    }
-
+if( ( $action == 'abmelden' ) && ( $dienst >= 0 ) )  {
+  $row = false;
+  if( $dienst > 0 and $dienstkontrollblatt_id > 0 )
+    $row = first( sql_dienstkontrollblatt( $dienstkontrollblatt_id ) );
+  if( $row ) {
+    open_form( 'small_form', '', 'index.php?login=logout' );
+      open_fieldset( 'small_form', '', 'Abmeldung im Dienstkontrollblatt' );
+        open_table('layout');
+            open_td('label','','Dein Name:');
+            open_td('kbd');
+              echo string_view( $coopie_name, 20, 'coopie_name' );
+              open_span('qquad', '', 'Telefon: ');
+              open_span('kbd', '', string_view( $telefon, '20', 'telefon' ) );
+          open_tr();
+            open_td('label', "colspan='2'", 'Notiz fuers Dienstkontrollblatt:' );
+              open_div('kbd');
+                echo "<textarea cols='80' rows='3' name='notiz'>$notiz</textarea>";
+              close_div();
+          open_tr();
+            open_td('right', "colspan='2'" );
+              submission_button('Abmelden...');
+        close_table();
+      close_fieldset();
+    close_form();
   } else {
-
-    $result = mysql_query( "SELECT id FROM dienstkontrollblatt ORDER BY id DESC LIMIT 5" );
-    $row = mysql_fetch_array( $result );
-    if( ! $row )
-      error( "konnte dienstkontrollblatt nicht lesen" );
-    $id_max = $row['id'];
-    get_http_var( 'id_to', 'u', $id_max, true );
-    $id_from = $id_to - 10;
-    if( $id_from < 1 )
-      $id_from = 1;
-    // get_http_var('id_from', 'u', $id_to - 10, true );
-  
-    $result = dienstkontrollblatt_select( $id_from, $id_to );
-
-    ?>
-      <h1>Dienstkontrollblatt</h1>
-      <table class='list'>
-        <tr>
-          <th> Nr. </th>
-          <th> Datum </th>
-          <th> Zeit </th>
-          <th> Dienst </th>
-          <th> Gruppe </th>
-          <th> Name </th>
-          <th> Telefon </th>
-          <th> Notiz </th>
-        </tr>
-    <?
-
-    if( $id_from > 1 ) {
-      $n = ( $id_from > 10 ) ? $id_from : 10;
-      ?>
-        <tr>
-          <td colspan='7'>
-            <? echo fc_button( 'self', "id_to=$n,text= &lt; &lt; &lt;  Bl&auml;ttern &lt; &lt; &lt; " ); ?>
-          </td>
-        </tr>
-      <?
-    }
-    while( $row = mysql_fetch_array( $result ) ) {
-      ?>
-        <tr>
-          <td>
-            <?  echo fc_alink( 'self', array( 'title' => 'Zentrieren', 'id_to' => $row['id'] + 5, 'text' => $row['id'] ) ); ?>
-          </td>
-          <td><? echo $row['datum']; ?></td>
-          <td><? echo $row['zeit']; ?></td>
-          <td><? echo $row['dienst']; ?></td>
-          <td><? echo $row['gruppen_name']; ?></td>
-          <td><? echo $row['name']; ?></td>
-          <td><? echo $row['telefon']; ?></td>
-          <td><? echo $row['notiz']; ?></td>
-        </tr>
-      <?
-    }
-    if( $id_to < $id_max ) {
-      $n = $id_to + 10;
-      if( $n > $id_max )
-        $n = $id_max;
-      ?>
-        <tr>
-          <td colspan='7'>
-            <? echo fc_button( 'self', "id_to=$n,text= &gt; &gt; &gt;  Bl&auml;ttern &gt; &gt; &gt; " ); ?>
-          </td>
-        </tr>
-      <?
-    }
-    echo "</table>";
+    // kein eintrag zum aktualisieren, also gleich ausloggen:
+    //
+    reload_immediately( 'index.php?login=logout' );
   }
+  return;
+}
+
+$result = mysql_query( "SELECT id FROM dienstkontrollblatt ORDER BY id DESC LIMIT 5" );
+$row = mysql_fetch_array( $result );
+if( ! $row )
+  error( "konnte dienstkontrollblatt nicht lesen" );
+$id_max = $row['id'];
+get_http_var( 'id_to', 'u', $id_max, true );
+$id_from = $id_to - 10;
+if( $id_from < 1 )
+  $id_from = 1;
+
+$result = sql_dienstkontrollblatt( $id_from, $id_to );
+
+echo "<h1>Dienstkontrollblatt</h1>";
+  open_table('list');
+      open_th( '', '', 'Nr.' );
+      open_th( '', '', 'Datum' );
+      open_th( '', '', 'Zeit' );
+      open_th( '', '', 'Dienst' );
+      open_th( '', '', 'Gruppe' );
+      open_th( '', '', 'Name' );
+      open_th( '', '', 'Telefon' );
+      open_th( '', '', 'Notiz' );
+
+if( $id_from > 1 ) {
+  $n = ( $id_from > 10 ) ? $id_from : 10;
+  open_tr();
+    open_th('', "colspan='7'", fc_link( '', "class=button,id_to=$n,text= &lt; &lt; &lt;  Bl&auml;ttern &lt; &lt; &lt; " ) );
+}
+foreach( $result as $row ) {
+  open_tr();
+    open_td('','', fc_link( 'self', array( 'title' => 'Zentrieren', 'id_to' => $row['id'] + 5, 'text' => $row['id'] ) ) );
+    open_td('','', $row['datum'] );
+    open_td('','', $row['zeit'] );
+    open_td('','', $row['dienst'] );
+    open_td('','', $row['gruppen_name'] );
+    open_td('','', $row['name'] );
+    open_td('','', $row['telefon'] );
+    open_td('','', $row['notiz'] );
+}
+if( $id_to < $id_max ) {
+  $n = $id_to + 10;
+  if( $n > $id_max )
+    $n = $id_max;
+  open_tr();
+    open_th('',"colspan='7'", fc_link( '', "class=button,id_to=$n,text= &gt; &gt; &gt;  Bl&auml;ttern &gt; &gt; &gt; " ) );
+}
+close_table();
 
 ?>
-
