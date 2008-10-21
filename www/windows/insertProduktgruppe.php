63f3a987a5a129be8f90ca8d98f9b1b5a49d41a0
Timo Felbinger <gittf@qipc.org>
2008-10-21 14:03:43 +0000

    - open_*-Funktionen aus html.php einsetzen - sql_produktgruppen liefert array statt mysql-object

--- www/windows/insertProduktgruppe.php
+++ www/windows/insertProduktgruppe.php
@@ -1,60 +1,45 @@
 <?PHP
-  assert( $angemeldet ) or exit();
+assert( $angemeldet ) or exit();
 
-  $editable = ( ! $readonly and ( $dienst == 4 ) );
+$editable = ( ! $readonly and ( $dienst == 4 ) );
 
-  get_http_var( 'action', 'w', '' );
-  $editable or $action = '';
-  switch( $action ) {
-    case 'insert':
-      need_http_var( 'neue_produktgruppe', 'H' );
-      sql_insert( 'produktgruppen', array( 'name' => $neue_produktgruppe ) );
-      break;
-    case 'delete':
-      need_http_var(' produktgruppen_id', 'u' );
-      doSql( 'DELETE * FROM produktgruppen WHERE id=$produktgruppen_id' );
-      break;
-  }
+get_http_var( 'action', 'w', '' );
+$editable or $action = '';
+switch( $action ) {
+  case 'insert':
+    need_http_var( 'neue_produktgruppe', 'H' );
+    sql_insert( 'produktgruppen', array( 'name' => $neue_produktgruppe ) );
+    break;
+  case 'delete':
+    need_http_var( 'produktgruppen_id', 'u' );
+    doSql( 'DELETE * FROM produktgruppen WHERE id=$produktgruppen_id' );
+    break;
+}
 
-$produktgruppen = sql_produktgruppen();
+open_fieldset( 'small_form', '', '', 'Neue Produktgruppe', 'off' );
+  open_form( '', '', '', 'action=insert' );
+    open_table('layout');
+      form_row_text( 'Name:', 'neue_produktgruppe', 20 );
+      submission_button( 'Speichern', true );
+    close_table();
+  close_form();
+close_fieldset();
+medskip();
+
+open_fieldset( 'small_form', '', 'Produktgruppen' );
+  open_table('list');
+      open_th('','', 'Produktgruppen' );
+      open_th('','', 'Aktionen' );
+
+    foreach( sql_produktgruppen() as $row ) {
+      open_tr();
+        open_td( '', '', $row['name'] );
+        open_td();
+          if( references_produktgruppe( $row['id'] ) == 0 )
+              echo fc_action( 'class=drop,text=,title=Produktgruppe l&ouml;schen?'
+                            , "action=delete,produktgruppen_id={$row['id']}" );
+    }
+  close_table();
+close_fieldset();
 
 ?>
-
-<fieldset class='small_form'>
-<legend>Produktgruppen</legend>
-
-<table class='list'>
-  <tr>
-    <th>Produktgruppen</th>
-    <th>Aktionen</th>
-  </tr>
-
-<? while( $row = mysql_fetch_array( $produktgruppen ) ) { ?>
-  <tr>
-    <td><? echo $row['name']; ?></td>
-    <td><? if( references_produktgruppe( $row['id'] ) == 0 )
-              echo fc_action( array( 'action' => 'delete', 'produktgruppen_id' => $row['id'], 'img' => 'img/b_drop.png', 'text' => ''
-                                     , 'title' => 'Produktgruppe l&ouml;schen?' ) ); ?></td>
-  </tr>
-<? } ?>
-
-</table>
-
-<h4>Neue Produktgruppe:</h4>
-
-<form action="<? echo self_url(); ?>" method='post'>
-  <? echo self_post(); ?>
-  <input type='hidden' name='action' value='insert'>
-  <table class="menu">
-    <tr>
-      <td><label>Name:</label></td>
-      <td><input type="text" size="20" name="neue_produktgruppe"></td>
-    </tr>
-    <tr>
-      <td colspan='2' style='text-align:right;'><input type="submit" value="Einf&uuml;gen"></td>
-    </tr>
-  </table>
-</form>
-
-</fieldset>
-
