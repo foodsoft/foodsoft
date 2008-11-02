a894e884b993256fbd70f3af9262b6fa2d7a4f65
Timo Felbinger <gittf@qipc.org>
2008-11-02 13:25:56 +0000

    auswahl_bestellung (Anzeige laufender Bestellungen und Abkuerzung zum Bestellformular) und Anzeige Gruppenkontostand auf Hautpseite (neben Hauptmenue) eingefuegt (drueber ist noch Platz fuer ein kleines schwarzes Brett :-)

--- www/windows/menu.php
+++ www/windows/menu.php
@@ -1,8 +1,25 @@
 <h1>Start ....</h1>  
 <?
-open_table( 'menu' );
-  foreach(possible_areas() as $menu_area){
-    areas_in_menu($menu_area);
-  }
+
+open_table( 'layout hfill' );
+  open_td();  // hauptmenu
+    open_table( 'menu' );
+      foreach(possible_areas() as $menu_area){
+        areas_in_menu($menu_area);
+      }
+    close_table();
+  open_td( 'qquad bottom' );   // schwarzes Brett, Schnellauswahl laufende Bestellungen
+
+    if( hat_dienst(0) ) {
+      open_div( 'bigskip bold', '', 'Euer Gruppenkontostand: ' . fc_link( 'meinkonto', array(
+        'text' => price_view( kontostand( $login_gruppen_id ) ) . " Euro"
+      , 'class' => 'href', 'gruppen_id' => $login_gruppen_id
+      ) ) );
+    }
+    open_div( 'bigskip' );
+      ?> <h4> Laufende Bestellungen: </h4> <?
+      auswahl_bestellung();
+    close_div();
+
 close_table();
 ?>
