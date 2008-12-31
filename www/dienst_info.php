dbadf8a6d9f5fc91c426fc90974279c0865ed69e
Timo Felbinger <gittf@qipc.org>
2008-12-31 13:58:27 +0000

    <form> ...  </form> durch fc_action ersetzt

--- www/dienst_info.php
+++ www/dienst_info.php
@@ -30,13 +30,7 @@
 	}
 
      }
-     ?>
-	       <form action="index.php">
-	       <input type="hidden" name="window" value=<?echo $window?>>
-	       <input type="hidden" name="dienst_rueckbestatigen" value="1">
-	       <input  type="submit" value="OK">  
-	       </form>
-     <?
+     fc_action( 'text=OK', 'dienst_rueckbestatigen=1' );
 
      exit();
      }
