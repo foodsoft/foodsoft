eaf9615f41b919aa5ad98de0827e74d96b95bfb9
Timo Felbinger <gittf@qipc.org>
2009-01-22 08:48:03 +0000

    html fuer "schwarzes brett" verbessert:   'float:left;' fuer natuerliche groesse ohne explizite angabe   'clear:left;' erzwingt zeilenumbruch

--- www/windows/menu.php
+++ www/windows/menu.php
@@ -14,7 +14,7 @@ switch( $action ) {
     $bulletinboard = '';
     $nl = '';
     for( $i = 0; $i <= 7; ++$i ) {
-      $bulletinboard .= ( $nl . preg_replace( '/\r/', '', $b[$i] ) );
+      $bulletinboard .= ( $nl . rtrim( preg_replace( '/\r/', '', $b[$i] ) ) );
       $nl = "\n";
     }
     // $bulletinboard = $b[0].$b[1].$b[2].$b[3].$b[4].$b[5].$b[6];
@@ -33,29 +33,30 @@ open_table( 'layout hfill' );
       }
     close_table();
   open_td( 'qquad bottom' );   // schwarzes Brett, Schnellauswahl laufende Bestellungen
-    open_div( 'wall_for_board' );
-      if( $action == 'edit' ) {
-        open_form( '', 'action=save' );
-          open_div( 'board' );
-            ?><textarea id='news' wrap='hard' name='bulletinboard' cols='38' rows='8'><? echo $bulletinboard; ?></textarea><?
-            open_div( 'right' );
-              submission_button();
-            close_div();
+    if( $action == 'edit' ) {
+      open_form( '', 'action=save' );
+        open_div( 'board' );
+          ?><textarea id='news' wrap='hard' name='bulletinboard' cols='38' rows='8'><? echo $bulletinboard; ?></textarea><?
+          open_div( 'chalk' );
+            submission_button();
           close_div();
-          open_javascript( "document.getElementById('news').focus();" );
-        close_form();
-      } else {
-        $form_id = open_form( '', 'action=edit' );
-          open_div( 'board' );
+        close_div();
+        open_javascript( "document.getElementById('news').focus();" );
+      close_form();
+    } else {
+      $form_id = open_form( '', 'action=edit' );
+        open_div( 'board' );
+          // open_div();
             ?><textarea name='news' readonly cols='38' rows='8'><? echo $bulletinboard; ?></textarea><?
-            open_div( 'right' );
+            open_div( 'chalk' );
               ?><a href='#' onclick="document.forms.form_<? echo $form_id; ?>.submit();"
                   title='Tafel beschreiben...'><img src='img/chalk_trans.gif'></a><?
             close_div();
-          close_div();
-        close_form();
-      }
-    close_div();
+          // close_div();
+        close_div();
+      close_form();
+    }
+  open_div( '', "style='clear:left;'", ' ' ); // erzwingt neue Zeile
 
     if( hat_dienst(0) ) {
       open_div( 'bigskip bold', '', 'Euer Gruppenkontostand: ' . fc_link( 'meinkonto', array(
