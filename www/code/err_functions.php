3fae09027e35478abcb0453736f03d593055beaa
Timo Felbinger <gittf@qipc.org>
2009-02-05 15:39:38 +0000

    err_functions sauberer: variable static statt global,                         $readonly bei Bedarf initialisieren

--- www/code/err_functions.php
+++ www/code/err_functions.php
@@ -3,33 +3,30 @@
 // low-level error handling and logging
 //
 
-global $in_error;
-$in_error = false;
-
 function error( $string ) {
-  global $in_error;
-  $stack = debug_backtrace();
-  ?> <div class='warn'>Fehler: <? echo htmlspecialchars( $string ); ?>
-     <br>
-     <pre><? echo htmlspecialchars( var_export($stack) ); ?>
-     </pre>
-     </div>
-   <?
+  static $in_error = false;
   if( ! $in_error ) { // avoid infinite recursion (e.g. if there is no database connection)
     $in_error = true;
+    $stack = debug_backtrace();
+    ?> <div class='warn'>Fehler: <? echo htmlspecialchars( $string ); ?>
+       <br>
+       <pre><? echo htmlspecialchars( var_export($stack) ); ?>
+       </pre>
+       </div>
+     <?
     logger( "error: $string [$stack]" );
   }
   die();
 }
 
 function need( $exp, $comment = "Fataler Fehler" ) {
-  global $in_error;
+  static $in_need = false;
   if( ! $exp ) {
+    if( $in_need )
+      die();
+    $in_need = true;
     open_div( 'warn', '', htmlspecialchars( "$comment: $exp" ) . fc_link( 'self', 'img=,text=weiter...' ) );
-    if( ! $in_error ) {
-      $in_error = true;
       logger( "assertion failed: $exp" );
-    }
     die();
   }
   return true;
@@ -37,7 +34,7 @@ function need( $exp, $comment = "Fataler Fehler" ) {
 
 function fail_if_readonly() {
   global $readonly;
-  if( $readonly ) {
+  if( isset( $readonly ) and $readonly ) {
     open_div( 'warn', '', 'Datenbank ist schreibgesch&uuml;tzt - Operation nicht m&ouml;glich!' );
     die();
   }
