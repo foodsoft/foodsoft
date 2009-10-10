e6d6d11cd3a289be9f52fc4ee1536ac7e7f0c1fb
Timo Felbinger <gittf@qipc.org>
2009-10-10 20:42:09 +0000

    Fehler korrigiert ($action manchmal undefiniert)

--- www/windows/produkte.php
+++ www/windows/produkte.php
@@ -59,7 +59,7 @@ bigskip();
 //
 /////////////////////////////
 
-get_http_var('action','w','');
+get_http_var('action','w','') or $action = '';
 $editable or $action = '';
 if( $action == 'delete' ) {
   need_http_var('produkt_id','u');
