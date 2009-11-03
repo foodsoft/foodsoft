600e879d8127a42b474e1f1a392d5291cdbeecec
Timo Felbinger <gittf@qipc.org>
2009-11-03 17:50:54 +0000

    Katalogabgleich ueberarbeitet:   - fallunterscheidung: mwst im katalog / nicht im katalog   - tabelle 'produktpreise' enthaelt jetzt (netto-)'lieferpreis', nicht mehr den (brutto-mit-pfand-V-)'preis'

--- www/code/katalogsuche.php
+++ www/code/katalogsuche.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // katalogsuche: sucht im lieferantenkatalog nach $produkt (soweit fuer den Lieferanten implementiert)
 // 
 // !!! dieses Skript ist nur fuer den _internen_ katalogabgleich aufgrund der Artikelnummer zustaendig!
@@ -39,7 +38,7 @@ function katalogsuche( $produkt ) {
 //
 // rueckgabe:
 //  0: ok
-//  1: Katalogeintrag weicht ab
+//  1: Katalogeintrag weicht ab (oder kein Preiseintrag in der Foodsoft-Datenbank)
 //  2: Katalogsuche fehlgeschlagen (kann auch heissen: kein Katalog dieses Lieferanten erfasst)
 // 
 //
@@ -48,6 +47,8 @@ function katalogabgleich(
 , $detail = false  // Katalogeintrag auch anzeigen?
 , & $preiseintrag_neu = array() // aus Katalogeintrag Vorschlag fuer Preiseintrag generieren
 ) {
+  global $mwst_default;
+
   $artikel = sql_produkt_details( $produkt_id );
   $prgueltig = $artikel['zeitstart'];
   $neednewprice = false;
@@ -74,7 +75,7 @@ function katalogabgleich(
   $katalog_herkunft =  $katalogeintrag["herkunft"];
   $katalog_verband = $katalogeintrag["verband"];
   $katalog_netto = $katalogeintrag["preis"];
-  $katalog_mwst = $katalogeintrag["mwst"];
+
 
   if( ! kanonische_einheit( $katalog_einheit, &$kan_liefereinheit, &$kan_liefermult, false ) ) {
     div_msg( 'warn', "Katalogsuche: unbekannte Einheit: $katalog_einheit" );
@@ -83,57 +84,60 @@ function katalogabgleich(
 
   $have_mwst = false; // ist die mwst im katalog gelisted? (terra ja, andere nicht!)
   switch( $katalogeintrag['katalogformat'] ) {
-  case 'terra':
-    $have_mwst = true;
+    case 'terra':
+      $have_mwst = true;
 
-    // setze:
-    //   $liefereinheit: auf diese bezieht sich der preis $katalog_netto
-    //   $gebindegroesse in vielfachen der $liefereinheit
-    switch( $kan_liefereinheit ) {
-      case 'g':
-        $liefereinheit = '1000 g';
-        $gebindegroesse = $katalog_gebindegroesse * $kan_liefermult / 1000.0;
-        $kan_liefermult = 1000.0;
-        break;
-      case 'ml':
-        $liefereinheit = '1000 ml';
-        $gebindegroesse = $katalog_gebindegroesse * $kan_liefermult / 1000.0;
-        $kan_liefermult = 1000.0;
-        break;
-      default:
-        $liefereinheit = "1 $kan_liefereinheit";
-        $gebindegroesse = $katalog_gebindegroesse * $kan_liefermult;
-        $kan_liefermult = 1;
-        break;
-    }
+      // setze:
+      //   - $liefereinheit: auf diese bezieht sich der preis $katalog_netto,
+      //     ebenso der wie der 'lieferpreis' in tabelle 'produktpreise'
+      //   - $liefergebinde in vielfachen der $liefereinheit
+      //     (achtung: 'gebindegroesse' in tabelle 'produktpreise' ist in v-einheiten!)
+      switch( $kan_liefereinheit ) {
+        case 'g':
+          $liefereinheit = '1000 g';
+          $liefergebinde = $katalog_gebindegroesse * $kan_liefermult / 1000.0;
+          $kan_liefermult = 1000.0;
+          break;
+        case 'ml':
+          $liefereinheit = '1000 ml';
+          $liefergebinde = $katalog_gebindegroesse * $kan_liefermult / 1000.0;
+          $kan_liefermult = 1000.0;
+          break;
+        default:
+          $liefereinheit = "1 $kan_liefereinheit";
+          $liefergebinde = $katalog_gebindegroesse * $kan_liefermult;
+          $kan_liefermult = 1;
+          break;
+      }
 
-    switch( $kan_liefereinheit ) {
-      case 'KI':
-      case 'PA':
-      case 'GB':
-        $verteileinheit_default = '1 ST';
-        $lv_faktor_default = 1;
-        break;
-      case 'g':
-        $verteileinheit_default = '500 g';
-        $lv_faktor_default = 2;
-        break;
-      case 'ml':
-        $verteileinheit_default = '1000 ml';
-        $lv_faktor_default = 1;
-        break;
-      default:
-        $verteileinheit_default = $liefereinheit;
-        $lv_faktor_default = 1;
-        break;
-    }
+      // default fuer v-einheit (nur relevant, falls gar keine oder inkompatible gesetzt)
+      switch( $kan_liefereinheit ) {
+        case 'KI':
+        case 'PA':
+        case 'GB':
+          $verteileinheit_default = '1 ST';
+          $lv_faktor_default = 1; // TODO: aus katalogtext parsen?
+          break;
+        case 'g':
+          $verteileinheit_default = '500 g';
+          $lv_faktor_default = 2;
+          break;
+        case 'ml':
+          $verteileinheit_default = '1000 ml';
+          $lv_faktor_default = 1;
+          break;
+        default:
+          $verteileinheit_default = $liefereinheit;
+          $lv_faktor_default = 1;
+          break;
+      }
 
       break;
 
     case 'bode':
 
-      $gebindegroesse = $katalog_gebindegroesse;
-      $liefereinheit = $katalog_einheit;
+      $liefergebinde = $katalog_gebindegroesse;
+      $liefereinheit = "$kan_liefermult $kan_liefereinheit";  // oder = $katalog_einheit ???
       $verteileinheit_default = $liefereinheit;
       $lv_faktor_default = 1;
 
@@ -141,8 +145,8 @@ function katalogabgleich(
 
     case 'rapunzel':
 
-      $gebindegroesse = $katalog_gebindegroesse;
-      $liefereinheit = $katalog_einheit;
+      $liefergebinde = $katalog_gebindegroesse;
+      $liefereinheit = "$kan_liefermult $kan_liefereinheit";
       $verteileinheit_default = $liefereinheit;
       $lv_faktor_default = 1;
 
@@ -150,11 +154,17 @@ function katalogabgleich(
 
     default:
     case 'keins':
-      error( "unbekanntes oder undefiniertes Katalogformat --- Katalogabgleich nicht moeglich" );
-      break;
+      open_div( 'warn', '', "unbekanntes oder undefiniertes Katalogformat --- Katalogabgleich nicht moeglich" );
+      return 2;
   }
 
-  $katalog_brutto = $katalog_netto * (1 + $katalog_mwst / 100.0 );
+  if( $have_mwst) {
+    $katalog_mwst = $katalogeintrag["mwst"];
+    $katalog_brutto = $katalog_netto * (1 + $katalog_mwst / 100.0 );
+  } else {
+    $katalog_mwst = 'n/a';
+    $katalog_brutto = 'n/a';
+  }
 
   if( $detail ) {
     open_fieldset( 'big_form', '', "Lieferantenkatalog: Artikel gefunden in Katalog $katalog_typ / $katalog_datum" );
@@ -182,7 +192,7 @@ function katalogabgleich(
           open_td( '', '', $katalog_brutto );
         open_tr();
           open_td( 'left small top', "colspan='3'", 'Interpretation der Foodsoft:' );
-          open_td( 'center small top', "colspan='2'", "1 Gebinde = $gebindegroesse * ($liefereinheit)" );
+          open_td( 'center small top', "colspan='2'", "1 Gebinde = $liefergebinde * ($liefereinheit)" );
           open_td( '', "colspan='2'", '');
           open_td( 'center small top', "colspan='3'", "Preis gilt pro $liefereinheit" );
       close_table();
@@ -192,12 +202,22 @@ function katalogabgleich(
   ////////////////////////////////
   // aktuellsten preiseintrag mit Katalogeintrag vergleichen,
   // Vorlage fuer neuen preiseintrag mit Katalogdaten vorbesetzen:
+  // - die L-felder werden immer aus dem katalog uebernommen, 
+  // - die V-felder wenn moeglich aus dem letzten Preiseintrag
   //
+  $preiseintrag_neu = array();
+
+  $preiseintrag_neu['liefereinheit'] = $liefereinheit;
+  $preiseintrag_neu['lieferpreis'] = $katalog_netto;
+  $preiseintrag_neu['bestellnummer'] = $katalog_bestellnummer;
+  if( $have_mwst ) {
+    $preiseintrag_neu['mwst'] = $katalog_mwst;
+  }
+
   if( $prgueltig ) {
 
     // liefereinheit und mwst sollten mit katalog uebereinstimmen:
     //
-    $preiseintrag_neu['liefereinheit'] = $liefereinheit;
     if( $preiseintrag_neu['liefereinheit'] != $artikel['liefereinheit'] ) {
       $neednewprice = TRUE;
       open_div( 'warn', '', "Problem: L-Einheit stimmt nicht:
@@ -206,13 +226,15 @@ function katalogabgleich(
         <div class='small'>die L-Einheit sollte dem Einzelpreis im Katalog zugrundeliegen</div>
       " );
     }
-    $preiseintrag_neu['mwst'] = $katalog_mwst;
-    if( abs( $preiseintrag_neu['mwst'] - $artikel['mwst'] ) > 0.005 ) {
-      $neednewprice = TRUE;
-      open_div( 'warn', '', "Problem: MWSt-Satz stimmt nicht:
-        <p class='li'>Katalog: <kbd>{$preiseintrag_neu['mwst']}</kbd></p>
-        <p class='li'>Foodsoft: <kbd>{$artikel['mwst']}</kbd></p>
-      " );
+    if( $have_mwst ) {
+      $preiseintrag_neu['mwst'] = $katalog_mwst;
+      if( abs( $preiseintrag_neu['mwst'] - $artikel['mwst'] ) > 0.005 ) {
+        $neednewprice = TRUE;
+        open_div( 'warn', '', "Problem: MWSt-Satz stimmt nicht:
+          <p class='li'>Katalog: <kbd>{$preiseintrag_neu['mwst']}</kbd></p>
+          <p class='li'>Foodsoft: <kbd>{$artikel['mwst']}</kbd></p>
+        " );
+      }
     }
 
     $preiseintrag_neu['verteileinheit'] =
@@ -237,23 +259,24 @@ function katalogabgleich(
     }
 
     kanonische_einheit( $preiseintrag_neu['verteileinheit'], &$kan_verteileinheit_neu, &$kan_verteilmult_neu );
+
     if( $kan_liefereinheit !== $kan_verteileinheit_neu ) {
       // keine automatische umrechnung moeglich; wir uebernehmen die alten werte, die
       // manuell geprueft und bei bedarf korrigiert werden muessen:
       if( $artikel['lv_faktor'] > 0.001 ) {
         $preiseintrag_neu['lv_faktor'] = $artikel['lv_faktor'];
-        $preiseintrag_neu['gebindegroesse'] = $gebindegroesse * $preiseintrag_neu['lv_faktor'];
+        $preiseintrag_neu['gebindegroesse'] = $liefergebinde * $preiseintrag_neu['lv_faktor'];
         if( abs( $preiseintrag_neu['gebindegroesse'] - $artikel['gebindegroesse'] ) > 0.001 ) {
           $neednewprice = TRUE;
           open_div( 'warn', '', "Problem: Gebindegroessen oder Umrechnung Liefer/Verteileinheit stimmen nicht:
-            <p class='li'>Katalog: <kbd>$gebindegroesse * $liefereinheit</kbd></p>
+            <p class='li'>Katalog: <kbd>$liefergebinde * $liefereinheit</kbd></p>
             <p class='li'>Foodsoft: <kbd>{$artikel['gebindegroesse']} * {$artikel['verteileinheit']}</kbd></p>
             <div class='small'>Bitte manuell pr&uuml;en und neuen Preiseintrag erfassen!</div>
           " );
         }
       } else {
         if( $artikel['gebindegroesse'] > 0.001 ) {
-          $preiseintrag_neu['lv_faktor'] = $gebindegroesse / $artikel['gebindegroesse'];
+          $preiseintrag_neu['lv_faktor'] = $liefergebinde / $artikel['gebindegroesse'];
         } else {
           $preiseintrag_neu['lv_faktor'] = 1.0;
         }
@@ -269,31 +292,29 @@ function katalogabgleich(
       // anschliessend mit dem ist-zustand:
 
       $preiseintrag_neu['lv_faktor'] = $kan_liefermult / $kan_verteilmult_neu;
-      $preiseintrag_neu['gebindegroesse'] = $gebindegroesse * $preiseintrag_neu['lv_faktor'];
+      $preiseintrag_neu['gebindegroesse'] = $liefergebinde * $preiseintrag_neu['lv_faktor'];
       if( abs( $preiseintrag_neu['gebindegroesse'] - $artikel['gebindegroesse'] ) > 0.001 ) {
         $neednewprice = TRUE;
         open_div( 'warn', '', "Problem: Gebindegroessen stimmen nicht:
-          <p class='li'>Katalog: <kbd>$gebindegroesse * $liefereinheit</kbd></p>
+          <p class='li'>Katalog: <kbd>$liefergebinde * $liefereinheit</kbd></p>
           <p class='li'>Foodsoft: <kbd>{$artikel['gebindegroesse']} * {$artikel['verteileinheit']}</kbd></p>
         " );
       }
     }
 
-    // lieferpreis: nettopreis wie katalog, aber pro V-einheit:
-    //
-    $preiseintrag_neu['lieferpreis'] = $katalog_netto / $preiseintrag_neu['lv_faktor'];
-
-    if( abs( $preiseintrag_neu['lieferpreis'] - $artikel['nettopreis'] ) > 0.005 ) {
+    if( abs( $preiseintrag_neu['lieferpreis'] - $artikel['nettolieferpreis'] ) > 0.005 ) {
       $neednewprice = TRUE;
-      open_div( 'warn', '', "Problem: Preise stimmen nicht (beide Brutto ohne Pfand):
-        <p class='li'>Katalog: <kbd>$katalog_netto / $liefereinheit</kbd></p>
-        <p class='li'>Foodsoft: <kbd>{$artikel['nettopreis']} / {$artikel['verteileinheit']}
-               = " . $artikel['nettoliefer'] . " / $liefereinheit
-          </kbd></p>
-      " );
+      open_div( 'warn' );
+        echo "Problem: Preise stimmen nicht (beide Brutto ohne Pfand):
+          <p class='li'>Katalog: <kbd>$katalog_netto / $liefereinheit</kbd></p>
+          <p class='li'>Foodsoft: <kbd>{$artikel['nettopreis']} / {$artikel['verteileinheit']} ";
+        if( $liefereinheit != $artikel['verteileinheit'] ) {
+          echo " = {$artikel['nettolieferpreis']} / $liefereinheit";
+        }
+        echo "</kbd></p>";
+      close_div();
     }
 
-    $preiseintrag_neu['bestellnummer'] = $katalog_bestellnummer;
     if( $katalog_bestellnummer != $artikel['bestellnummer'] ) {
       $neednewprice = TRUE;
       open_div( 'warn', '', "Problem: Bestellnummern stimmen nicht:
@@ -301,23 +322,17 @@ function katalogabgleich(
         <p class='li'>Foodsoft: <kbd>{$artikel['bestellnummer']}</kbd></p>
       " );
     }
-    $rv = ( $neednewprice ? 1 : 0 );
 
   } else {
-    // kein aktuell gueltiger Preiseintrag: wir erzeugen Vorlage aus Katalogdaten:
-    $neednewprice = TRUE;
-    $preiseintrag_neu['liefereinheit'] = $liefereinheit;
+    // kein aktuell gueltiger Preiseintrag: wir erzeugen defaults aus Katalogdaten...
     $preiseintrag_neu['verteileinheit'] = $verteileinheit_default;
     $preiseintrag_neu['lv_faktor'] = $lv_faktor_default;
-    $preiseintrag_neu['gebindegroesse'] = $gebindegroesse * $lv_faktor_default;
-    $preiseintrag_neu['mwst'] = $katalog_mwst;
-    $preiseintrag_neu['bestellnummer'] = $katalog_bestellnummer;
-    $preiseintrag_neu['lieferpreis'] = $katalog_netto / $lv_faktor_default;
-
-    $rv = 1;
+    $preiseintrag_neu['gebindegroesse'] = $liefergebinde * $lv_faktor_default;
+    // ...und empfehlen einen neuen Preiseintrag:
+    $neednewprice = TRUE;
   }
 
-  return $rv;
+  return ( $neednewprice ? 1 : 0 );
 }
 
 ?>
