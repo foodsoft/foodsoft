303a80f2d64aeaef1e330119db089068fe3e91bd
Timo Felbinger <gittf@qipc.org>
2009-11-03 14:28:29 +0000

    Fehlermeldungen optional, schoener anzeigen

--- www/code/err_functions.php
+++ www/code/err_functions.php
@@ -8,26 +8,35 @@ function error( $string ) {
   if( ! $in_error ) { // avoid infinite recursion (e.g. if there is no database connection)
     $in_error = true;
     $stack = debug_backtrace();
-    ?> <div class='warn'>Fehler: <? echo htmlspecialchars( $string ); ?>
-       <br>
-       <pre><? echo htmlspecialchars( var_export($stack) ); ?>
-       </pre>
-       </div>
-     <?
+    open_div( 'warn' );
+      smallskip();
+      open_fieldset( '', '', "Fehler", 'off' );
+        echo "<pre><br>[" .htmlspecialchars($string)."]<br>". htmlspecialchars( var_export( $stack, true ) ) . "</pre>";
+      close_fieldset();
+      open_span( 'qquad', '', fc_link( 'self', 'img=,text=weiter...' ) );
+      bigskip();
+    close_div();
     logger( "error: $string [$stack]" );
   }
   die();
 }
 
-function need( $exp, $comment = "Fataler Fehler" ) {
+function need( $exp, $comment = "Problem" ) {
   static $in_need = false;
   if( ! $exp ) {
     if( $in_need )
       die();
     $in_need = true;
-    open_div( 'warn', '', htmlspecialchars( "$comment: $exp" ) . fc_link( 'self', 'img=,text=weiter...' ) );
     $stack = debug_backtrace();
-    logger( "assertion failed: $stack" );
+    open_div( 'warn' );
+      smallskip();
+      open_fieldset( '', '', htmlspecialchars( "$comment" ), 'off' );
+        echo "<pre>". htmlspecialchars( var_export( $stack, true ) ) . "</pre>";
+      close_fieldset();
+      open_span( 'qquad', '', fc_link( 'self', 'img=,text=weiter...' ) );
+      bigskip();
+    close_div();
+    logger( "assertion failed: $exp [$stack]" );
     die();
   }
   return true;
