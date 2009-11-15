1d5b12bb8dd1a1b97db7694c29a4fc590e20c65b
Timo Felbinger <gittf@qipc.org>
2009-11-15 21:03:19 +0000

    lokale Variable $bestell_id -> $b_id ($bestell_id bereits global benutzt)

--- www/windows/basar.php
+++ www/windows/basar.php
@@ -21,11 +21,11 @@ if( $action == 'basarzuteilung' ) {
     if( ! get_http_var( "produkt$i", 'U' ) )
       continue;
     need_http_var( "bestellung$i", 'U' );
-    $bestell_id = ${"bestellung$i"};
-    if( sql_bestellung_status( $bestell_id ) >= STATUS_ABGERECHNET )
+    $b_id = ${"bestellung$i"};
+    if( sql_bestellung_status( $b_id ) >= STATUS_ABGERECHNET )
       continue;
     if( get_http_var( "menge$i", "f" ) ) {
-      $pr = sql_produkt( array( 'bestell_id' => $bestell_id, 'produkt_id' => ${"produkt$i"} ) );
+      $pr = sql_produkt( array( 'bestell_id' => $b_id, 'produkt_id' => ${"produkt$i"} ) );
       $gruppen_menge = ${"menge$i"} / $pr['kan_verteilmult'];
       if( $gruppen_menge > 0 or ( $gruppen_id == $muell_id ) )
         sql_basar2group( $gruppen_id, ${"produkt$i"}, ${"bestellung$i"}, $gruppen_menge );
