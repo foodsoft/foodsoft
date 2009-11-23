1d3153718542c016f5d135af099e97d920c3d7af
Timo Felbinger <gittf@qipc.org>
2009-11-23 14:41:29 +0000

    Gesamtlieferschein, erste Version

--- www/windows/gesamtlieferschein.php
+++ www/windows/gesamtlieferschein.php
@@ -1,6 +1,6 @@
 <?php
 //
-// bestellschein.php: detailanzeige bestellschein / lieferschein, abhaengig vom status der bestellung
+// bestellschein.php: detailanzeige gesamtlieferschein, abhaengig vom status der bestellung
 //
 
 error_reporting(E_ALL);
@@ -9,26 +9,114 @@ error_reporting(E_ALL);
 assert( $angemeldet ) or exit();
 
 need_http_var( 'abrechnung_id', 'U', true );
-$bestell_ids = sql_abrechnung_set( $abrechnung_id );
+$bestell_id_set = sql_abrechnung_set( $abrechnung_id );
+
+$bestellungen = array();
+$bestell_id_list = '';
+$komma = '(';
+foreach( $bestell_id_set as $b ) {
+  $bestell_id_list .= "$komma $b";
+  $komma = ',';
+  $bestellungen[] = sql_bestellung( $b );
+}
+$bestell_id_list .= ')';
 
 
-echo "<h1>$Gesamtlieferschein</h1>";
+echo "<h1>Gesamtlieferschein</h1>";
 
-$produkt_ids = doSql( "
+abrechnung_overview( $abrechnung_id );
+medskip();
+
+$p = mysql2array( doSql( "
   SELECT DISTINCT produkt_id
   FROM bestellvorschlaege
-  WHERE gesamtbestellung.id IN $bestell_ids_list
-" );
+  WHERE gesamtbestellung_id IN $bestell_id_list
+" ) );
+$produkt_ids = array();
+foreach( $p as $q ) {
+  $produkt_ids[] = $q['produkt_id'];
+}
 
-open_table();
+open_table('list');
   open_tr();
-    open_th( 'Name' );
-    open_th( 'A-Nummer' );
-    open_th( 'B-Nummer' );
-    open_th( 'L-Preis', "colspan='2'" );
-    open_th( 'Menge' );
-    open_th( 'Netto' );
-    open_th( 'Brutto' );
+    open_th( '', '', 'Name' );
+    open_th( '', '', 'Bestellung' );
+    open_th( '', '', 'Lieferung' );
+    open_th( '', '', 'A-Nummer' );
+    open_th( '', '', 'B-Nummer' );
+    open_th( '', "colspan='2'", 'L-Preis' );
+    open_th( '', "colspan='2'", 'Menge' );
+    open_th( '', '', 'Netto' );
+    open_th( '', '', 'Brutto' );
+
+
+  $netto_total = 0;
+  $brutto_total = 0;
+  foreach( $produkt_ids as $produkt_id ) {
+    $stammdaten = sql_produkt( array( 'produkt_id' => $produkt_id ) );
+    $produktbestellungen = array();
+    foreach( $bestellungen as $b ) {
+      $bestell_id = $b['id'];
+      $r = sql_bestellung_produkte( $bestell_id, $produkt_id );
+      switch( count($r) ) {
+        case 0:
+          continue 2;
+        case 1:
+          $p = $r[0];
+          if( $p['liefermenge'] < 0.001 )
+            continue 2;
+          $p['name'] = $b['name'];
+          $p['lieferung'] = $b['lieferung'];
+          $produktbestellungen[] = $p;
+          break;
+        default: 
+          error( "internal error: unexpected count: " . count($r) );
+      }
+    }
+    $rowcount = count( $produktbestellungen );
+    if( $rowcount < 1 )
+      continue;
+    open_tr();
+    $rowcount++;
+    open_td( 'bold top', "rowspan='$rowcount'", $stammdaten['name'] );
+    $netto_summe = 0;
+    $brutto_summe = 0;
+    $liefermenge_summe = 0;
+    $first = true;
+    foreach( $produktbestellungen as $p ) {
+      if( ! $first )
+        open_tr();
+      $first = false;
+      $liefermenge = $p['liefermenge'];
+      $netto = $liefermenge * $p['nettopreis'];
+      $brutto = $liefermenge * $p['bruttopreis'];
+      $liefermenge_summe += $liefermenge;
+      $netto_summe += $netto;
+      $brutto_summe += $brutto;
+      open_td( 'left', '', $p['name'] );
+      open_td( 'center', '', $p['lieferung'] );
+      open_td( 'center', '', $p['artikelnummer'] );
+      open_td( 'right', '', $p['bestellnummer'] );
+      open_td( 'mult', '', $p['nettolieferpreis'] );
+      open_td( 'unit', '', " / " . $p['liefereinheit_anzeige'] );
+      open_td( 'mult', '', sprintf( '%.3lf', $p['liefermenge'] * $p['kan_liefermult_anzeige'] / $p['lv_faktor'] ) );
+      open_td( 'unit', '', $p['kan_liefereinheit_anzeige'] );
+      open_td( 'number', '', price_view( $netto ) );
+      open_td( 'number', '', price_view( $brutto ) );
+    }
+    open_tr( 'summe' );
+    open_td( '', "colspan='8'", 'Summe:' );
+    open_td( 'number', '', price_view( $netto_summe ) );
+    open_td( 'number', '', price_view( $brutto_summe ) );
+    $netto_total += $netto_summe;
+    $brutto_total += $brutto_summe;
+  }
+  open_tr( 'summe' );
+    open_td( '', "colspan='9'", 'Gesamtsumme:' );
+    open_td( 'number', '', price_view( $netto_total ) );
+    open_td( 'number', '', price_view( $brutto_total ) );
+
+
 close_table();
 
 ?>
