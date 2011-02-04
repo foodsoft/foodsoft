99cbb8375c7f7690e4255ae2128909216afef922
Tilman Vogel <tilman.vogel@web.de>
2011-02-04 14:30:15 +0100

    deploy.sh: do not use git describe --always

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export LANG=C
 BRANCH=`git branch | sed -e '/^[^*]/d' -e 's/^\* \(.*\)/\1/'`
-COMMIT=`git describe --always`
+COMMIT=`git rev-parse --short HEAD`
 DIRTY=""
 git status | grep -qF 'working directory clean' || DIRTY='-dirty'
 umask 022 # just in case version.txt does not exist yet
