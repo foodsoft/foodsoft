277d6d2015915b93883af6303a1e01a91af90f85
Timo Felbinger <gittf@qipc.org>
2012-02-22 16:31:04 +0100

    in lieferantenkonto.php: rechnungsnummer anzeigen

--- code/zuordnen.php
+++ code/zuordnen.php
@@ -3534,6 +3534,7 @@ function sql_bestellungen_soll_lieferant( $lieferanten_id, $bestell_id = NULL ) 
   $query = "
     SELECT gesamtbestellungen.id as gesamtbestellung_id
          , gesamtbestellungen.name
+         , gesamtbestellungen.rechnungsnummer
          , DATE_FORMAT(gesamtbestellungen.lieferung,'%d.%m.%Y') as lieferdatum_trad
          , DATE_FORMAT(gesamtbestellungen.bestellende,'%d.%m.%Y') as valuta_trad
          , DATE_FORMAT(gesamtbestellungen.bestellende,'%Y%m%d') as valuta_kan
