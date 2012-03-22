7d9e089772ac45307db95b1077df9b4ff99c8861
Timo Felbinger <gittf@qipc.org>
2012-03-22 10:25:26 +0100

    display rechnungsnummer in lieferantenkonto

--- windows/lieferantenkonto.php
+++ windows/lieferantenkonto.php
@@ -109,10 +109,12 @@ open_table('list');
       open_td('bold', '', 'Bestellung' );
       open_td('', '', $vert_row['valuta_trad'] );
       open_td('', '', $vert_row['lieferdatum_trad'] );
-      open_td( '', '', 'Bestellung: ' . fc_link( 'lieferschein', array(
-         'bestell_id' => $bestell_id, 'text' => $vert_row['name'], 'class' => 'href'
-       , 'spalten' => ( PR_COL_NAME | PR_COL_BESTELLMENGE | PR_COL_LPREIS | PR_COL_LIEFERMENGE | PR_COL_NETTOSUMME | PR_COL_ENDSUMME )
-      ) ) );
+      open_td(); 
+        echo 'Bestellung: ' . fc_link( 'lieferschein', array(
+           'bestell_id' => $bestell_id, 'text' => $vert_row['name'], 'class' => 'href'
+         , 'spalten' => ( PR_COL_NAME | PR_COL_BESTELLMENGE | PR_COL_LPREIS | PR_COL_LIEFERMENGE | PR_COL_NETTOSUMME | PR_COL_ENDSUMME )
+        ) );
+        open_span( 'small', '', $vert_row['rechnungsnummer'] );
       open_td( 'number', '', price_view( $pfand_soll ) );
       open_td( 'number', '', price_view( $pfandsumme ) );
       open_td( 'number', '', price_view( $waren_soll ) );
