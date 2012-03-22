79073f04a07703ce88693159beedeb55c5cf59ac
Timo Felbinger <gittf@qipc.org>
2012-03-22 10:33:33 +0100

    restrict "produkte hinzufuegen" to same "lieferant"

--- windows/bestellen.php
+++ windows/bestellen.php
@@ -795,7 +795,8 @@ if( ! $readonly ) {
   $unlisted_products = sql_produkte( array(
       (hat_dienst( 4 ) ? 'price_on_date_or_null' : 'price_on_date') 
           => $gesamtbestellung['lieferung']
-    , 'not_in_order' => $gesamtbestellung['id'] ));
+    , 'not_in_order' => $gesamtbestellung['id']
+    , 'lieferanten_id' => $lieferanten_id  ));
     
   foreach ($unlisted_products as $p) {
     $json = array();
