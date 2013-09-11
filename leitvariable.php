99172a97b36fdb53827a56709eaea274f699c30c
Timo Felbinger <gittf@qipc.org>
2013-09-11 09:45:41 +0000

    current db version is 28

--- leitvariable.php
+++ leitvariable.php
@@ -124,7 +124,7 @@ $leitvariable = array(
   )
 , 'database_version' => array(
     'meaning' => 'Version der Datenbank'
-  , 'default' => '26'
+  , 'default' => '28'
   , 'comment' => 'Bitte den vorgeschlagenen Wert &uuml;bernehmen und nicht manuell &auml;ndern: diese Variable wird bei Upgrades automatisch hochgesetzt!'
   , 'local' => false
   , 'runtime_editable' => 0
