97915da0ea1527307e560c0490c668a54b16a9a1
Oliver Gondring <oliver.gondring@cyren.com>
2020-11-30 13:34:14 +0100

    fix: (temp) set reduced German COVID VAT rates

--- src/windows/katalog_upload.php
+++ src/windows/katalog_upload.php
@@ -631,10 +631,10 @@ function upload_bnn( $katalogformat ) {
 
     switch( trim( $splitline[33] ) ) {
       case '1':
-        $mwst = "7.00";
+        $mwst = "5.00";
         break;
       case '2':
-        $mwst = "19.00";
+        $mwst = "16.00";
         break;
       default:
         break;
