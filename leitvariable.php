052c6359228cd29878403b42d61aa42b8d4fc1cf
Tilman Vogel <tilman.vogel@web.de>
2021-08-24 11:09:34 +0200

    leitvariable.php: Repariere falsche initiale Datenbank-Version

--- leitvariable.php
+++ leitvariable.php
@@ -156,7 +156,7 @@ $leitvariable = array(
   )
 , 'database_version' => array(
     'meaning' => 'Version der Datenbank'
-  , 'default' => '35'
+  , 'default' => '37'
   , 'comment' => 'Bitte den vorgeschlagenen Wert &uuml;bernehmen und nicht manuell &auml;ndern: diese Variable wird bei Upgrades automatisch hochgesetzt!'
   , 'local' => false
   , 'runtime_editable' => 0
