bc253483284eec46e9026dc4c322c99aa896957e
Oliver Gondring <ol.go@posteo.de>
2022-02-03 01:31:50 +0100

    feat: highlight uncleared orders in group account view

--- src/code/zuordnen.php
+++ src/code/zuordnen.php
@@ -3720,6 +3720,7 @@ function sql_bestellungen_soll_gruppe( $gruppen_id, $bestell_id = 0 ) {
   $query = "
     SELECT gesamtbestellungen.id as gesamtbestellung_id
          , gesamtbestellungen.name
+         , gesamtbestellungen.rechnungsstatus AS gesamtbestellung_status
          , DATE_FORMAT(gesamtbestellungen.lieferung,'%d.%m.%Y') as lieferdatum_trad
          , DATE_FORMAT(gesamtbestellungen.bestellende,'%d.%m.%Y') as valuta_trad
          , DATE_FORMAT(gesamtbestellungen.bestellende,'%Y%m%d') as valuta_kan
