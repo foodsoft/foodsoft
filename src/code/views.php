27581e199ebeefc7946db3da8b02d37e0ef4173a
Oliver Gondring <og-dev@gmx.de>
2023-03-06 01:48:23 +0100

    fix: $rowspan is not initialized

--- src/code/views.php
+++ src/code/views.php
@@ -677,6 +677,7 @@ function pick_group_text() {
   $fieldcount = 0;
   $gesamtwert = 0;
   $output = '';
+  $rowspan = 1;
   foreach( $basar as $basar_row ) {
     list( $kan_verteilmult, $kan_verteileinheit ) = kanonische_einheit( $basar_row['verteileinheit'] );
     $menge = $basar_row['basarmenge'];
