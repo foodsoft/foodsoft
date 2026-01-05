7eefbb0fb4db2ed9f0a8b5d6b9b14769ee9b65dc
Tilman Vogel <tilman.vogel@web.de>
2026-01-05 23:27:25 +0100

    QR-Überweisung: Benutze Kontoname als Kontoinhaber...
    ...um IBAN-Prüfung zu ermöglichen, wenn Kontoinhaber eine Privatperson
    ist.

--- deploy.sh
+++ deploy.sh
@@ -39,7 +39,7 @@ chmod 644 code/zuordnen.php
 chmod 644 css/foodsoft.css
 chmod 644 css/modified.gif
 chmod 644 css/readonly.gif
-chmod 744 deploy.sh
+chmod 755 deploy.sh
 chmod 644 dokuwiki_auth_plugin/authfoodsoft/README
 chmod 644 dokuwiki_auth_plugin/authfoodsoft/auth/authentication.php
 chmod 644 dokuwiki_auth_plugin/authfoodsoft/lang/en/lang.php
